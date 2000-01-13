c92f56fadbaf59aedafe42778ff656ceea5141d4
fernandoFernandeSantos <f_157@hotmail.com>
2000-01-13 15:44:14 -0200

    pedestrian detection hog

---
+++ cuda/histogram_ori_gradients/HOG.cpp
@@ -0,0 +1,27 @@
+//new classes
+#include "App.h"
+
+//using namespace std;
+//using namespace cv;
+
+
+
+int main(int argc, char** argv) {
+	try {
+		Args args;
+		if (argc < 2)
+			args.printHelp();
+		args = Args::read(argc, argv);
+		if (args.help_showed)
+			return -1;
+		App app(args);
+		app.run();
+	} catch (const Exception& e) {
+		return cout << "error: " << e.what() << endl, 1;
+	} catch (const exception& e) {
+		return cout << "error: " << e.what() << endl, 1;
+	} catch (...) {
+		return cout << "unknown exception" << endl, 1;
+	}
+	return 0;
+}
