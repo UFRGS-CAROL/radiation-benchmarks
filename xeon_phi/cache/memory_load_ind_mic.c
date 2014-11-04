5204a01e4d6b27243384ca401771334bbea124fc
Marco Alves <mazalves@inf.ufrgs.br>
2014-11-04 10:43:11 -0500

    Fixed the memory and rotate benchmarks

--- xeon_phi/cache/memory_load_ind_mic.c
+++ xeon_phi/cache/memory_load_ind_mic.c
@@ -19,18 +19,19 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-#define MIC_NUM_CORES 1
+// Xeon Phi total cores = 57. 1 core probably runs de OS.
+#define MIC_NUM_THREADS 1
+// #define ARRAY_SIZE 56000
+// #define MAX 32000
+#define REFWORD 1 //  0x55555555
 
-struct list {
-    uint32_t value;
-    char pad[60];
-};
-typedef struct list element;
 
 ///=============================================================================
 int main (int argc, char *argv[]) {
+
     uint32_t size=0;
     uint32_t repetitions=0;
+
     if(argc != 3) {
         printf("Please provide the number of repetitions and array size.\n");
         exit(EXIT_FAILURE);
@@ -43,68 +44,263 @@ int main (int argc, char *argv[]) {
         printf("The array size needs to be divisible by 32 (due to unrolling).\n");
         exit(EXIT_FAILURE);
     }
-    printf("Struct size %"PRIu32"\n", (uint32_t)sizeof(element));
-    printf("Repetitions:%"PRIu32" Size:%"PRIu32"\n", repetitions, size);
-    printf("Memory to be accessed: %"PRIu32"KB\n", (uint32_t)(size * sizeof(element)) / 1024);
+    printf("Element size %"PRIu32"B\n", (uint32_t)sizeof(uint32_t));
+    printf("Repetitions:%"PRIu32" Size:%"PRIu32"KB\n", repetitions, size / 1024);
+    printf("Elements to be accessed: %"PRIu32"K\n", (uint32_t)(size / sizeof(uint32_t)) / 1024);
+
+    omp_set_num_threads(MIC_NUM_THREADS);
+    printf("Threads:%"PRIu32"\n", MIC_NUM_THREADS);
 
     uint32_t i = 0;
     uint32_t j = 0;
     uint32_t jump = 0;
     uint32_t count = 0;
+    uint32_t slice = (size / sizeof(uint32_t)) / MIC_NUM_THREADS ;
 
-    element *ptr_vector;
-    ptr_vector = (element *)valloc(sizeof(element) * size);
+    uint32_t *ptr_vector;
+    ptr_vector = (uint32_t *)valloc(size);
 
-    for (i = 0; i < size; i++) {
-        ptr_vector[i].value = 1;
+    for (i = 0; i < (size / sizeof(uint32_t)); i++) {
+        ptr_vector[i] = REFWORD;
     }
 
-    #pragma offload target(mic) in(ptr_vector:length(sizeof(element) * size)) inout(count)
+    #pragma offload target(mic) in(ptr_vector:length(size / sizeof(uint32_t))) reduction(+:count)
     {
-        #pragma omp parallel for private(i, j)
-        for(j = 0; j < MIC_NUM_CORES; j++)
+        #pragma omp parallel for private(i, j, jump)
+        for(j = 0; j < MIC_NUM_THREADS; j++)
         {
             asm volatile ("nop");
             asm volatile ("nop");
             asm volatile ("nop");
+
+	    uint32_t th_id = omp_get_thread_num();
             for (i = 0; i < repetitions; i++) {
-                for (jump = 0; jump <= size - 32; jump += 32) {
+                for (jump = slice * th_id; jump < slice * (th_id + 1) ;){
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
 
-                    count += ptr_vector[jump + 0].value;
-                    count += ptr_vector[jump + 1].value;
-                    count += ptr_vector[jump + 2].value;
-                    count += ptr_vector[jump + 3].value;
-                    count += ptr_vector[jump + 4].value;
-                    count += ptr_vector[jump + 5].value;
-                    count += ptr_vector[jump + 6].value;
-                    count += ptr_vector[jump + 7].value;
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
 
-                    count += ptr_vector[jump + 8].value;
-                    count += ptr_vector[jump + 9].value;
-                    count += ptr_vector[jump + 10].value;
-                    count += ptr_vector[jump + 11].value;
-                    count += ptr_vector[jump + 12].value;
-                    count += ptr_vector[jump + 13].value;
-                    count += ptr_vector[jump + 14].value;
-                    count += ptr_vector[jump + 15].value;
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
 
-                    count += ptr_vector[jump + 16].value;
-                    count += ptr_vector[jump + 17].value;
-                    count += ptr_vector[jump + 18].value;
-                    count += ptr_vector[jump + 19].value;
-                    count += ptr_vector[jump + 20].value;
-                    count += ptr_vector[jump + 21].value;
-                    count += ptr_vector[jump + 22].value;
-                    count += ptr_vector[jump + 23].value;
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
 
-                    count += ptr_vector[jump + 24].value;
-                    count += ptr_vector[jump + 25].value;
-                    count += ptr_vector[jump + 26].value;
-                    count += ptr_vector[jump + 27].value;
-                    count += ptr_vector[jump + 28].value;
-                    count += ptr_vector[jump + 29].value;
-                    count += ptr_vector[jump + 30].value;
-                    count += ptr_vector[jump + 31].value;
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+		    if ((ptr_vector[jump] ^ REFWORD)) {
+			count++;
+			printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, jump, j, ptr_vector[jump] ^ REFWORD);
+			ptr_vector[jump] = REFWORD;
+		    }
+		    jump++;
+
+
                 }
             }
             asm volatile ("nop");
--- xeon_phi/rotate/rotate.c
+++ xeon_phi/rotate/rotate.c
@@ -5,38 +5,39 @@
 #include <math.h>
 
 // Xeon Phi total cores = 57. 1 core probably runs de OS.
-#define MIC_NUM_CORES 1
+#define MIC_NUM_THREADS 1
 #define ARRAY_SIZE 56000
 #define MAX 32000
-#define refword 1 //  0x55555555
+#define REFWORD 1 //  0x55555555
 
 ///=============================================================================
 int main (int argc, char *argv[]) {
-    uint64_t size=0;
-    uint64_t repetitions=0;
+    uint32_t size=0;
+    uint32_t repetitions=0;
     if(argc != 2) {
         printf("Please provide the number of repetitions.\n");
         exit(EXIT_FAILURE);
     }
 
     repetitions = atoi(argv[1]);
+    printf("Repetitions:%"PRIu32"\n", repetitions);
 
-    printf("Repetitions:%"PRIu64"\n", repetitions);
+    omp_set_num_threads(MIC_NUM_THREADS);
+    printf("Threads:%"PRIu32"\n", MIC_NUM_THREADS);
 
     uint32_t i = 0;
+    uint32_t j = 0;
     uint32_t error_count = 0;
 
     #pragma offload target(mic) reduction(+:error_count)
     {
-        #pragma omp parallel for
-        for(i = 0; i < MIC_NUM_CORES; i++)
+        #pragma omp parallel for private(j)
+        for(i = 0; i < MIC_NUM_THREADS; i++)
         {
             asm volatile ("nop");
             asm volatile ("nop");
             asm volatile ("nop");
-            uint32_t value=refword;
-<<<<<<< HEAD
-            uint32_t j;
+            uint32_t value = REFWORD;
             for (j = 1; j <= repetitions; j++) {
                 
 	    	asm ("roll %0" : "+r" (value) : "0" (value) );
@@ -79,11 +80,11 @@ int main (int argc, char *argv[]) {
                // if(i == 1)
                // 	value = 1;
 
-		if ( value != refword) {
+		if ( value != REFWORD) {
 			// DEBUG
                 	// printf("Error found!\n");
 			error_count++;
-			value = refword;
+			value = REFWORD;
 		}
             }
             asm volatile ("nop");
