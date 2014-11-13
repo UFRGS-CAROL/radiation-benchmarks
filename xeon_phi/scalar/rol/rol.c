d5a0753951bdd3ee7f46fdf3106dc7d3ec654ff0
Marco Alves <mazalves@inf.ufrgs.br>
2014-11-13 07:09:50 -0500

    Added new benchmarks and fixed the fma. Notice that fma is not using zmm registers

---
+++ xeon_phi/scalar/rol/rol.c
@@ -0,0 +1,116 @@
+#include "../../refword.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <math.h>
+
+// Xeon Phi Configuration
+#define MIC_NUM_CORES       (56)                      // Max. 56 Cores (+1 core runs de OS)
+#define MIC_NUM_THREADS     (4*MIC_NUM_CORES)         // Max. 4 Threads per Core.
+
+//======================================================================
+#define LOOP_BLOCK {\
+                        asm ("rol %0" : "+r" (value) : "0" (value) );\
+                    }
+
+//======================================================================
+int main (int argc, char *argv[]) {
+
+    uint32_t repetitions = 0;
+    uint32_t ref_word = 0;
+
+    if(argc != 3) {
+        printf("Please provide the number of <repetitions> and <refword option>.\n");
+        print_refword();
+        exit(EXIT_FAILURE);
+    }
+
+    repetitions = atoi(argv[1]);
+
+    ref_word = get_refword(atoi(argv[2]));
+
+    printf("Repetitions:%"PRIu32"\n",           repetitions);
+    printf("Ref Word:0x%08x\n",                 ref_word);
+
+    omp_set_num_threads(MIC_NUM_THREADS);
+    printf("Threads:%"PRIu32"\n",               MIC_NUM_THREADS);
+
+    //==================================================================
+    // Benchmark variables
+    uint32_t th_id = 0;
+    uint32_t i = 0;
+    uint32_t error_count = 0;
+
+    #pragma offload target(mic)
+    {
+        #pragma omp parallel for private(th_id, i) reduction(+:error_count)
+        for(th_id = 0; th_id < MIC_NUM_THREADS; th_id++)
+        {
+            asm volatile ("nop");
+            asm volatile ("nop");
+            asm volatile ("nop");
+
+            uint32_t value = ref_word;
+
+            for (i = 1; i <= repetitions; i++) {
+
+                // DEBUG: injecting one error
+                //if(th_id == 0 && i == 0)
+                    //value = ~ref_word; // Bit-wise not
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+                LOOP_BLOCK
+
+                if (value != ref_word) {
+                    error_count++;
+                    printf("%d it, %d pos, %d thread, 0x%08x syndrome\n", i, 0, th_id, value ^ ref_word); \
+                    value = ref_word;
+                }
+            }
+            asm volatile ("nop");
+            asm volatile ("nop");
+            asm volatile ("nop");
+        }
+    }
+
+    printf("Errors: %"PRIu32"\n", error_count);
+    exit(EXIT_SUCCESS);
+}
--- xeon_phi/vector/fma/fma.c
+++ xeon_phi/vector/fma/fma.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <omp.h>
+
+//https://software.intel.com/en-us/articles/using-intel-avx-without-writing-avx
 
 // Xeon Phi Configuration
 #define MIC_NUM_CORES       1                      // Max. 56 Cores (+1 core runs de OS)
@@ -8,7 +12,7 @@
 #define MAX_SIZE            512*1024*MIC_NUM_CORES  // Max. 512KB per L2
 
 
-#define SIZE 16
+#define SIZE 8
 // 56 cores * 4 threads = 224
 // ITER multiple of 224 to equally schedule threads among cores
 #define ITER 22400000
@@ -16,7 +20,7 @@
 extern double elapsedTime (void);
 
 //======================================================================
-int main() {
+int main(int argc, char *argv[]) {
 
     uint32_t repetitions=0;
 
@@ -26,7 +30,6 @@ int main() {
     }
 
     repetitions = atoi(argv[1]);
-
     printf("Repetitions:%"PRIu32"\n",           repetitions);
 
     omp_set_num_threads(MIC_NUM_THREADS);
@@ -35,35 +38,44 @@ int main() {
     //==================================================================
     // Benchmark variables
     double startTime,  duration;
-    int i;
+    uint32_t th_id = 0;
+    uint32_t i = 0;
+    uint32_t error_count = 0;
 
-    omp_set_num_threads(MIC_NUM_THREADS);
-
-    int count = 0;
-
-    #pragma offload target(mic) reduction(+:count)
+    #pragma offload target(mic)
     {
-
-        __declspec(aligned(64)) double a[SIZE],b[SIZE],c[SIZE];
-
-
-        for (i=0; i<SIZE;i++)
+        #pragma omp parallel for private(th_id, i) reduction(+:error_count)
+        for(th_id = 0; th_id < MIC_NUM_THREADS; th_id++)
         {
-            c[i]=b[i]=a[i]=(double)rand();
-        }
+            asm volatile ("nop");
+            asm volatile ("nop");
+            asm volatile ("nop");
 
-        #pragma omp parallel for
-        for(i=0; i<ITER;i++) {
+            __declspec(aligned(64)) double a[SIZE],b[SIZE],c[SIZE];
+
+            for (i=0; i<SIZE;i++) {
+                c[i]=b[i]=a[i]=(double)rand();
+            }
+
+            asm volatile ("nop");
+            asm volatile ("nop");
+            asm volatile ("nop");
+
             #pragma vector aligned (a,b,c)
-            a[0:SIZE]=b[0:SIZE]*c[0:SIZE]+a[0:SIZE];
-        }
+            for(i=0; i<ITER;i++) {
+                a[0:SIZE]=b[0:SIZE]*c[0:SIZE]+a[0:SIZE];
+            }
+
+            asm volatile ("nop");
+            asm volatile ("nop");
+            asm volatile ("nop");
+
 
         // need the gold output to chech errors
         // count++;???
+        }
     }
 
-    printf("error count = %d\n",count);
-
-    return 0;
-
+    printf("Errors: %"PRIu32"\n", error_count);
+    exit(EXIT_SUCCESS);
 }
