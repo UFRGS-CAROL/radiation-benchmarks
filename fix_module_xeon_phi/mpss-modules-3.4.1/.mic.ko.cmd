78ed7e1423e7858fd67770587b1bad1d259b0752
Daniel Oliveira <daniel.kblin.arroba@gmail.com>
2014-11-19 16:41:02 -0200

    Fix for the slow boot of the Xeon Phi machine

---
+++ fix_module_xeon_phi/mpss-modules-3.4.1/.mic.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/carol/mpss-modules-3.4.1/mic.ko := ld -r -m elf_x86_64 -T /usr/src/kernels/3.10.0-123.el7.x86_64/scripts/module-common.lds --build-id  -o /home/carol/mpss-modules-3.4.1/mic.ko /home/carol/mpss-modules-3.4.1/mic.o /home/carol/mpss-modules-3.4.1/mic.mod.o
