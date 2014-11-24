5ce37a6ee292852afc2c7b85372c118f26fd4b45
Daniel <dagoliveira@inf.ufrgs.br>
2014-11-24 20:46:15 -0200

    Log helper functions update to automatic measure time, and hashtags included for each line.

--- include/log_helper.c
+++ include/log_helper.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 
 char log_file_name[100] = "";
 char absolute_path[200] = "";
 char full_log_file_name[300] = "";
 
-double kernels_total_time = 0;
 unsigned long int kernels_total_errors = 0;
 unsigned long int iteration_number = 0;
+double kernel_time_acc = 0;
+long long it_time_start;
+
+
+inline long long get_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000000) + tv.tv_usec;
+}
 
 // In case the user needs the log to be generated in some exact absolute path
 void set_absolute_path(char *path){
@@ -20,13 +29,13 @@ char * get_log_file_name(){
 	return full_log_file_name;
 }
 
-// Generate the log file name and reset log variables
-int start_log_file(char *benchmark_name){
+// Generate the log file name, log info from user about the test to be executed and reset log variables
+int start_log_file(char *benchmark_name, char *test_info){
 
 	time_t file_time;
 	struct tm *ptm;
-	char day[2], month[2], year[4], hour[2], second[2], minute[2];
-	char log_file_name[60];
+	char day[3], month[3], year[5], hour[3], second[3], minute[3];
+	char log_file_name[80];
 
 	file_time = time(NULL);
 	ptm = gmtime(&file_time);
@@ -56,19 +65,23 @@ int start_log_file(char *benchmark_name){
 		fprintf(stderr, "[ERROR in create_log_file(char *)] Unable to open file %s\n",full_log_file_name);
 		return 1;
 	} else
-		close(file);
+	if(test_info != NULL)
+	    fprintf(file, "#HEADER %s\n",test_info);
+    else
+	    fprintf(file, "#HEADER\n");
+	fprintf(file, "#BEGIN Year:%s Month:%s Day:%s Time:%s:%s:%s\n", year, month, day, hour, minute, second);
+	fflush(file);
+	close(file);
 
 
-	kernels_total_time = 0;
 	kernels_total_errors = 0;
 	iteration_number = 0;
+	kernel_time_acc = 0;
 	return 0;
 }
 
-
-// Print some string to log file
-int log_string(char *string){
-
+// Log the string "#END" and reset global variables
+int end_log_file(){
 	FILE *file = fopen(full_log_file_name, "a");
 
 	if (file == NULL){
@@ -76,16 +89,21 @@ int log_string(char *string){
 		return 1;
 	}
 
-	fputs(string, file);
-	fprintf(file, "\n");
+	fprintf(file, "#END");
 	fflush(file);
 	close(file);
+	kernels_total_errors = 0;
+	iteration_number = 0;
+	kernel_time_acc = 0;
+	strcpy(log_file_name, "");
+	strcpy(absolute_path, "");
+	strcpy(full_log_file_name, "");
+
 	return 0;
 }
-
-
-// Print some string to log file followed by some integer value
-int log_string_int(char *string, int value){
+	
+// Start time to measure kernel time, also update iteration number and log to file
+int start_iteration(){
 
 	FILE *file = fopen(full_log_file_name, "a");
 
@@ -94,16 +112,20 @@ int log_string_int(char *string, int value){
 		return 1;
 	}
 
-	fputs(string, file);
-	fprintf(file, "%d\n", value);
+	fprintf(file, "#ITER it:%lu\n", iteration_number);
 	fflush(file);
 	close(file);
+	iteration_number++;
+	it_time_start = get_time();
 	return 0;
+
 }
 
+// Finish the measured kernel time log both time (total time and kernel time)
+int end_iteration(){
 
-// Print some string to log file followed by some double value
-int log_string_double(char *string, double value){
+	double kernel_time = (double) (get_time() - it_time_start) / 1000000;
+	kernel_time_acc += kernel_time;
 
 	FILE *file = fopen(full_log_file_name, "a");
 
@@ -112,27 +134,8 @@ int log_string_double(char *string, double value){
 		return 1;
 	}
 
-	fputs(string, file);
-	fprintf(file, "%f\n", value);
-	fflush(file);
-	close(file);
-	return 0;
-}
-
-// Update total time variable and log both time (total time and kernel time)
-int log_time(double kernel_time){
-
-	kernels_total_time += kernel_time;
-
-	FILE *file = fopen(full_log_file_name, "a");
-
-	if (file == NULL){
-		fprintf(stderr, "[ERROR in log_string(char *)] Unable to open file %s\n",full_log_file_name);
-		return 1;
-	}
-
-	fprintf(file, "kernel time: %f\n", kernel_time);
-	fprintf(file, "kernels total time: %f\n", kernels_total_time);
+	fprintf(file, "#TIME kernel_time:%f\n", kernel_time);
+	fprintf(file, "#ACC_TIME total_time:%f\n", kernel_time_acc);
 	fflush(file);
 	close(file);
 	return 0;
@@ -140,7 +143,7 @@ int log_time(double kernel_time){
 }
 
 // Update total errors variable and log both errors(total errors and kernel errors)
-int log_error(unsigned long int kernel_errors){
+int log_error_count(unsigned long int kernel_errors){
 
 	kernels_total_errors += kernel_errors;
 
@@ -151,16 +154,16 @@ int log_error(unsigned long int kernel_errors){
 		return 1;
 	}
 
-	fprintf(file, "kernel errors: %lu\n", kernel_errors);
-	fprintf(file, "kernels total errors: %lu\n", kernels_total_errors);
+	fprintf(file, "#SDC kernel_errors:%lu\n", kernel_errors);
+	fprintf(file, "#TOTAL_SDC total_errors:%lu\n", kernels_total_errors);
 	fflush(file);
 	close(file);
 	return 0;
 
 }
 
-// Update total errors variable and log both errors(total errors and kernel errors)
-int start_iteration(){
+// Print some string with the detail of an error to log file
+int log_error_detail(char *string){
 
 	FILE *file = fopen(full_log_file_name, "a");
 
@@ -169,9 +172,11 @@ int start_iteration(){
 		return 1;
 	}
 
-	fprintf(file, "---it: %lu\n", iteration_number);
+	fputs("#ERROR ", file);
+	fputs(string, file);
+	fprintf(file, "\n");
 	fflush(file);
 	close(file);
 	return 0;
-
 }
+
