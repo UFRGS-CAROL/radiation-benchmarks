24cbd49d24ef88544252b8d237714ae313402aa2
Daniel <dagoliveira@inf.ufrgs.br>
2014-12-08 10:57:49 -0200

    script again

--- xeon_phi/run_tests.sh
+++ xeon_phi/run_tests.sh
@@ -10,22 +10,22 @@ source /opt/intel/composerxe/bin/compilervars.sh intel64
 ##      Infinite repetitions
 ##
 ################################################################################
-./cache/cache 0 $((28672 * 56))     # L1 Cache (28KB per core)
-./cache/cache 0 $((491520 * 56))    # L2 Cache (480KB per core)
+/home/carol/radiation-benchmarks/xeon_phi/cache/cache 0 $((28672 * 56))     # L1 Cache (28KB per core)
+/home/carol/radiation-benchmarks/xeon_phi/cache/cache 0 $((491520 * 56))    # L2 Cache (480KB per core)
 ###############################################
-./scalar/reg/reg 0      # Scalar registers
-./scalar/and/and 0      # Scalar AND + ADD + MUL + DIV
-./scalar/or/or   0      # Scalar OR  + ADD + MUL + DIV
-./scalar/slr/slr 0      # Scalar SLR + ADD + MUL + DIV
-./scalar/rol/rol 0      # Scalar ROL + ADD + MUL + DIV
+/home/carol/radiation-benchmarks/xeon_phi/scalar/reg/reg 0      # Scalar registers
+/home/carol/radiation-benchmarks/xeon_phi/scalar/and/and 0      # Scalar AND + ADD + MUL + DIV
+/home/carol/radiation-benchmarks/xeon_phi/scalar/or/or   0      # Scalar OR  + ADD + MUL + DIV
+/home/carol/radiation-benchmarks/xeon_phi/scalar/slr/slr 0      # Scalar SLR + ADD + MUL + DIV
+/home/carol/radiation-benchmarks/xeon_phi/scalar/rol/rol 0      # Scalar ROL + ADD + MUL + DIV
 ###############################################
-./vector/reg/reg     0      # Vectorial registers:  32x ZMM#
-./vector/and/and_int 0      # Vectorial Integer:    AND + ADD + MUL
-./vector/and/and_fpd 0      # Vectorial FP Double:  AND + ADD + MUL
-./vector/or/or_int   0      # Vectorial Integer:    OR  + ADD + MUL
-./vector/or/or_fpd   0      # Vectorial FP Double:  OR  + ADD + MUL
-./vector/slr/slr_int 0      # Vectorial Integer:    SLR + ADD + MUL
-./vector/slr/slr_fpd 0      # Vectorial FP Double:  SLR + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/reg/reg     0      # Vectorial registers:  32x ZMM#
+/home/carol/radiation-benchmarks/xeon_phi/vector/and/and_int 0      # Vectorial Integer:    AND + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/and/and_fpd 0      # Vectorial FP Double:  AND + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/or/or_int   0      # Vectorial Integer:    OR  + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/or/or_fpd   0      # Vectorial FP Double:  OR  + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/slr/slr_int 0      # Vectorial Integer:    SLR + ADD + MUL
+/home/carol/radiation-benchmarks/xeon_phi/vector/slr/slr_fpd 0      # Vectorial FP Double:  SLR + ADD + MUL
 ################################################################################
 ##
 ##      10 repetitions only
