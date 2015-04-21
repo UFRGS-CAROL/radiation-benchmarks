fd6e226ce0050521dc8d863cb2ea7deb755bdcef
Daniel Oliveira <daniel.kblin.arroba@gmail.com>
2015-04-21 16:57:51 -0300

    kmeans again ...

--- opencl/kmeans/kmeans.cpp
+++ opencl/kmeans/kmeans.cpp
@@ -41,11 +41,11 @@
      #define BLOCK_SIZE2 256
 #endif
 
-long long get_time() {
-        struct timeval tv; 
-        gettimeofday(&tv, NULL);
-        return (tv.tv_sec * 1000000) + tv.tv_usec;
-}
+//long long get_time() {
+//        struct timeval tv; 
+//        gettimeofday(&tv, NULL);
+//        return (tv.tv_sec * 1000000) + tv.tv_usec;
+//}
 
 
 
