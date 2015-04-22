9997f8a79b14bd995818599fc28734749a48c1a6
Daniel Oliveira <daniel.kblin.arroba@gmail.com>
2015-04-22 20:12:52 -0300

    gemm, minor fixing again

--- opencl/gemm/GEMM.cpp
+++ opencl/gemm/GEMM.cpp
@@ -481,7 +481,7 @@ void runTest(const string& testName, cl_device_id dev, cl_context ctx,
         CL_BAIL_ON_ERROR(err);
 
 
-	double kernel_time = (double) (get_time() - k_time_start) / 1000000;
+	double kernel_time = (double) (get_time() - k_start) / 1000000;
 	double flops = 2.0*(double)input_size*input_size*input_size;
 	double gflops = flops / kernel_time;
 	printf("kernel time: %lf\n",kernel_time);
