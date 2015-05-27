e8ee6b426d3d05bc5c067ff1b47ac255886013ab
Caio Lunardi <caio.b.lunardi@gmail.com>
2015-05-26 21:24:32 -0300

    cudaMxM finished, added perfmesure

--- cuda/mxm/perfmesure.sh
+++ cuda/mxm/perfmesure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for ((i=1; i <= $#; i++)); do
 	echo "size: ${!i} - FLOP - TIME"
-	rm Double_*_${!i}.matrix output.txt
+	rm Double_*_${!i}.matrix output.txt -f
 	./generateMatrices ${!i} > /dev/null
-	./cudaGEMM ${!i} Double_A_8192.matrix Double_B_8192.matrix Double_GOLD_${!i}.matrix 1 > output.txt 2>&1
+	./cudaMxM ${!i} Double_A_8192.matrix Double_B_8192.matrix Double_GOLD_${!i}.matrix 1 > output.txt 2>&1
 	time= cat output.txt | grep "time: [0-9]\{1,\}.[0-9]\{1,\}" -o | cut -c7-
-	nvprof -m flop_count_dp ./cudaGEMM ${!i} Double_A_8192.matrix Double_B_8192.matrix Double_GOLD_${!i}.matrix 1 > output.txt 2>&1
+	nvprof -m flop_count_dp ./cudaMxM ${!i} Double_A_8192.matrix Double_B_8192.matrix Double_GOLD_${!i}.matrix 1 > output.txt 2>&1
 	flop= cat output.txt | grep "Double Precisi  [0-9e+\.]\{1,\}" -o | cut -c17-
 done
