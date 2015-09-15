d2e276b1204bf7e86fe8e938a284730430d528a2
Fernando <f_157@hotmail.com>
2015-09-15 10:07:30 -0300

    test hog

--- cuda/histogram_ori_gradients/git_update.sh
+++ cuda/histogram_ori_gradients/git_update.sh
@@ -7,7 +7,7 @@ git commit -m "$2"
 git push origin master
 expected "Username for 'https://github.com': "
 send "fernandoFernandeSantos"
-expected "password"
+expected "Password for 'https://fernandoFernandeSantos@github.com': "
 send "informatica@4"
 #vai pedir a senha
 
