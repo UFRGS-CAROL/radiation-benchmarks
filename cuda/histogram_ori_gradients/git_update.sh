894b4bebdaad805f7eac2a3efe861b0f5a40c608
Fernando <f_157@hotmail.com>
2015-09-15 10:07:01 -0300

    test hog

--- cuda/histogram_ori_gradients/git_update.sh
+++ cuda/histogram_ori_gradients/git_update.sh
@@ -5,7 +5,7 @@ git add $1/*
 
 git commit -m "$2"
 git push origin master
-expected "username"
+expected "Username for 'https://github.com': "
 send "fernandoFernandeSantos"
 expected "password"
 send "informatica@4"
