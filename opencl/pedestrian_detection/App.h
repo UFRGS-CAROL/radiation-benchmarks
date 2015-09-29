5d840781e099638ee776840c8ed4a130ac93cdde
Fernando <f_157@hotmail.com>
2015-09-29 20:31:17 -0300

    New classes created

--- opencl/pedestrian_detection/App.h
+++ opencl/pedestrian_detection/App.h
@@ -5,20 +5,25 @@
  *      Author: fernando
  */
 
+#ifndef APP_H_
+#define APP_H_
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <iomanip>
 #include <stdexcept>
-#include "opencv2/ocl/ocl.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
-
-#ifndef APP_H_
-#define APP_H_
 
 class App {
 public:
@@ -44,7 +49,6 @@ private:
 
 	//Args args;
 	bool running;
-	bool use_gpu;
 	bool make_gray;
 	double scale;
 	double resize_scale;
@@ -77,20 +81,19 @@ App::App(CommandLineParser& cmd) {
 			<< "\t3/e - increase/decrease HOG group threshold\n"
 			<< "\t4/r - increase/decrease hit threshold\n" << endl;
 
-	use_gpu = true;
-	make_gray = cmd.get<bool>("g");
+	make_gray = cmd.has("gray");
 	resize_scale = cmd.get<double>("s");
-	win_width = cmd.get<bool>("l") == true ? 64 : 48;
-	vdo_source = cmd.get<string>("v");
-	img_source = cmd.get<string>("i");
-	output = cmd.get<string>("o");
+	vdo_source = cmd.get < string > ("v");
+	img_source = cmd.get < string > ("i");
+	output = cmd.get < string > ("o");
 	camera_id = cmd.get<int>("c");
 
+	win_width = 48;
 	win_stride_width = 8;
 	win_stride_height = 8;
 	gr_threshold = 8;
 	nlevels = 13;
-	hit_threshold = win_width == 48 ? 1.4 : 0.;
+	hit_threshold = 1.4;
 	scale = 1.05;
 	gamma_corr = true;
 	write_once = false;
@@ -113,24 +116,15 @@ void App::run() {
 	Size win_stride(win_stride_width, win_stride_height);
 
 	// Create HOG descriptors and detectors here
-	vector<float> detector;
-	if (win_size == Size(64, 128))
-		detector = ocl::HOGDescriptor::getPeopleDetector64x128();
-	else
-		detector = ocl::HOGDescriptor::getPeopleDetector48x96();
 
-	ocl::HOGDescriptor gpu_hog(win_size, Size(16, 16), Size(8, 8), Size(8, 8),
-			9, ocl::HOGDescriptor::DEFAULT_WIN_SIGMA, 0.2, gamma_corr,
-			ocl::HOGDescriptor::DEFAULT_NLEVELS);
-	HOGDescriptor cpu_hog(win_size, Size(16, 16), Size(8, 8), Size(8, 8), 9, 1,
-			-1, HOGDescriptor::L2Hys, 0.2, gamma_corr,
+	HOGDescriptor hog(win_size, Size(16, 16), Size(8, 8), Size(8, 8), 9, 1, -1,
+			HOGDescriptor::L2Hys, 0.2, gamma_corr,
 			cv::HOGDescriptor::DEFAULT_NLEVELS);
-	gpu_hog.setSVMDetector(detector);
-	cpu_hog.setSVMDetector(detector);
+	hog.setSVMDetector(HOGDescriptor::getDaimlerPeopleDetector());
 
 	while (running) {
 		VideoCapture vc;
-		Mat frame;
+		UMat frame;
 
 		if (vdo_source != "") {
 			vc.open(vdo_source.c_str());
@@ -147,25 +141,22 @@ void App::run() {
 			}
 			vc >> frame;
 		} else {
-			frame = imread(img_source);
+			imread(img_source).copyTo(frame);
 			if (frame.empty())
 				throw runtime_error(
 						string("can't open image file: " + img_source));
 		}
 
-		Mat img_aux, img, img_to_show;
-		ocl::oclMat gpu_img;
+		UMat img_aux, img;
+		Mat img_to_show;
 
 		// Iterate over all frames
-		bool verify = false;
 		while (running && !frame.empty()) {
 			workBegin();
 
 			// Change format of the image
 			if (make_gray)
-				cvtColor(frame, img_aux, CV_BGR2GRAY);
-			else if (use_gpu)
-				cvtColor(frame, img_aux, CV_BGR2BGRA);
+				cvtColor(frame, img_aux, COLOR_BGR2GRAY);
 			else
 				frame.copyTo(img_aux);
 
@@ -176,51 +167,33 @@ void App::run() {
 				resize(img_aux, img, sz);
 			} else
 				img = img_aux;
-			img_to_show = img;
-			gpu_hog.nlevels = nlevels;
-			cpu_hog.nlevels = nlevels;
-			vector<Rect> found;
+			img.copyTo(img_to_show);
+			hog.nlevels = nlevels;
+			vector < Rect > found;
 
 			// Perform HOG classification
 			hogWorkBegin();
-			if (use_gpu) {
-				gpu_img.upload(img);
-				gpu_hog.detectMultiScale(gpu_img, found, hit_threshold,
-						win_stride, Size(0, 0), scale, gr_threshold);
-				if (!verify) {
-					// verify if GPU output same objects with CPU at 1st run
-					verify = true;
-					vector<Rect> ref_rst;
-					cvtColor(img, img, CV_BGRA2BGR);
-					cpu_hog.detectMultiScale(img, ref_rst, hit_threshold,
-							win_stride, Size(0, 0), scale, gr_threshold - 2);
-					double accuracy = checkRectSimilarity(img.size(), ref_rst,
-							found);
-					cout << "\naccuracy value: " << accuracy << endl;
-				}
-			} else
-				cpu_hog.detectMultiScale(img, found, hit_threshold, win_stride,
-						Size(0, 0), scale, gr_threshold);
+
+			hog.detectMultiScale(img, found, hit_threshold, win_stride,
+					Size(0, 0), scale, gr_threshold);
 			hogWorkEnd();
 
 			// Draw positive classified windows
 			for (size_t i = 0; i < found.size(); i++) {
 				Rect r = found[i];
-				rectangle(img_to_show, r.tl(), r.br(), CV_RGB(0, 255, 0), 3);
+				rectangle(img_to_show, r.tl(), r.br(), Scalar(0, 255, 0), 3);
 			}
 
-			if (use_gpu)
-				putText(img_to_show, "Mode: GPU", Point(5, 25),
-						FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0), 2);
-			else
-				putText(img_to_show, "Mode: CPU", Point(5, 25),
-						FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0), 2);
+			putText(img_to_show,
+					ocl::useOpenCL() ? "Mode: OpenCL" : "Mode: CPU",
+					Point(5, 25), FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0),
+					2);
 			putText(img_to_show, "FPS (HOG only): " + hogWorkFps(),
 					Point(5, 65), FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0),
 					2);
 			putText(img_to_show, "FPS (total): " + workFps(), Point(5, 105),
 					FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0), 2);
-			imshow("opencv_gpu_hog", img_to_show);
+			imshow("opencv_hog", img_to_show);
 			if (vdo_source != "" || camera_id != -1)
 				vc >> frame;
 
@@ -234,19 +207,20 @@ void App::run() {
 				} else                    //write video
 				{
 					if (!video_writer.isOpened()) {
-						video_writer.open(output, CV_FOURCC('x', 'v', 'i', 'd'),
-								24, img_to_show.size(), true);
+						video_writer.open(output,
+								VideoWriter::fourcc('x', 'v', 'i', 'd'), 24,
+								img_to_show.size(), true);
 						if (!video_writer.isOpened())
 							throw std::runtime_error(
 									"can't create video writer");
 					}
 
 					if (make_gray)
-						cvtColor(img_to_show, img, CV_GRAY2BGR);
+						cvtColor(img_to_show, img, COLOR_GRAY2BGR);
 					else
-						cvtColor(img_to_show, img, CV_BGRA2BGR);
+						cvtColor(img_to_show, img, COLOR_BGRA2BGR);
 
-					video_writer << img;
+					video_writer << img.getMat(ACCESS_READ);
 				}
 			}
 
@@ -262,8 +236,9 @@ void App::handleKey(char key) {
 		break;
 	case 'm':
 	case 'M':
-		use_gpu = !use_gpu;
-		cout << "Switched to " << (use_gpu ? "CUDA" : "CPU") << " mode\n";
+		ocl::setUseOpenCL(!cv::ocl::useOpenCL());
+		cout << "Switched to " << (ocl::useOpenCL() ? "OpenCL enabled" : "CPU")
+				<< " mode\n";
 		break;
 	case 'g':
 	case 'G':
@@ -349,47 +324,4 @@ inline string App::workFps() const {
 	ss << work_fps;
 	return ss.str();
 }
-
-double App::checkRectSimilarity(Size sz, std::vector<Rect>& ob1,
-		std::vector<Rect>& ob2) {
-	double final_test_result = 0.0;
-	size_t sz1 = ob1.size();
-	size_t sz2 = ob2.size();
-
-	if (sz1 != sz2) {
-		return sz1 > sz2 ? (double) (sz1 - sz2) : (double) (sz2 - sz1);
-	} else {
-		if (sz1 == 0 && sz2 == 0)
-			return 0;
-		cv::Mat cpu_result(sz, CV_8UC1);
-		cpu_result.setTo(0);
-
-		for (vector<Rect>::const_iterator r = ob1.begin(); r != ob1.end();
-				r++) {
-			cv::Mat cpu_result_roi(cpu_result, *r);
-			cpu_result_roi.setTo(1);
-			cpu_result.copyTo(cpu_result);
-		}
-		int cpu_area = cv::countNonZero(cpu_result > 0);
-
-		cv::Mat gpu_result(sz, CV_8UC1);
-		gpu_result.setTo(0);
-		for (vector<Rect>::const_iterator r2 = ob2.begin(); r2 != ob2.end();
-				r2++) {
-			cv::Mat gpu_result_roi(gpu_result, *r2);
-			gpu_result_roi.setTo(1);
-			gpu_result.copyTo(gpu_result);
-		}
-
-		cv::Mat result_;
-		multiply(cpu_result, gpu_result, result_);
-		int result = cv::countNonZero(result_ > 0);
-		if (cpu_area != 0 && result != 0)
-			final_test_result = 1.0 - (double) result / (double) cpu_area;
-		else if (cpu_area == 0 && result != 0)
-			final_test_result = -1;
-	}
-	return final_test_result;
-}
-
 #endif /* APP_H_ */
