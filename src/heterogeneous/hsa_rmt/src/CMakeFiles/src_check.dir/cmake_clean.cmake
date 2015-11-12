abb968973dff4edce25ce6dab8c0ecfbbcc81db2
Vinicius Fratin <viniciusfratin@github.com>
2015-11-12 11:09:21 +0000

    removed more cmake trash

--- src/heterogeneous/hsa_rmt/src/CMakeFiles/src_check.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/src_check"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/src_check.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
