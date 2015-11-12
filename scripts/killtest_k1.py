da22f842ce72071edc3399272a3b6dacfb59703a
fernandoFernandeSantos <f_157@hotmail.com>
2015-11-12 20:37:03 +0000

    killtest k1 fixed

--- scripts/killtest_k1.py
+++ scripts/killtest_k1.py
@@ -20,25 +20,8 @@ from datetime import datetime
 # ]
 # will execute lavaMD for about one hour and then execute gemm for two hours
 # When the list finish executing, it start to execute from the beginning
-commandList = [ 
-#hog
-[ "sudo "+bin_path+"/hog "+data_path+"/1x_pedestrians.jpg --dst_data "+data_path+"/GOLD_1x.data --iterations 10000000 &", 1, "hog"],
-[ "sudo "+bin_path+"/hog "+data_path+"/4x_pedestrians.jpg --dst_data "+data_path+"/GOLD_4x.data --iterations 10000000 &", 1, "hog"],
-[ "sudo "+bin_path+"/hog "+data_path+"/9x_pedestrians.jpg --dst_data "+data_path+"/GOLD_9x.data --iterations 10000000 &", 1, "hog"],
-#cuda gemm
-[ "sudo "+bin_path+"/cudaGEMM -size=2048 -input_a="+data_path+"/Double_A_2048.matrix -input_b="+data_path+"/Double_B_2048.matrix -gold="+data_path+"/GOLD_2048 -iterations=10000000 &", 1, "cudaGEMM" ],
-[ "sudo "+bin_path+"/cudaGEMM -size=512 -input_a="+data_path+"/Double_A_512.matrix -input_b="+data_path+"/Double_B_512.matrix -gold="+data_path+"/GOLD_512 -iterations=10000000 &", 1, "cudaGEMM"],
-[ "sudo "+bin_path+"/cudaGEMM -size=1024 -input_a="+data_path+"/Double_A_1024.matrix -input_b="+data_path+"/Double_B_1024.matrix -gold="+data_path+"/GOLD_1024 -iterations=10000000 &", 1, "cudaGEMM"],
-#hotspot
-[ "sudo "+bin_path+"/hotspot -size=1024 -temp_file="+data_path+"/temp_1024 -power_file="+data_path+"/power_1024 -gold_file="+data_path+"/GOLD_1024 -iterations=10000000 &", 1, "hotspot"],
-#lava
-[ "sudo "+bin_path+"/lava -boxes=6 -input_distances="+data_path+"/input_distances_6 -input_charges="+data_path+"/input_charges_6 -output_gold="+data_path+"/GOLD_6 -iterations=10000000 -streams=1 &", 1, "lava"], 
-[ "sudo "+bin_path+"/lava -boxes=7 -input_distances="+data_path+"/input_distances_7 -input_charges="+data_path+"/input_charges_7 -output_gold="+data_path+"/GOLD_7 -iterations=10000000 -streams=1 &", 1, "lava"],
-[ "sudo "+bin_path+"/lava -boxes=5 -input_distances="+data_path+"/input_distances_5 -input_charges="+data_path+"/input_charges_5 -output_gold="+data_path+"/GOLD_5 -iterations=10000000 -streams=1 &", 1, "lava"],
-#clamr
-[ "sudo "+installDir+"/bin/run_clamr.sh big 8", 1, "clamr_openmponly" ],
-[ "sudo "+installDir+"/bin/run_clamr.sh little 2", 1, "clamr_openmponly" ] 
-];
+data_path = "";
+bin_path  = "";
 
 # Command used to kill application
 killcmd="killall -9 "
@@ -199,9 +182,36 @@ try:
 except IOError as e:
 	print >> sys.stderr, "Configuration setup error: "+str(e)
 	sys.exit(1)
-	
+
+data_path = installDir + "data";
+bin_path  = installDir + "bin";
 logFile = logDir+"killtest.log"
 timestampFile = varDir+"timestamp.txt"
+
+#run everything ###################################3
+commandList = [ 
+#hog
+[ "sudo "+bin_path+"/hog "+data_path+"/histogram_ori_gradients/1x_pedestrians.jpg --dst_data "+data_path+"/histogram_ori_gradients/GOLD_1x.data --iterations 10000000 &", 1, "hog"],
+[ "sudo "+bin_path+"/hog "+data_path+"/histogram_ori_gradients/4x_pedestrians.jpg --dst_data "+data_path+"/histogram_ori_gradients/GOLD_4x.data --iterations 10000000 &", 1, "hog"],
+[ "sudo "+bin_path+"/hog "+data_path+"/histogram_ori_gradients/9x_pedestrians.jpg --dst_data "+data_path+"/histogram_ori_gradients/GOLD_9x.data --iterations 10000000 &", 1, "hog"],
+#cuda gemm
+[ "sudo "+bin_path+"/cudaGEMM -size=2048 -input_a="+data_path+"/Double_A_2048.matrix -input_b="+data_path+"/Double_B_2048.matrix -gold="+data_path+"/GOLD_2048 -iterations=10000000 &", 1, "cudaGEMM" ],
+[ "sudo "+bin_path+"/cudaGEMM -size=512 -input_a="+data_path+"/Double_A_512.matrix -input_b="+data_path+"/Double_B_512.matrix -gold="+data_path+"/GOLD_512 -iterations=10000000 &", 1, "cudaGEMM"],
+[ "sudo "+bin_path+"/cudaGEMM -size=1024 -input_a="+data_path+"/Double_A_1024.matrix -input_b="+data_path+"/Double_B_1024.matrix -gold="+data_path+"/GOLD_1024 -iterations=10000000 &", 1, "cudaGEMM"],
+#hotspot
+[ "sudo "+bin_path+"/hotspot -size=1024 -temp_file="+data_path+"/temp_1024 -power_file="+data_path+"/power_1024 -gold_file="+data_path+"/GOLD_1024 -iterations=10000000 &", 1, "hotspot"],
+#lava
+[ "sudo "+bin_path+"/lava -boxes=6 -input_distances="+data_path+"/input_distances_6 -input_charges="+data_path+"/input_charges_6 -output_gold="+data_path+"/GOLD_6 -iterations=10000000 -streams=1 &", 1, "lava"], 
+[ "sudo "+bin_path+"/lava -boxes=7 -input_distances="+data_path+"/input_distances_7 -input_charges="+data_path+"/input_charges_7 -output_gold="+data_path+"/GOLD_7 -iterations=10000000 -streams=1 &", 1, "lava"],
+[ "sudo "+bin_path+"/lava -boxes=5 -input_distances="+data_path+"/input_distances_5 -input_charges="+data_path+"/input_charges_5 -output_gold="+data_path+"/GOLD_5 -iterations=10000000 -streams=1 &", 1, "lava"],
+#clamr
+[ "sudo "+bin_path+"/run_clamr.sh big 8", 1, "clamr_openmponly" ],
+[ "sudo "+bin_path+"/run_clamr.sh little 2", 1, "clamr_openmponly" ] 
+];
+
+####################################################
+
+
 
 # Start last kill timestamo with an old enough timestamp
 lastKillTimestamp = int(time.time()) - 50*timestampMaxDiff
