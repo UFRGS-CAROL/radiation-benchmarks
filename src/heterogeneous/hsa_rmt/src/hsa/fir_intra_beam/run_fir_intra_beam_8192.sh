0f778f5297c82381e2aa7afd37eda24d01d4afb0
Vinicius Fratin <viniciusfratin@github.com>
2015-11-12 12:44:31 +0000

    scripts hsa rmt ok

---
+++ src/heterogeneous/hsa_rmt/src/hsa/fir_intra_beam/run_fir_intra_beam_8192.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while ((1))
+do
+        cd /home/carol/vinicius/radiation-benchmarks/bin/fir_intra_beam; sudo /home/carol/vinicius/radiation-benchmarks/bin/fir_intra_beam/fir_intra_beam -b 8192 -n 8192
+done
---
+++ src/heterogeneous/hsa_rmt/src/hsa/page_rank_intra_beam/run_page_rank_intra_beam_4096.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while ((1))
+do
+        cd /home/carol/vinicius/radiation-benchmarks/bin/page_rank_intra_beam; sudo /home/carol/vinicius/radiation-benchmarks/bin/page_rank_intra_beam/page_rank_intra_beam -i input/csr_4096_10.txt
+done
