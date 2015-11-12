29b753f014f515997dc70f6d36de95a2ecc7f5f0
Vinicius Fratin <viniciusfratin@github.com>
2015-11-12 11:51:33 +0000

    removed cmake trash

--- src/heterogeneous/hsa_rmt/src/hsa/kmeans_intra_beam/CMakeFiles/kmeans_inter_beam_check.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/kmeans_inter_beam_check"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/kmeans_inter_beam_check.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- src/heterogeneous/hsa_rmt/src/hsa/kmeans_intra_beam/CMakeFiles/kmeans_intra_beam_check.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/kmeans_intra_beam_check"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/kmeans_intra_beam_check.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
