b05b65b826ce4b1ead2ba75e399e5d291c120a17
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2016-07-11 02:17:26 +0100

    Generate

--- src/cuda/nw/needle_generate.cu
+++ src/cuda/nw/needle_generate.cu
@@ -14,11 +14,6 @@
 // includes, kernels
 #include "needle_kernel.cu"
 
-//================== log include
-#ifdef LOGS
-#include "log_helper.h"
-#endif
-//====================================
 
 ///////////////////////////////////////////////////////////////////////////////
 // declaration, forward
@@ -87,37 +82,34 @@ double mysecond() {
 	return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
 }
 
-void ReadArrayFromFile(int* input_itemsets, int* gold_itemsets, char** argv, std::string array, std::string gold) {
+void ReadArrayFromFile(int* input_itemsets,  char** argv, std::string array = "") {
 	double time = mysecond();
 	int n = atoi(argv[1]) + 1;
 
 	//"/home/carol/TestGPU/GenerateNeedleArray/NeedleInput_"
-	std::string filenameinput(array);
-	filenameinput += argv[1];
-	filenameinput += ".array";
-	//"/home/carol/TestGPU/GenerateNeedleArray/NeedleGOLD_"
-	std::string filenamegold(gold);
-	filenamegold += argv[1];
-	filenamegold += ".array";
-
-	std::cout << "open array...";
-
-	FILE *f_a, *f_gold;
-	f_a = fopen(filenameinput.c_str(), "rb");
-	f_gold = fopen(filenamegold.c_str(), "rb");
-
-	if ((f_a == NULL) || (f_gold == NULL)) {
-		std::cout << "error.\n";
-		exit(-3);
+	if(array == ""){
+		std::cout << "Input array path is null so I'm generating a random array  with size == " << n * n << endl;
+		for(int i = 0; i < n * n; i++){
+			input_itemsets[i] = rand() % 100000 + 1;
+		}
+		FILE *f_a;
+		std::string filenameinput(array);
+		filenameinput += std::string("input_") + argv[1];
+		f_a = fopen(filenameinput.c_str(), "wb");
+		fwrite(input_itemsets, sizeof(int) * n * n, 1, f_a);
+		fclose(f_a);
+	}else{
+		FILE *f_a;
+		std::string filenameinput(argv[1]);
+		f_a = fopen(filenameinput.c_str(), "rb");
+		if (f_a == NULL) {
+			std::cout << "error.\n";
+			exit(-3);
+		}
+		std::cout << "read...";
+		fread(input_itemsets, sizeof(int) * n * n, 1, f_a);
+		fclose(f_a);
 	}
-
-	std::cout << "read...";
-	fread(input_itemsets, sizeof(int) * n * n, 1, f_a);
-	fread(gold_itemsets, sizeof(int) * n * n, 1, f_gold);
-	fclose(f_a);
-	fclose(f_gold);
-
-	printf("ok in %f\n", mysecond() - time);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -157,16 +149,15 @@ void runTest(int argc, char** argv) {
 	int size;
 	int zero = 0;
 	double timeG;
-	std::string array_path, gold_path;
+	std::string array_path;
 
 	// the lengths of the two sequences should be able to divided by 16.
 	// And at current stage  max_rows needs to equal max_cols
-	if (argc == 5) {
+	if (argc == 4) {
 		max_rows = atoi(argv[1]);
 		max_cols = atoi(argv[1]);
 		penalty = atoi(argv[2]);
 		array_path = std::string(argv[3]);
-		gold_path =  std::string(argv[4]);
 	} else {
 		usage(argc, argv);
 	}
@@ -185,87 +176,30 @@ void runTest(int argc, char** argv) {
 	dim3 gchk_dimGrid(gchk_gridsize, gchk_gridsize);
 	////////////////////////////////////////////////////
 
-	// Log files
-	/*FILE* file;
-	 FILE* log_file;
-	 */
-	//================== Init logs
-#ifdef LOGS
-	char test_info[90];
-	snprintf(test_info, 90, "max_rows:%d max_cols:%d penalty:%d", max_rows, max_cols, penalty);
-	start_log_file("cudaNW", test_info);
-#endif
 	//====================================
 	int ea = 0; //wrong integers in the current loop
 	int t_ea = 0; //total number of wrong integers
 	int old_ea = 0;
 
 	double total_time = 0.0;
-	////////////////FILE NAME//////////////////////////////
-	/*time_t file_time;
-	 struct tm *ptm;
-	 char day[2], month[2], year[4], hour[2], second[2], minute[2];
-	 char file_name[60];
-	 char file_name_log[60];
 
-	 file_time = time(NULL);
-	 ptm = gmtime(&file_time);
-
-	 std::string logfile_matrixname("cudaNW");
-	 logfile_matrixname += argv[1];
-
-	 snprintf(day, sizeof(day + 1), "%d", ptm->tm_mday);
-	 snprintf(month, sizeof(month + 1), "%d", ptm->tm_mon + 1);
-	 snprintf(year, sizeof(year + 1), "%d", ptm->tm_year + 1900);
-	 snprintf(hour, sizeof(hour + 1), "%d", ptm->tm_hour);
-	 snprintf(minute, sizeof(minute + 1), "%d", ptm->tm_min);
-	 snprintf(second, sizeof(second + 1), "%d", ptm->tm_sec);
-	 strcpy(file_name, day);
-	 strcat(file_name, "_");
-	 strcat(file_name, month);
-	 strcat(file_name, "_");
-	 strcat(file_name, year);
-	 strcat(file_name, "_");
-	 strcat(file_name, hour);
-	 strcat(file_name, "_");
-	 strcat(file_name, minute);
-	 strcat(file_name, "_");
-	 strcat(file_name, second);
-	 strcat(file_name, "_");
-	 strcat(file_name, logfile_matrixname.c_str());
-	 strcpy(file_name_log, file_name);
-
-	 strcat(file_name, ".txt");
-	 strcat(file_name_log, "log.txt");
-	 *///////////////////////////////////////////////////////
 	max_rows = max_rows + 1;
 	max_cols = max_cols + 1;
 	referrence = (int *) malloc(max_rows * max_cols * sizeof(int));
 	input_itemsets = (int *) malloc(max_rows * max_cols * sizeof(int));
 	output_itemsets = (int *) malloc(max_rows * max_cols * sizeof(int));
-	gold_itemsets = (int *) malloc(max_rows * max_cols * sizeof(int));
+	//gold_itemsets = (int *) malloc(max_rows * max_cols * sizeof(int));
 
-	int *kerrors;
-	kerrors = (int*) malloc(sizeof(int));
+	//int *kerrors;
+	//kerrors = (int*) malloc(sizeof(int));
 
 	if (!input_itemsets)
 		fprintf(stderr, "error: can not allocate memory");
 
 	printf("Start Needleman-Wunsch\n");
 
-	ReadArrayFromFile(input_itemsets, gold_itemsets, argv, array_path, gold_path);
+	ReadArrayFromFile(input_itemsets, argv, array_path);
 
-	/*    	srand ( time(NULL) );
-	 std::cout << "Original -  input : " << input_itemsets[1*max_cols+0] << "\treference : " << blosum62[input_itemsets[1*max_cols]][input_itemsets[1]] << "\n";
-	 input_itemsets[1*max_cols+0] = rand () % 10 + 1;
-	 std::cout << "Modified -  input : " << input_itemsets[1*max_cols+0] << "\treference : " << blosum62[input_itemsets[1*max_cols]][input_itemsets[1]] << "\n";
-	 */
-	/*for (int i = 0 ; i < max_cols; i++)
-	 {
-	 //getchar();
-	 for (int j = 0 ; j < max_rows; j++)
-	 std::cout << "[" << i << "][" << j << "] : " << input_itemsets[i*max_cols+j] << "\t";
-	 }*/
 	for (int i = 1; i < max_cols; i++) {
 		for (int j = 1; j < max_rows; j++) {
 			referrence[i * max_cols + j] =
@@ -306,9 +240,7 @@ void runTest(int argc, char** argv) {
 		int block_width = (max_cols - 1) / BLOCK_SIZE;
 
 		timeG = mysecond();
-#ifdef LOGS
-		start_iteration();
-#endif
+
 		//printf("Processing top-left matrix\n");
 		//process top-left matrix
 		for (int i = 1; i <= block_width; i++) {
@@ -325,15 +257,18 @@ void runTest(int argc, char** argv) {
 			needle_cuda_shared_2<<<dimGrid, dimBlock>>>(referrence_cuda,
 					matrix_cuda, max_cols, penalty, i, block_width);
 		}
-#ifdef LOGS
-		end_iteration();
-#endif
+
 		timeG = mysecond() - timeG;
 		total_time += timeG;
 
-		//std::cout << "Done in " << timeG << "s.\n";
+		std::cout << "Done in " << timeG << "s.\n Saving gold";
+		FILE *f_a;
+		std::string gold_name("gold_" + array_path);
+		f_a = fopen(gold_name.c_str(), "wb");
+		fwrite(input_itemsets, sizeof(int) * n * n, 1, f_a);
+		fclose(f_a);
 
-		*kerrors = 0;
+		/*kerrors = 0;
 		// Check errors on GPU...
 		//std::cout << "Sending gold matrix to GPU...";
 		timeG = mysecond();
@@ -353,35 +288,11 @@ void runTest(int argc, char** argv) {
 		gpuErrchk(cudaPeekAtLastError());
 		cudaMemcpyFromSymbol(kerrors, gpukerrors, sizeof(unsigned int));
 
-		//std::cout << "Errors : " << *kerrors << "\n";
-
-		///////////UPDATE FILE//////////////////////
-		/*file_time = time(NULL);
-		 ptm = gmtime(&file_time);
-		 snprintf(hour, sizeof(hour + 1), "%d", ptm->tm_hour);
-		 snprintf(minute, sizeof(minute + 1), "%d", ptm->tm_min);
-		 snprintf(second, sizeof(second + 1), "%d", ptm->tm_sec);
-		 fprintf(file, "\n start time: %s/%s_%s:%s:%s", day, month, hour, minute,
-		 second);
-		 fclose (file);
-		 */
 
 		ea = 0;
 
-		/////////////UPDATE TIMESTAMP///////////////////
-		//UpdateTimestamp();
-		////////////////////////////////////////////////
-
 		if (*kerrors > 0) {
-			//file = fopen(file_name, "a");
-			//std::cout <<  << *kerrors << "\n";
-#ifdef LOGS
-			char error_info[100];
 
-			sprintf(error_info, "Error detected! kerrors = %d" ,*kerrors);
-			log_error_detail(error_info);
-			int host_errors =0;
-#endif
 			cudaMemcpy(output_itemsets, matrix_cuda, sizeof(int) * size,
 					cudaMemcpyDeviceToHost);
 			for (int i = 0; (i < n) && (ea < N_ERRORS_LOG); i++) {
@@ -394,29 +305,12 @@ void runTest(int argc, char** argv) {
 								"\n p: [%d, %d], r: %i, e: %i, error: %d\n", i,
 								j, output_itemsets[i + n * j],
 								gold_itemsets[i + n * j], ea);
-#ifdef LOGS
-						log_error_detail(error_detail);
-						host_errors++;
-#endif
 
 					}
 				}
 			}
 			t_ea += *kerrors;
 
-			///////////UPDATE LOG FILE//////////////////////
-			/*log_file = fopen(file_name_log, "a");
-			fprintf(log_file, "\ntest number: %d", loop2);
-			fprintf(log_file, "\ntime: %f", timeG);
-			fprintf(log_file, "\ntotal time: %f", total_time);
-			fprintf(log_file, "\nerrors: %d", *kerrors);
-			fprintf(log_file, "\ntotal errors: %d", t_ea);
-			fclose (log_file);
-			fclose (file);
-			*/
-#ifdef LOGS
-			log_error_count(host_errors);
-#endif
 		}
 
 		if (*kerrors > 0 || (loop2 % 10 == 0)) {
@@ -432,13 +326,11 @@ void runTest(int argc, char** argv) {
 			old_ea = *kerrors;
 		} else {
 			printf(".");
-		}
+		}*/
 		cudaFree(referrence_cuda);
 		cudaFree(matrix_cuda);
 	}
-#ifdef LOGS
-	end_log_file();
-#endif
+
 	free(referrence);
 	free(input_itemsets);
 	free(output_itemsets);
