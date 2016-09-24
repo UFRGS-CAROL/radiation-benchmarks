403e9df056afba9e2cf2f571868ed2561ac5b54a
reliabilitylogdata <reliabilitylogdata@github.com>
2016-09-24 16:39:13 -0300

    fixing big problem

--- src/cuda/darknet/src/yolo.c
+++ src/cuda/darknet/src/yolo.c
@@ -150,7 +150,7 @@ void validate_yolo(char *cfgfile, char *weightfile)
     char *base = "results/comp4_det_test_";
     //list *plist = get_paths("data/voc.2007.test");
     //list *plist = get_paths("/home/pjreddie/data/voc/2007_test.txt");
-    list *plist = get_paths("data/voc.2012.test");
+    list *plist = get_paths("voc.2012.test");
     char **paths = (char **)list_to_array(plist);
 
     layer l = net.layers[net.n-1];
