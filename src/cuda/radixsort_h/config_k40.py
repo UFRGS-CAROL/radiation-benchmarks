2c0792a49e497253b9ba87e3951aad845b547b6c
Caio Lunardi <caio.b.lunardi@gmail.com>
2016-09-28 14:35:56 -0300

    Configs for sorts, ready for tests maybe

--- src/cuda/radixsort_h/config_k40.py
+++ src/cuda/radixsort_h/config_k40.py
@@ -20,9 +20,9 @@ except IOError as e:
 	print >> sys.stderr, "Configuration setup error: "+str(e)
 	sys.exit(1)
 
-data_path=installDir+"data/radixsort"
+data_path=installDir+"data/radixsort_h"
 bin_path=installDir+"bin"
-src_radixsort = installDir+"src/cuda/radixsort"
+src_radixsort = installDir+"src/cuda/radixsort_h"
 
 if not os.path.isdir(data_path):
 	os.mkdir(data_path, 0777);
@@ -30,19 +30,20 @@ if not os.path.isdir(data_path):
 
 os.system("cd "+src_radixsort)
 
-os.system("sudo ./radixsort -size=1048576 -input=radixsort_input_134217728 -gold=radixsort_gold_1048576 -generate -iterations=1")
-os.system("sudo ./radixsort -size=33554432 -input=radixsort_input_134217728 -gold=radixsort_gold_33554432 -generate -iterations=1")
-os.system("sudo ./radixsort -size=67108864 -input=radixsort_input_134217728 -gold=radixsort_gold_67104464 -generate -iterations=1")
-os.system("sudo ./radixsort -size=134217728 -input=radixsort_input_134217728 -gold=radixsort_gold_134217728 -generate -iterations=1")
+os.system("make logs=-DLOGS")
+
+os.system("sudo ./radixsort_h -size=1048576 -input=radixsort_input_134217728 -gold=radixsort_gold_1048576 -generate -iterations=1")
+os.system("sudo ./radixsort_h -size=33554432 -input=radixsort_input_134217728 -gold=radixsort_gold_33554432 -generate -iterations=1")
+os.system("sudo ./radixsort_h -size=134217728 -input=radixsort_input_134217728 -gold=radixsort_gold_134217728 -generate -iterations=1")
 os.system("sudo chmod 777 radixsort_*");
-os.system("mv radixsort_* "+data_path);
-os.system("mv ./radixsort "+bin_path)
+os.system("mv radixsort_gold_* "+data_path);
+os.system("mv radixsort_input_* "+data_path);
+os.system("mv ./radixsort_h "+bin_path)
 
 fp = open(installDir+"scripts/how_to_run_radixsort_cuda_K40", 'w')
-print >>fp, "sudo "+bin_path+"/radixsort -size=1048576 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_1048576 -iterations=10000000"
-print >>fp, "sudo "+bin_path+"/radixsort -size=33554432 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_33554432 -iterations=10000000"
-print >>fp, "sudo "+bin_path+"/radixsort -size=67108864 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_67108864 -iterations=10000000"
-print >>fp, "sudo "+bin_path+"/radixsort -size=134217728 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_134217728 -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/radixsort_h -size=1048576 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_1048576 -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/radixsort_h -size=33554432 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_33554432 -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/radixsor_ht -size=134217728 -input="+data_path+"/radixsort_input_134217728 -gold="+data_path+"/radixsort_gold_134217728 -iterations=10000000"
 
 print "\nConfiguring done, to run check file: "+installDir+"scripts/how_to_run_radixsort_cuda\n"
 
