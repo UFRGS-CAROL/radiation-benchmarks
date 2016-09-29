0ac93a55633b6b89476ec5854ca268a036fecd4b
reliabilitylogdata <reliabilitylogdata@github.com>
2016-09-29 05:01:44 -0300

    pyfaster

--- src/cuda/py-faster-rcnn/tools/py_faster_rcnn.py
+++ src/cuda/py-faster-rcnn/tools/py_faster_rcnn.py
@@ -166,33 +166,41 @@ def compare(gold, current, img_name):
     scores_curr = current[0]
     boxes_curr = current[1]
 
-    min_m_range = scores_m_gold = len(scores_gold)
-    scores_m_curr = len(scores_curr)
-    #diff size
-    size_error_m = abs(scores_m_gold - scores_m_curr)
-    if size_error_m != 0:
-        min_m_range = min(scores_m_gold, scores_m_curr)
-        lh.log_error_detail("score_missing_lines: " + size_error_m)
-        error_count += size_error_m
-        
-    #compare scores
-    for i in range(1,min_m_range):
-        min_n_range = scores_n_gold = len(scores_gold[i])
-        scores_n_curr = len(scores_curr[i])
-        size_error_n = abs(scores_n_gold - scores_n_curr)
-        if size_error_n != 0:
-            min_n_range = min(scores_n_gold, scores_n_curr)
-            lh.log_error_detail("score_missing_collumns: " + size_error_n + " line: " + i)
-            error_count += size_error_n
+    # min_m_range = scores_m_gold = len(scores_gold)
+    # scores_m_curr = len(scores_curr)
+    # #diff size
+    # size_error_m = abs(scores_m_gold - scores_m_curr)
+    # if size_error_m != 0:
+    #     min_m_range = min(scores_m_gold, scores_m_curr)
+    #     lh.log_error_detail("score_missing_lines: " + size_error_m)
+    #     error_count += size_error_m
+    #
+    # #compare scores
+    # for i in range(1,min_m_range):
+    #     min_n_range = scores_n_gold = len(scores_gold[i])
+    #     scores_n_curr = len(scores_curr[i])
+    #     size_error_n = abs(scores_n_gold - scores_n_curr)
+    #     if size_error_n != 0:
+    #         min_n_range = min(scores_n_gold, scores_n_curr)
+    #         lh.log_error_detail("score_missing_collumns: " + size_error_n + " line: " + i)
+    #         error_count += size_error_n
+    #
+    #     for j in range(0, min_n_range):
+    #         gold_ij = float(scores_gold[i][j])
+    #         curr_ij = float(scores_curr[i][j])
+    #         diff = math.fabs(gold_ij -  curr_ij)
+    #         if diff > THRESHOLD:
+    #             error_detail = "scores: [" + str(i) + "," + str(j) + "] e: " +  str(gold_ij) + " r: " + str(curr_ij)
+    #             error_count += 1
+    #             lh.log_error_detail(error_detail)
 
-        for j in range(0, min_n_range):
-            gold_ij = float(scores_gold[i][j])
-            curr_ij = float(scores_curr[i][j])
-            diff = math.fabs(gold_ij -  curr_ij)
-            if diff > THRESHOLD:
-                error_detail = "scores: [" + str(i) + "," + str(j) + "] e: " +  str(gold_ij) + " r: " + str(curr_ij)
-                error_count += 1
-                lh.log_error_detail(error_detail)
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+        cls_ind += 1  # because we skipped background
+        # cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
+        cls_scores_current = scores_curr[:, cls_ind]
+        cls_scores_gold = scores_gold[:, cls_ind]
+        print cls_scores_current
+        print cls_scores_gold
 
     #compare boxes #####################################################         
     min_m_range = boxes_m_gold = len(boxes_gold)
