1359f4799620240aba76c57421530461c5d13fcb
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-16 01:35:54 -0200

    abft almost done

--- src/cuda/darknet/src/abft.h
+++ src/cuda/darknet/src/abft.h
@@ -11,12 +11,14 @@
 #define MAX_THRESHOLD  0.0001
 #define BLOCK_SIZE 1024
 
-void abraham_sum(float *a, float *b, long rows_a, long cols_a, long rows_b, long cols_b);
-void abraham_check(float *c, long rows, long cols);
-
 typedef struct erro_return{
 	unsigned long long int row_detected_errors;
 	unsigned long long int col_detected_errors;
 }ErrorReturn;
+
+void abraham_sum(float *a, float *b, long rows_a, long cols_a, long rows_b, long cols_b);
+ErrorReturn abraham_check(float *c, long rows, long cols);
+
+
 
 #endif /* ABFT_H_ */
