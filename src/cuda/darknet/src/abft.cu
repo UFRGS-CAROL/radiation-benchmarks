22300f248c23234d3cda639aa76e4107ebe6b89f
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-17 02:32:30 -0200

    fixing abft

--- src/cuda/darknet/src/abft.cu
+++ src/cuda/darknet/src/abft.cu
@@ -44,20 +44,22 @@ __global__ void check_row(float *mat, long rows, long cols) {
 	long j = blockIdx.x * blockDim.x + threadIdx.x;
 
 	long k;
-	double acc = 0;
+	float acc = 0;
 	//must be less one
 
 	for (k = 0; k < rows - 1; k++) {
-		acc += mat[k * cols + j] / rows;
+		acc += (mat[k * cols + j] / float(rows));
 	}
 	float last_one = mat[(k + 1) * cols + j];
-	//printf("a_index %ld acc %lf \n", rows_a * cols_a + j, acc);
+
+
 	long a_index = (rows - 1) * cols + j;
-	float diff = fabs((fabs(mat[a_index]) / rows) - fabs(acc));
+	float pos = (fabs(mat[a_index]) / float(rows));
+	float diff = fabs(pos - fabs(acc));
 	if (diff >= MAX_THRESHOLD) {
 		atomicAdd(&err_count.row_detected_errors, 1);
 		printf("passou no row mat[%ld] = %lf diff %lf last one %lf calc %lf i value %ld\n",
-				a_index, mat[a_index - 1], diff, last_one, acc, j);
+				a_index, pos, diff, last_one, acc, j);
 	}
 	//__syncthreads();
 }
