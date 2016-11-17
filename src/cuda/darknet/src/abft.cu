fa9efb14c8edfa8f71b3fe772170e1f9af0697ec
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-17 02:42:39 -0200

    fixing abft

--- src/cuda/darknet/src/abft.cu
+++ src/cuda/darknet/src/abft.cu
@@ -56,6 +56,7 @@ __global__ void check_row(float *mat, long rows, long cols) {
 	long a_index = (rows - 1) * cols + j;
 	float diff = fabs((fabs(mat[a_index]) / rows) - fabs(acc));
 	if (diff >= MAX_THRESHOLD) {
+		printf("rows %ld, cols %ld\n", rows, cols);
 		atomicAdd(&err_count.row_detected_errors, 1);
 		printf("passou no row mat[%ld] = %lf diff %lf last one %lf calc %lf i value %ld\n",
 				a_index, mat[a_index], diff, last_one, acc, j);
