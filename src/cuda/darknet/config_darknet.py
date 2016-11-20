31d7fca974b8922505199e2bccd0e76c22b1d668
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-20 03:52:39 -0200

    config, not tested yet

--- src/cuda/darknet/config_darknet.py
+++ src/cuda/darknet/config_darknet.py
@@ -4,6 +4,7 @@ import os
 import sys
 import ConfigParser
 import sys
+import copy
 
 def main(board):
 
@@ -32,88 +33,123 @@ def main(board):
         os.chmod(data_path, 0777);
 
 
-    execution_type = 'yolo'
-    execution_model = 'valid'
-    config_file = data_path + '/yolo.cfg'
-    weights = data_path + '/yolo.weights'
-    iterations = '1000' #it is not so much, since each dataset have at least 10k of images
-    base_caltech_out = src_darknet
-    base_voc_out = src_darknet
+#	./darknet -e yolo -m valid -c cfg/yolo.cfg -w data/yolo.weights -n 3 -d data/gold_voc2012.test
+# -l data/voc.2012.DEBUG.txt -b ~/radiation-benchmarks/src/cuda/darknet -x 0 -s 1 -a 1
+
+    gold = data_path + '/gold.voc.2012.2K.test'
+    txt_list = installDir + 'data/networks_img_list/voc.2012.2K.txt'
+    voc_gen_clean = {
+        'bin':[bin_path, "/darknet"],
+        'e':[' -e ', 'yolo'],                   #execution_type =
+        'm':[' -m ', 'valid'],                    #execution_model =
+        'c':[' -c ', data_path + '/yolo.cfg'],    #config_file =
+        'w':[' -w ', data_path + '/yolo.weights'],#weights =
+        'n':[' -n ', '1'],                     #iterations =  #it is not so much, since each dataset have at least 10k of images
+        'g':[' -g ', gold],                #base_caltech_out = base_voc_out = src_darknet
+        'l':[' -l ', txt_list],
+        'b':[' -b ', src_darknet],
+        'x':[' -x ', 0],
+        's':[' -s ', 0],
+        'a':[' -a ', 0],
+    }
+
+    voc_gen_abft = copy.deepcopy(voc_gen_clean)
+    gold_abft = data_path + '/gold.voc.2012.2K.abft.test'
+    voc_gen_abft['a'][1] = 1
+    voc_gen_abft['g'][1] = gold_abft
+
+    #caltech gen
+    gold = data_path + '/gold.caltech.2K.test'
+    txt_list = installDir + 'data/networks_img_list/caltech.pedestrians.2K.txt'
+    caltech_gen_clean = copy.deepcopy(voc_gen_clean)
+    caltech_gen_clean['l'][1] = txt_list
+    caltech_gen_clean['g'][1] = gold
+
+    gold_abft = data_path + '/gold.caltech.2K.abft.test'
+    caltech_gen_abft =copy.deepcopy(caltech_gen_clean)
+    caltech_gen_abft['a'][1] = 1
+    caltech_gen_abft['g'][1] = gold_abft
 
 
-    img_datasets = str.replace(data_path,'/darknet', '') + "/networks_img_list"
-    #all inputs
-    caltech_gold_FULL = data_path + '/gold_caltech_full.test'
-    caltech_img_list_FULL = img_datasets + '/caltech/'+board+'/caltech.pedestrians.1K.txt'
-    cal_FULL_str = caltech_gold_FULL + " -l " + caltech_img_list_FULL
-
-    #half of inputs
-    caltech_gold_HALF = data_path + '/gold_caltech_full.test'
-    caltech_img_list_HALF = img_datasets + '/caltech/'+board+'/caltech.pedestrians.5K.txt'
-    cal_HALF_str = caltech_gold_HALF + " -l " + caltech_img_list_HALF
-
-    #VOC
-    #all inputs
-    voc_gold_FULL = data_path + '/gold_voc_full.test'
-    voc_img_list_FULL = img_datasets + '/voc/'+board+'/voc.2012.1K.txt'
-    voc_FULL_str = voc_gold_FULL + " -l " + voc_img_list_FULL
-
-    #half of inputs
-    voc_gold_HALF = data_path + '/gold_voc_full.test'
-    voc_img_list_HALF = img_datasets + '/voc/'+board+'/voc.2012.5K.txt'
-    voc_HALF_str = voc_gold_HALF + " -l " + voc_img_list_HALF
-
-
-    #voc
-    vc_full_gen = "sudo "+bin_path+"/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 1 -g " + voc_FULL_str + " -b " + base_voc_out + " -x 0"
-
-
-    #caltech
-    cl_full_gen = "sudo "+bin_path+"/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 1 -g " + cal_FULL_str + " -b " + base_caltech_out + " -x 0"
 
     #execute################################################################
-    #voc
-    vc_half_ex =  "[\"sudo "+ bin_path + "/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 10000 -d " + voc_HALF_str + " -b " + base_voc_out + " -x 0 \" , 1, \"darknet\"],"
+    #caltech----------
+    #clean
+    caltech_exe_clean = copy.deepcopy(caltech_gen_clean)
+    caltech_exe_clean['n'][1] = 10000
+    caltech_exe_clean['g'][0] = ' -d '
+    #abft
+    caltech_exe_abft = copy.deepcopy(caltech_gen_abft)
+    caltech_exe_abft['n'][1] = 10000
+    caltech_exe_abft['g'][0] = ' -d '
+    #save layers
+    caltech_exe_save = copy.deepcopy(caltech_exe_clean)
+    caltech_exe_save['s'][1] = 1
 
-    vc_full_ex = "[\"sudo "+ bin_path + "/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 10000 -d " + voc_FULL_str + " -b " + base_voc_out + " -x 0 \" , 1, \"darknet\"],"
+    #voc---------------
+    #clean
+    voc_exe_clean = copy.deepcopy(voc_gen_clean)
+    voc_exe_clean['n'][1] = 10000
+    voc_exe_clean['g'][0] = ' -d '
+
+    #abft
+    voc_exe_abft = copy.deepcopy(voc_gen_abft)
+    voc_exe_abft['n'][1] = 10000
+    voc_exe_abft['g'][0] = ' -d '
+    #save layers
+    voc_exe_save = copy.deepcopy(voc_exe_clean)
+    voc_exe_save['s'][1] = 1
 
 
-    #caltech
-    cl_half_ex = "[\"sudo "+ bin_path + "/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 10000 -d " + cal_HALF_str + " -b " + base_caltech_out + " -x 0 \" , 1, \"darknet\"],"
+    # os.system("cd "+src_darknet)
 
-    cl_full_ex = "[\"sudo "+ bin_path + "/darknet -e " + execution_type + " -m " + execution_model + " -c " + \
-          config_file + " -w " +  weights + " -n 10000 -d " + cal_FULL_str + " -b " + base_caltech_out + " -x 0 \" , 1, \"darknet\"],"
+    make_clean = "make clean GPU=1"
 
-    os.system("cd "+src_darknet)
+    make_str = "make -j 4 GPU=1 "
+    if 'X1' in board:
+        make_str +="ARCH_I=53"
 
-    os.system("make clean")
-    os.system("make -j 4 GPU=1 ARCH_I=53")
+    os.system(make_clean)
+    os.system(make_str)
+
+
+
     os.system("mv ./darknet "+bin_path)
 
-    os.system(vc_full_gen)
-    os.system(cl_full_gen)
+    generate = []
+    generate.append(" ".join([''.join(map(str, value)) for key,value in caltech_gen_clean.iteritems()]))
+    generate.append(" ".join([''.join(map(str, value)) for key,value in  caltech_gen_abft.iteritems()]))
+    generate.append(" ".join([''.join(map(str, value)) for key,value in  voc_gen_clean.iteritems()]))
+    generate.append(" ".join([''.join(map(str, value)) for key,value in  voc_gen_abft.iteritems()]))
 
-    
-    os.system("make clean")
-    os.system("make -C ../../include/")
-    os.system("make -j 4 GPU=1 LOGS=1 ARCH_I=53")
+    execute = []
+    execute.append (" ".join([''.join(map(str, value))for key,value in caltech_exe_clean.iteritems()]))
+    execute.append (" ".join([''.join(map(str, value))for key,value in caltech_exe_abft.iteritems()]))
+    execute.append (" ".join([''.join(map(str, value))for key,value in caltech_exe_save.iteritems()]))
+
+    execute.append (" ".join([''.join(map(str, value))for key,value in voc_exe_clean.iteritems()]))
+    execute.append (" ".join([''.join(map(str, value))for key,value in voc_exe_abft.iteritems()]))
+    execute.append (" ".join([''.join(map(str, value))for key,value in voc_exe_save.iteritems()]))
+
+
+    for i in generate:
+        os.system(str(i))
+
+
+    make_str += " LOGS=1"
+
+    os.system(make_clean)
+    os.system("make -C ../../include/ cuda")
+    os.system(make_str)
+
 
     # os.system("sudo chmod 777 gold_* ");
     # os.system("mv gold_* "+data_path);
     os.system("mv ./darknet "+bin_path)
-
-
     fp = open(installDir+"scripts/how_to_run_darknet_cuda_"+board, 'w')
 
-    print >>fp, vc_half_ex
-    print >>fp, vc_full_ex
-    print >>fp, cl_half_ex
-    print >>fp, cl_full_ex
+    for i in execute:
+        print >>fp, str(i)
 
 
     print "\nConfiguring done, to run check file: "+installDir+"scripts/how_to_run_darknet_cuda_"+board+"\n"
