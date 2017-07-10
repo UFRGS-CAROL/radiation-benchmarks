e1034c0ba1a2fe7719f1f338ee91027f0544196b
reliabilitylogdata <reliabilitylogdata@github.com>
2017-07-10 00:19:29 -0300

    device

--- src/cuda/LeNetCUDA/src/DeviceVector.h
+++ src/cuda/LeNetCUDA/src/DeviceVector.h
@@ -22,8 +22,8 @@ private:
 	size_t v_size;
 
 	inline void memcopy(T* src, size_t size_count);
-	inline void free_memory();
-	inline void alloc_memory();
+//	inline void free_memory();
+//	inline void alloc_memory();
 
 public:
 	DeviceVector(size_t siz);
@@ -52,38 +52,40 @@ public:
 };
 
 template<class T>
-inline void DeviceVector<T>::free_memory() {
+//inline void DeviceVector<T>::free_memory() {
+inline void free_memory(T* device_data, bool *allocated){
 	CudaCheckError();
-	CudaSafeCall(cudaFree(this->device_data));
-	this->allocated = false;
+	CudaSafeCall(cudaFree(device_data));
+	*allocated = false;
 }
 
 template<class T>
-inline void DeviceVector<T>::alloc_memory() {
+//inline void DeviceVector<T>::alloc_memory() {
+inline void alloc_memory(T *device_data, size_t v_size, bool *allocated){
 	CudaSafeCall(
-			cudaMallocManaged(&this->device_data, sizeof(T) * this->v_size));
+			cudaMallocManaged(&device_data, sizeof(T) * v_size));
 	CudaCheckError();
-	this->allocated = true;
+	*allocated = true;
 }
 // Unified memory copy constructor allows pass-by-value
 template<class T>
 DeviceVector<T>::DeviceVector(const DeviceVector<T>& copy) {
 	if (this->allocated) {
-		this->free_memory();
+		free_memory(this->device_data, &this->allocated);
 	}
 
 	this->v_size = copy.v_size;
-	this->alloc_memory();
+	alloc_memory(this->device_data, this->v_size, &this->allocated);
 	this->memcopy(copy.device_data, this->v_size);
 }
 
 template<class T>
 DeviceVector<T>::DeviceVector(size_t siz) {
 	if (this->allocated) {
-		this->free_memory();
+		free_memory(this->device_data, &this->allocated);
 	}
 	this->v_size = siz;
-	this->alloc_memory();
+	alloc_memory(this->device_data, this->v_size, &this->allocated);
 }
 
 template<class T>
@@ -96,7 +98,7 @@ DeviceVector<T>::DeviceVector() {
 template<class T>
 DeviceVector<T>::~DeviceVector() {
 	if (this->allocated) {
-		this->free_memory();
+		free_memory(this->device_data, &this->allocated);
 		CudaCheckError();
 		this->device_data = nullptr;
 		this->v_size = 0;
@@ -107,11 +109,11 @@ DeviceVector<T>::~DeviceVector() {
 template<class T>
 DeviceVector<T>::DeviceVector(T *data, size_t siz) {
 	if (this->allocated) {
-		this->free_memory();
+		free_memory(this->device_data, &this->allocated);
 	}
 
 	this->v_size = siz;
-	this->alloc_memory();
+	alloc_memory(this->device_data, this->v_size, &this->allocated);
 	this->memcopy(data, siz);
 }
 
@@ -121,11 +123,11 @@ DeviceVector<T>& DeviceVector<T>::operator=(const DeviceVector<T>& other) {
 		T *data = (T*) other.device_data;
 
 		if (this->allocated) {
-			this->free_memory();
+			free_memory(this->device_data, &this->allocated);
 		}
 
 		this->v_size = other.v_size;
-		this->alloc_memory();
+		alloc_memory(this->device_data, this->v_size, &this->allocated);
 		this->memcopy(data, other.v_size);
 	}
 
@@ -139,11 +141,11 @@ DeviceVector<T>& DeviceVector<T>::operator=(const std::vector<T>& other) {
 		size_t siz = other.size();
 
 		if (this->allocated) {
-			this->free_memory();
+			free_memory(this->device_data, &this->allocated);
 		}
 
 		this->v_size = siz;
-		this->alloc_memory();
+		alloc_memory(this->device_data, this->v_size, &this->allocated);
 		this->memcopy(data, siz);
 	}
 	return *this;
@@ -153,10 +155,10 @@ template<class T>
 void DeviceVector<T>::resize(size_t siz) {
 	if (this->v_size != siz) {
 		if (this->v_size != 0) {
-			this->free_memory();
+			free_memory(this->device_data, &this->allocated);
 		}
 		this->v_size = siz;
-		this->alloc_memory();
+		alloc_memory(this->device_data, this->v_size, &this->allocated);
 		this->allocated = true;
 	}
 }
