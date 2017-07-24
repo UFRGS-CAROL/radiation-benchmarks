ae5191d8c2cd3e5ea09f635adb48e080190bf74b
Fernando <fernandofernandesantos@gmail.com>
2017-07-24 16:00:19 -0300

    removing function declaration

--- src/cuda/LeNetCUDA/src/LogsProcessing.cpp
+++ src/cuda/LeNetCUDA/src/LogsProcessing.cpp
@@ -10,44 +10,8 @@
 size_t error_count = 0;
 
 #ifdef LOGS
-
-#if defined(__cplusplus) && !defined(GPU)
-extern "C" {
-#endif
-
 #include "log_helper.h"
-
-// Return the name of the log file generated
-	char * get_log_file_name();
-
-// Generate the log file name, log info from user about the test
-// to be executed and reset log variables
-	int start_log_file(char *benchmark_name, char *test_info);
-
-// Log the string "#END" and reset global variables
-	int end_log_file();
-
-// Start time to measure kernel time, also update
-// iteration number and log to file
-	int start_iteration();
-
-// Finish the measured kernel time log both
-// time (total time and kernel time)
-	int end_iteration();
-
-// Update total errors variable and log both
-// errors(total errors and kernel errors)
-	int log_error_count(unsigned long int kernel_errors);
-
-// Print some string with the detail of an error to log file
-	int log_error_detail(char *string);
-
-// Print some string with the detail of an error/information to log file
-	int log_info_detail(char *string);
-
-#if defined(__cplusplus) && !defined(GPU)
-}
-#endif //extern C
+#endif //LOGS def
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
 	std::stringstream ss(s);
@@ -64,7 +28,7 @@ std::vector<std::string> split(const std::string &s, char delim) {
 	return elems;
 }
 
-#endif //LOGS def
+
 
 void start_count_app(char *test, char *app) {
 #ifdef LOGS
