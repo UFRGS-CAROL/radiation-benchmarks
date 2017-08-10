fc617d2df7bbde9dae3d46199e8337d4bf7f5b86
reliabilitylogdata <reliabilitylogdata@github.com>
2017-08-10 00:49:21 -0300

    config

--- src/cuda/darknet_v2/config_darknet_v2.py
+++ src/cuda/darknet_v2/config_darknet_v2.py
@@ -7,9 +7,14 @@ import copy
 
 DATASETS = [
     # normal
-    {'txt': 'caltech.pedestrians.1K.txt', 'gold': 'gold.caltech.1K.csv'},
-    {'txt': 'urban.street.1.1K.txt', 'gold': 'gold.urban.street.1.1K.csv'},
-    {'txt': 'voc.2012.1K.txt', 'gold': 'gold.voc.2012.abft.1K.csv'},
+    {'txt': 'caltech.pedestrians.1K.txt', 'gold': 'gold.caltech.1K.csv', 'mode': 'full'},
+    {'txt': 'urban.street.1.1K.txt', 'gold': 'gold.urban.street.1.1K.csv', 'mode': 'full'},
+    {'txt': 'voc.2012.1K.txt', 'gold': 'gold.voc.2012.1K.csv', 'mode': 'full'},
+
+    # small, for layers and x2
+    {'txt': 'caltech.pedestrians.100.txt', 'gold': 'gold.caltech.100.csv', 'mode': 'small'},
+    {'txt': 'urban.street.100.txt', 'gold': 'gold.urban.street.100.csv', 'mode': 'small'},
+    {'txt': 'voc.2012.100.txt', 'gold': 'gold.voc.2012.100.csv', 'mode': 'small'},
 ]
 
 
@@ -51,36 +56,41 @@ def main(board):
     execute = []
 
     # 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
-    for abft in [0, 2]:
-        for save_layer in [0, 1]:
-            for i in DATASETS:
-                gold = data_path + '/' + i['gold']
-                txt_list = installDir + 'data/networks_img_list/' + i['txt']
-                gen = {
-                    'bin': [bin_path, "/darknet_v2"],
-                    # 'e': [' -e ', 'yolo'],  # execution_type =
-                    'aa': ['test_radiation', ''],  # execution_model =
-                    'c': [' -c ', data_path + '/yolo_v2.cfg'],  # config_file =
-                    'w': [' -w ', data_path + '/yolo_v2.weights'],  # weights =
-                    'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
-                    'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
-                    'l': [' -l ', txt_list],
-                    # 'b': [' -b ', src_darknet],
-                    # 'x': [' -x ', 0],
-                    's': [' -s ', save_layer],
-                    'a': [' -a ', abft],
-                }
 
-                exe = copy.deepcopy(gen)
-                exe['n'][1] = 10000
-                exe['g'][0] = ' -d '
+    for save_layer in [0, 1]:
+        for abft in [0, 2]:
+                for i in DATASETS:
+                    if save_layer == 1 and i['mode'] == 'big':
+                        continue
+                    gold = data_path + '/' + i['gold']
+                    txt_list = installDir + 'data/networks_img_list/' + i['txt']
+                    gen = {
+                        'bin': [bin_path, "/darknet_v2"],
+                        # 'e': [' -e ', 'yolo'],  # execution_type =
+                        'aa': ['test_radiation', ''],  # execution_model =
+                        'c': [' -c ', data_path + '/yolo_v2.cfg'],  # config_file =
+                        'w': [' -w ', data_path + '/yolo_v2.weights'],  # weights =
+                        'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
+                        'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
+                        'l': [' -l ', txt_list],
+                        # 'b': [' -b ', src_darknet],
+                        # 'x': [' -x ', 0],
+                        's': [' -s ', save_layer],
+                        'a': [' -a ', abft],
+                    }
 
-                exe_save = copy.deepcopy(exe)
-                exe_save['s'][1] = 1
+                    exe = copy.deepcopy(gen)
+                    exe['n'][1] = 10000
+                    exe['g'][0] = ' -d '
 
-                generate.append(" ".join([''.join(map(str, gen[key])) for key in gen]))
-                execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
-                execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
+                    exe_save = copy.deepcopy(exe)
+                    exe_save['s'][1] = 1
+
+                    if abft == 0:
+                        generate.append(" ".join([''.join(map(str, gen[key])) for key in gen]))
+
+                    execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
+                    execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
 
     # end for generate
     generate.append("make clean GPU=1 ")
