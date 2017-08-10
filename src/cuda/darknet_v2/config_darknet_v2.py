f2cdf94f4aa582a8d4653f8109fd7c110e1b06cb
reliabilitylogdata <reliabilitylogdata@github.com>
2017-08-10 10:47:37 -0300

    config

--- src/cuda/darknet_v2/config_darknet_v2.py
+++ src/cuda/darknet_v2/config_darknet_v2.py
@@ -60,38 +60,37 @@ def main(board):
     for save_layer in [0, 1]:
         for abft in [0, 2]:
                 for i in DATASETS:
-                    if save_layer == 1 and i['mode'] == 'big':
+                    if save_layer == 1 and i['mode'] == 'full':
                         continue
-                    else:
-                        gold = data_path + '/' + i['gold']
-                        txt_list = installDir + 'data/networks_img_list/' + i['txt']
-                        gen = {
-                            'bin': [bin_path, "/darknet_v2"],
-                            # 'e': [' -e ', 'yolo'],  # execution_type =
-                            'aa': ['test_radiation', ''],  # execution_model =
-                            'c': [' -c ', data_path + '/yolo_v2.cfg'],  # config_file =
-                            'w': [' -w ', data_path + '/yolo_v2.weights'],  # weights =
-                            'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
-                            'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
-                            'l': [' -l ', txt_list],
-                            # 'b': [' -b ', src_darknet],
-                            # 'x': [' -x ', 0],
-                            's': [' -s ', save_layer],
-                            'a': [' -a ', abft],
-                        }
+                    gold = data_path + '/' + i['gold']
+                    txt_list = installDir + 'data/networks_img_list/' + i['txt']
+                    gen = {
+                        'bin': [bin_path, "/darknet_v2"],
+                        # 'e': [' -e ', 'yolo'],  # execution_type =
+                        'aa': ['test_radiation', ''],  # execution_model =
+                        'c': [' -c ', data_path + '/yolo_v2.cfg'],  # config_file =
+                        'w': [' -w ', data_path + '/yolo_v2.weights'],  # weights =
+                        'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
+                        'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
+                        'l': [' -l ', txt_list],
+                        # 'b': [' -b ', src_darknet],
+                        # 'x': [' -x ', 0],
+                        's': [' -s ', save_layer],
+                        'a': [' -a ', abft],
+                    }
 
-                        exe = copy.deepcopy(gen)
-                        exe['n'][1] = 10000
-                        exe['g'][0] = ' -d '
+                    exe = copy.deepcopy(gen)
+                    exe['n'][1] = 10000
+                    exe['g'][0] = ' -d '
 
-                        exe_save = copy.deepcopy(exe)
-                        exe_save['s'][1] = 1
+                    exe_save = copy.deepcopy(exe)
+                    exe_save['s'][1] = 1
 
-                        if abft == 0:
-                            generate.append(" ".join([''.join(map(str, gen[key])) for key in gen]))
+                    if abft == 0:
+                        generate.append(" ".join([''.join(map(str, gen[key])) for key in gen]))
 
-                        execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
-                        execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
+                    execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
+                    execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
 
     # end for generate
     generate.append("make clean GPU=1 ")
@@ -100,10 +99,10 @@ def main(board):
     generate.append("mv ./darknet_v2 " + bin_path + "/")
 
     for i in generate:
-        if os.system(str(i)) != 0:
-            print "Something went wrong with generate of ", str(i)
-            exit(1)
-        # print i, "\n"
+        # if os.system(str(i)) != 0:
+        #     print "Something went wrong with generate of ", str(i)
+        #     exit(1)
+        print i, "\n"
 
     fp = open(installDir + "scripts/how_to_run_darknet_v2_cuda_" + board, 'w')
 
