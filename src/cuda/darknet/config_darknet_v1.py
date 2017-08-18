a35ed1e61fbf757a50d6e17700d77fc4b37a86a4
reliabilitylogdata <reliabilitylogdata@github.com>
2017-08-17 22:28:17 -0300

    darknet setup

--- src/cuda/darknet/config_darknet_v1.py
+++ src/cuda/darknet/config_darknet_v1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import os
 import sys
 import ConfigParser
@@ -38,7 +39,11 @@ def main(board):
     try:
         config = ConfigParser.RawConfigParser()
         config.read(confFile)
+
         installDir = config.get('DEFAULT', 'installdir') + "/"
+        varDir = config.get('DEFAULT', 'vardir') + "/"
+        logDir = config.get('DEFAULT', 'logdir') + "/"
+        tmpDir = config.get('DEFAULT', 'tmpdir') + "/"
 
     except IOError as e:
         print >> sys.stderr, "Configuration setup error: " + str(e)
@@ -46,7 +51,7 @@ def main(board):
 
     data_path = installDir + "data/darknet"
     bin_path = installDir + "bin"
-    src_darknet = installDir + "src/cuda/darknet_v2"
+    src_darknet = installDir + "src/cuda/darknet"
 
     if not os.path.isdir(data_path):
         os.mkdir(data_path, 0777)
@@ -55,8 +60,8 @@ def main(board):
     # executing weights test first
     download_weights(src_dir=src_darknet, data_dir=data_path)
 
-    # change it for darknetv2
-    generate = ["mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet, "make clean GPU=1", "make -j4 GPU=1 ", "mv ./darknet_v2 " + bin_path + "/"]
+
+    generate = ["mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet, "make clean GPU=1", "make -j 4 GPU=1 ", "mv ./darknet " + bin_path + "/"]
     execute = []
 
     # datasets = DATASETS_BIG
@@ -64,26 +69,27 @@ def main(board):
     datasets = DATASETS
 
     # 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
-
     for save_layer in [0, 1]:
         for abft in [0, 2]:
                 for i in datasets:
                     if save_layer == 1 and i['mode'] != 'small':
                         continue
+
                     gold = data_path + '/' + i['gold']
                     txt_list = installDir + 'data/networks_img_list/' + i['txt']
                     gen = {
-                        'bin': [bin_path, "/darknet_v2"],
-                        # 'e': [' -e ', 'yolo'],  # execution_type =
-                        'aa': ['test_radiation', ''],  # execution_model =
-                        'c': [' -c ', data_path + '/yolo_v2.cfg'],  # config_file =
-                        'w': [' -w ', data_path + '/yolo_v2.weights'],  # weights =
+                        'bin': [bin_path, "/darknet_v1"],
+                        'e': [' -e ', 'yolo'],  # execution_type =
+                        'aa': ['test_radiation', ''],
+                        'm': [' -m ', 'valid'],  # execution_model =
+                        'c': [' -c ', data_path + '/yolo.cfg'],  # config_file =
+                        'w': [' -w ', data_path + '/yolo.weights'],  # weights =
                         'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
                         'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
                         'l': [' -l ', txt_list],
-                        # 'b': [' -b ', src_darknet],
-                        # 'x': [' -x ', 0],
-                        's': [' -s ', save_layer],
+                        'b': [' -b ', src_darknet],
+                        'x': [' -x ', 0],
+                        's': [' -s ', 0],
                         'a': [' -a ', abft],
                     }
 
@@ -95,29 +101,32 @@ def main(board):
                     exe_save['s'][1] = 1
 
                     if abft == 0:
-                        generate.append(" ".join([''.join(map(str, gen[key])) for key in gen]))
+                        generate.append(" ".join([''.join(map(str, value)) for key, value in gen.iteritems()]))
 
                     execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
                     execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
 
+
+
     # end for generate
+
     generate.append("make clean GPU=1 ")
     generate.append("make -C ../../include/")
     generate.append("make -j 4 GPU=1 LOGS=1")
-    generate.append("mv ./darknet_v2 " + bin_path + "/")
+    generate.append("mv ./darknet_v1 " + bin_path + "/")
 
     for i in generate:
         if os.system(str(i)) != 0:
             print "Something went wrong with generate of ", str(i)
             exit(1)
-        print i, "\n"
+        print i
 
-    fp = open(installDir + "scripts/how_to_run_darknet_v2_cuda_" + board, 'w')
+    fp = open(installDir + "scripts/how_to_run_darknet_v1_cuda_" + board, 'w')
 
     for i in execute:
-        print >> fp, "[\"""sudo " + str(i) + "\" , 0.016, \"darknet_v2\"],"
+        print >> fp, "[\"""sudo " + str(i) + "\" , 0.016, \"darknet_v1\"],"
 
-    print "\nConfiguring done, to run check file: " + installDir + "scripts/how_to_run_darknet_v2_cuda_" + board + "\n"
+    print "\nConfiguring done, to run check file: " + installDir + "scripts/how_to_run_darknet_v1_cuda_" + board + "\n"
 
     sys.exit(0)
 
