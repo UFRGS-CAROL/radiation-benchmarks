92f94d98b2c30c466b410c5c43c729c57089c68d
reliabilitylogdata <reliabilitylogdata@github.com>
2017-09-04 00:07:36 -0300

    resnet

--- src/cuda/resnet_torch/fb.resnet.torch/pretrained/classify_radiation.lua
+++ src/cuda/resnet_torch/fb.resnet.torch/pretrained/classify_radiation.lua
@@ -1,16 +1,6 @@
 #!/home/carol/radiation-benchmarks/src/cuda/resnet_torch/torch/install/bin/th
 
---
---  Copyright (c) 2016, Manuel Araoz
---  Copyright (c) 2016, Facebook, Inc.
---  All rights reserved.
---
---  This source code is licensed under the BSD-style license found in the
---  LICENSE file in the root directory of this source tree. An additional grant
---  of patent rights can be found in the PATENTS file in the same directory.
---
---  classifies an image using a trained model
---
+
 
 require 'torch'
 require 'paths'
