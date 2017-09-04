c1f44f9e2b394004f97f62dc70e7ce9acff720ce
carol <fernandofernandesantos@gmail.com>
2017-09-04 22:02:43 +0000

    radiation benchmarks

--- src/cuda/resnet_torch/fb.resnet.torch/pretrained/classify_radiation.lua
+++ src/cuda/resnet_torch/fb.resnet.torch/pretrained/classify_radiation.lua
@@ -1,6 +1,6 @@
 #!/home/carol/radiation-benchmarks/src/cuda/resnet_torch/torch/install/bin/th
 
--- 
+--
 --  Copyright (c) 2016, Manuel Araoz
 --  Copyright (c) 2016, Facebook, Inc.
 --  All rights reserved.
@@ -18,7 +18,6 @@ require 'cudnn'
 require 'cunn'
 require 'image'
 
-
 local lp = require 'logs_processing'
 local t = require '../datasets/transforms'
 local imagenetLabel = require './imagenet'
@@ -28,9 +27,9 @@ local imagenetLabel = require './imagenet'
 
 function parse_args()
   if #arg < 2 then
-    io.stderr:write('Usage for normal execution: th classify.lua [MODEL] [FILE]...\n')
-    io.stderr:write('Usage for radiation execution: th classifi_radiation.lua [model] \
-    [radiation mode gen/rad] [txt dataset] [gold_file] [iterations -- ignored for gen]\n')
+    io.stderr:write('Usage for normal execution: ./classify.lua [MODEL] [FILE]...\n')
+    io.stderr:write('Usage for radiation execution: ./classifi_radiation.lua [model] \
+    [radiation mode generate/rad_test] [txt dataset] [gold_file] [iterations -- ignored for gen] [log]\n')
     os.exit(1)
   end
   for _, f in ipairs(arg) do
@@ -38,15 +37,45 @@ function parse_args()
       io.stderr:write('file not found: ' .. f .. '\n')
       os.exit(1)
     end
+    break
   end
 
+  -- return the variables
+  -- rad mode
+  local weights, rad_mode, txt_dataset, gold_file, iterations = nil
+  log = false
+  if #arg > 3 then
+    local i = 1
+    weights = arg[i]
+    i = i + 1
+    rad_mode = arg[i]
+    i = i + 1
+    txt_dataset = arg[i]
+    i = i + 1
+    gold_file = arg[i]
+    i = i + 1
+    iterations = arg[i]
+    i = i + 1
+    if arg[i] ~= nil and (arg[i] == 1 or arg[i] == "true" or arg[i] == "True") then
+      log = true
+    end
+  else
+    for _, f in ipairs(arg) do
+      if not paths.filep(f) then
+        io.stderr:write('file not found: ' .. f .. '\n')
+        os.exit(1)
+      end
+    end
 
+  end
+
+  return weights, rad_mode, txt_dataset, gold_file, iterations, log
 end
 
 function load_model()
-  print("Loading Resnet models " .. arg[1])
+  print("Loading Resnet models " .. weights)
   -- Load the model
-  local model = torch.load(arg[1]):cuda()
+  local model = torch.load(weights):cuda()
   local softMaxLayer = cudnn.SoftMax():cuda()
 
   -- add Softmax layer
@@ -73,7 +102,7 @@ function load_model()
 end
 
 function normal_execution()
-  local model, softMaxLayer, transform, meanstd = load_model()
+  local model_obj, softMaxLayer, transform, meanstd = load_model()
   local N = 10
 
   for i=2,#arg do
@@ -89,7 +118,7 @@ function normal_execution()
     local batch = img:view(1, table.unpack(img:size():totable()))
 
     -- Get the output of the softmax
-    local output = model:forward(batch:cuda()):squeeze()
+    local output = model_obj:forward(batch:cuda()):squeeze()
 
     -- Get the top 5 class indexes and probabilities
     local probs, indexes = output:topk(N, true, true)
@@ -105,38 +134,143 @@ end
 
 
 function generate_radiation_test(arg_size)
-  local model, softMaxLayer, transform, meanstd = load_model()
-  local N = 10
+  local model_obj, softMaxLayer, transform, meanstd = load_model()
 
---  for i=arg_size,#arg do
+  local N = 0
+  local lines = lines_from(txt_dataset)
+  gold_file = io.open(gold_path, "w")
+  gold_file:write(#lines .. ";\n")
+  if gold_file then
+    for k,v in pairs(lines) do
 
-    -- load the image as a RGB float tensor with values 0..1
-    -- [model] [radiation mode gen/rad] [txt dataset] [gold_file] [iterations -- ignored for gen]
-    local img = image.load(arg[2], 3, 'float')
-    local name = arg[2]:match( "([^/]+)$" )
-      
+      -- load the image as a RGB float tensor with values 0..1
+      -- [model] [radiation mode gen/rad] [txt dataset] [gold_file] [iterations -- ignored for gen]
+      local img = image.load(v, 3, 'float')
+      local name = v:match( "([^/]+)$" )
 
-    -- Scale, normalize, and crop the image
-    img = transform(img)
 
-    -- View as mini-batch of size 1
-    local batch = img:view(1, table.unpack(img:size():totable()))
+      -- Scale, normalize, and crop the image
+      img = transform(img)
 
-    -- Get the output of the softmax
-    local output = model:forward(batch:cuda()):squeeze()
+      -- View as mini-batch of size 1
+      local batch = img:view(1, table.unpack(img:size():totable()))
 
-    -- Get the top 5 class indexes and probabilities
-    local probs, indexes = output:topk(N, true, true)
-    print('Classes for', arg[i])
-    for n=1,N do
-      print(probs[n], imagenetLabel[indexes[n]])
+      -- Get the output of the softmax
+      local output = model_obj:forward(batch:cuda()):squeeze()
+
+      -- get the dim
+      if output:dim() == 1 then
+        N = tonumber(#(output:storage()))
+      end
+
+      -- Get the all class indexes and probabilities
+      local probs, indexes = output:topk(N, true, true)
+
+      print('Classes for', v, 'output lenght', N)
+      save_gold(gold_file, v, probs, indexes, N)
+
+      print('')
+
     end
-    print('')
-
---  end
-
+    gold_file:close()
+  else
+    print("Failed to open", gold_path)
+  end
 end
 
 
-parse_args()
-normal_execution()
+--load all images
+function load_images(txt_dataset)
+  local lines = lines_from(txt_dataset)
+  local images = {}
+  for k,v in pairs(lines) do
+    -- load the image as a RGB float tensor with values 0..1
+    local img = image.load(v, 3, 'float')
+    local name = v:match( "([^/]+)$" )
+    images[k] = {img, name, v}
+
+  end
+  return images
+end
+
+
+function test_radiation(arg_size)
+  print("Loading gold")
+  -- load gold
+  local gold_content = load_gold(gold_path)
+  if gold_content ~= nil then
+    print("Gold loaded with sucess")
+  else
+    print("Failed load gold")
+    return
+  end
+
+  -- //////////////////////////////////////////////////////
+  -- test
+  start_app(weights:match("([^/]+)$"), txt_dataset, gold_path, log)
+
+  -- //////////////////////////////////////////////////////
+  local model_obj, softMaxLayer, transform, meanstd = load_model()
+
+  local N = 0
+  local images = load_images(txt_dataset)
+  for i = 0, iterations do
+    for k, v in pairs(images) do
+
+      local img = v[1]
+      local name = v[2]
+      local raw_name = v[3]
+
+      --    local img = image.load(v, 3, 'float')
+      --    local name = v:match( "([^/]+)$" )
+
+      -----------------------------------------------------------------
+      local classifing_time = os.clock()
+      start_iteration(log)
+      -- Scale, normalize, and crop the image
+      local img_trans = transform(img)
+
+      -- View as mini-batch of size 1
+      local batch = img_trans:view(1, table.unpack(img_trans:size():totable()))
+
+      -- Get the output of the softmax
+      local output = model_obj:forward(batch:cuda()):squeeze()
+
+      -- get the dim
+      if output:dim() == 1 then
+        N = tonumber(#(output:storage()))
+      end
+      end_iteration(log)
+      -- Get the all class indexes and probabilities
+      local probs, indexes = output:topk(N, true, true)
+
+      -----------------------------------------------------------------
+      print(string.format("Classifing took %.5f seconds", os.clock() - classifing_time))
+      local compare_time = os.clock()
+      local gold_probs = gold_content[raw_name][1]
+      local gold_indexes = gold_content[raw_name][2]
+
+
+      compare_and_log(true, gold_probs, gold_indexes, probs, indexes, raw_name, i)
+      print(string.format("Comparing took %.5f seconds", os.clock() - compare_time))
+
+    end
+  end
+
+  end_app(log)
+end
+
+weights, rad_mode, txt_dataset, gold_path, iterations, log = parse_args()
+
+if rad_mode == nil then
+
+  normal_execution()
+
+elseif rad_mode =="generate" then
+  generate_radiation_test(#arg)
+elseif rad_mode == "rad_test" then
+  test_radiation(#arg)
+end
+  
+  
+  
--- src/cuda/resnet_torch/fb.resnet.torch/pretrained/logs_processing.lua
+++ src/cuda/resnet_torch/fb.resnet.torch/pretrained/logs_processing.lua
@@ -3,15 +3,15 @@
 
 
 local lh = require '../../../../include/log_helper_swig_wraper/log_helper'
-
+local THRESHOLD = 0.005
 -- start a log file
 function start_app(network, dataset, gold_file, log)
   -- from C start_log_file(char *benchmark_name, char *test_info);
   if log then
 
-    local benchmark = "cudaResnet" .. network
+    local benchmark = "cudaResnet"
 
-    local header = "img_list_txt: " .. dataset .. " gold_file: " .. gold_file
+    local header = "model: " .. network .. " img_list_txt: " .. dataset .. " gold_file: " .. gold_file
     lh.start_log_file(benchmark, header)
   end
 end
@@ -23,7 +23,139 @@ function end_app(log)
   end
 end
 
-
-function save_gold(gold, datapath)
-  	
+function start_iteration(log)
+  if log then
+    lh.start_iteration()
+  end
 end
+
+function end_iteration(log)
+  if log then
+    lh.end_iteration()
+  end
+end
+
+-- save gold for an image, in an opened file
+-- gold_file
+function save_gold(gold_file, img, probs, classes, tensor_size)
+  -- tensor size, img, #probs, #classes
+  gold_file:write(tensor_size .. ";" .. img .. ";" .. #(probs:storage()) .. ";" .. #(classes:storage()) .. ";\n")
+  for n=1,tensor_size do
+    -- probs[n];indexes[n]
+    gold_file:write(probs[n] .. ";" .. classes[n] .. ";\n")
+  end
+end
+
+-- load gold
+function load_gold(gold_path)
+  local gold_content = {}
+  local gold_file = io.open(gold_path, "r")
+
+  if gold_file ~= nil then
+
+    -- read first the size of the dataset
+    local line = gold_file:read()
+    local splited = split(line, ";")
+    local dataset_size = tonumber(splited[0])
+
+    for j = 0,dataset_size do
+      line = gold_file:read()
+      if line == nil then
+        break
+      end
+      local probs = {}
+      local classes = {}
+      -- read first header
+      splited = split(line, ";")
+      local tensor_size = tonumber(splited[0])
+      local img = splited[1]
+      local probs_size = tonumber(splited[2])
+      local classes_size = tonumber(splited[3])
+
+      -- read each probability
+      for i = 0, probs_size - 1 do
+        line = gold_file:read()
+        splited = split(line, ";")
+        --probs[n];indexes
+        probs[i] = tonumber(splited[0])
+        classes[i] = tonumber(splited[1])
+
+      end
+
+      gold_content[img] = {probs, classes}
+    end
+    gold_file:close()
+  else
+    print("Gold file not open", gold_path)
+    return nil
+  end
+
+  return gold_content
+end
+
+-- compare and log if log is activated
+function compare_and_log(log, gold_probs, gold_indexes, found_probs, found_indexes, img, iteration)
+  local error_count = 0
+  --  local i = 0
+  for i, gp in pairs(gold_probs) do
+    print(i, gp)
+    local gi = gold_indexes[i]
+    local fp = found_probs[i]
+    local fi = found_indexes[i]
+    i = i + 1
+    local diff_probs = math.abs(gp - fp)
+    local diff_indexes = math.abs(gi - fi)
+
+    -- compare and log if it is greater than threashold
+    if diff_probs > THRESHOLD or diff_indexes > THRESHOLD then
+      local error_string = string.format("img: [%s] iteration: [%d] found_prob: [%f] gold_prob: [%f] found_index: [%d] gold_index: [%d]",
+        img, iteration, fp, gp, fi, gi)
+      if log then
+        lh.error_log_detail(error_string)
+      else
+        print(error_string)
+      end
+    end
+
+    error_count = error_count + 1
+  end
+
+  if log then
+    lh.error_count(error_count)
+  end
+
+end
+
+-- see if the file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+
+function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={} ; i=0
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    t[i] = str
+    i = i + 1
+  end
+  return t
+end
+
+
