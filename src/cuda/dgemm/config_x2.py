860843ccf2617fe556336dcbd7856d2f505ec9cc
Caio Lunardi <caio.b.lunardi@gmail.com>
2017-09-06 15:22:55 -0300

    Fixed configs for hgemm and dgemm

--- src/cuda/dgemm/config_x2.py
+++ src/cuda/dgemm/config_x2.py
@@ -35,15 +35,15 @@ os.system("cd "+src_dgemm)
 
 os.system("sudo ./generateMatricesDouble -size=1024 -input_a=dgemmA_8192.matrix -input_b=dgemmB_8192.matrix -gold=dgemmGOLD_1024.matrix")
 os.system("sudo ./generateMatricesDouble -size=2048 -input_a=dgemmA_8192.matrix -input_b=dgemmB_8192.matrix -gold=dgemmGOLD_2048.matrix")
-os.system("sudo ./generateMatricesDouble -size=4096 -input_a=dgemmA_8192.matrix -input_b=dgemmB_8192.matrix -gold=dgemmGOLD_4096.matrix")
+os.system("sudo ./generateMatricesDouble -size=8192 -input_a=dgemmA_8192.matrix -input_b=dgemmB_8192.matrix -gold=dgemmGOLD_8192.matrix")
 os.system("sudo chmod 777 dgemm*.matrix");
 os.system("mv dgemm*.matrix "+data_path);
 os.system("mv ./generateMatricesDouble ./cudaDGEMM "+bin_path)
 
 fp = open(installDir+"scripts/how_to_run_dgemm_cuda_x2", 'w')
-print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=1024 -input_a="+data_path+"dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_1024.matrix -iterations=10000000"
-print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=2048 -input_a="+data_path+"dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_2048.matrix -iterations=10000000"
-print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=4096 -input_a="+data_path+"dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_4096.matrix -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=1024 -input_a="+data_path+"/dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_1024.matrix -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=2048 -input_a="+data_path+"/dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_2048.matrix -iterations=10000000"
+print >>fp, "sudo "+bin_path+"/cudaDGEMM -size=8192 -input_a="+data_path+"/dgemmA_8192.matrix -input_b="+data_path+"/dgemmB_8192.matrix -gold="+data_path+"/dgemmGOLD_8192.matrix -iterations=10000000"
 
 print "\nConfiguring done, to run check file: "+installDir+"scripts/how_to_run_dgemm_cuda_x2\n"
 
