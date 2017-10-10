f53d7217163d5728b6b8d4a4cd5402ae13af6413
Daniel Oliveira <dagoliveira@inf.ufrgs.br>
2017-10-10 17:11:47 -0300

    Comment are now allowed in the input file of killtest

--- scripts/test_killtest_commands_json.py
+++ scripts/test_killtest_commands_json.py
@@ -64,7 +64,7 @@ def readCommands(filelist):
         fp = open(filelist, "r")
         for f in fp:
             f = f.strip()
-            if f.startswith("#") or f.startswith("%") or f.startswith("//"):
+            if f.startswith("#") or f.startswith("%"):
                 continue
             if os.path.isfile(f):
                 fjson = open(f, "r")
