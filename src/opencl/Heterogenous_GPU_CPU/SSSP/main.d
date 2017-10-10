1319684d911cabe13162349eadef73109a537c40
gpdavila <gpiscoya2013@hotmail.com>
2017-10-10 14:33:35 -0300

    SSSP APU

---
+++ src/opencl/Heterogenous_GPU_CPU/SSSP/main.d
@@ -0,0 +1,11 @@
+main.o: main.cpp \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/kernel.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/common.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/common.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/ocl.h \
+ /opt/AMDAPPSDK-3.0/include/CL/cl.h \
+ /opt/AMDAPPSDK-3.0/include/CL/cl_platform.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/timer.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/verify.h \
+ /home/carol/radiation-benchmarks/src/opencl/Heterogenous_GPU_CPU/SSSP/support/common.h \
+ /home/carol/radiation-benchmarks/src/include/log_helper.h
