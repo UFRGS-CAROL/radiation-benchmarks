ed7422851ac74a26c6c947bc8c13615d4e20f2b3
reliabilitylogdata <reliabilitylogdata@github.com>
2017-10-12 18:16:33 -0300

    bezier

--- src/cuda/bezier_surface/config_bezier_surface.py
+++ src/cuda/bezier_surface/config_bezier_surface.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python
+
+
+import copy
 import os
 import sys
 import ConfigParser
 
-INPUT = ['image.pgm']
+INPUT = ['input/control.txt']
 ITERATIONS = 100000
+ALPHA_VARIATIONS = [0]
+RESOLUTIONS = [2500]
 
 
 def main(board):
-
     print "Generating Bezier Surface for CUDA on " + str(board)
 
     confFile = '/etc/radiation-benchmarks.conf'
@@ -21,7 +26,7 @@ def main(board):
         print >> sys.stderr, "Configuration setup error: " + str(e)
         sys.exit(1)
 
-    benchmark_bin = "bs"
+    benchmark_bin = "bezier_surface"
     data_path = installDir + "data/" + benchmark_bin
     bin_path = installDir + "bin"
     src_srad = installDir + "src/cuda/" + benchmark_bin
@@ -30,13 +35,54 @@ def main(board):
         os.mkdir(data_path, 0777)
         os.chmod(data_path, 0777)
 
+    generate = ["cd " + src_srad, "make clean", "make -j4",
+                "mv ./" + benchmark_bin + " " + bin_path + "/"]
+    execute = []
+
+    for i in INPUT:
+        for j in ALPHA_VARIATIONS:
+            for r in RESOLUTIONS:
+                inputFile = data_path + i
+
+                # $(RAD_BENCH) / src / cuda / bezier_surface /$(EXE) - w
+                # 0 - r
+                # 10 - a
+                # 0 - s
+                # 1 \
+                # - z $(RAD_BENCH) / data / bezier_surface / temp.gold \
+                #      - f $(RAD_BENCH) / data / bezier_surface / input / control.txt - n
+                # 2500
+                gen = [None] * 8
+                gen[0] = ['sudo ', bin_path + "/" + benchmark_bin + " "]
+                gen[1] = ['-w ', 0]
+                gen[2] = ['-r ', 1]
+                gen[3] = ['-a ', j]
+                gen[4] = ['-s ', 0]  # change for execute
+                gen[5] = ['-z ',
+                          data_path + "/alpha_" + str(j) + "_in_size_" + str(r) + "_out_size_" + str(r) + ".gold"]
+                gen[6] = ['-f ', inputFile]
+                gen[7] = ['-n ', r]
+
+                # change mode and iterations for exe
+                exe = copy.deepcopy(gen)
+                exe[4][1] = 1
+                exe[2][1] = ITERATIONS
+
+                generate.append(' '.join(str(r) for v in gen for r in v))
+                execute.append(' '.join(str(r) for v in exe for r in v))
+
+    generate.extend(
+        ["make clean", "make -C ../../include/",
+         "make -j4 LOGS=1",
+         "mv ./" + benchmark_bin + " " + bin_path + "/"])
+    execute_and_write_how_to_file(execute, generate, installDir, benchmark_bin)
 
 
 def execute_and_write_how_to_file(execute, generate, installDir, benchmark_bin):
     for i in generate:
-        if os.system(str(i)) != 0:
-            print "Something went wrong with generate of ", str(i)
-            exit(1)
+        # if os.system(str(i)) != 0:
+        #     print "Something went wrong with generate of ", str(i )
+        #     exit(1)
         print i
     fp = open(installDir + "scripts/json_files/" + benchmark_bin + ".json", 'w')
 
@@ -61,5 +107,3 @@ if __name__ == "__main__":
         print "./config_generic <k1/x1/k40>"
     else:
         main(str(parameter[0]).upper())
-
-
