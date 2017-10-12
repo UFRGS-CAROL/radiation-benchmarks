e08a087ab8ad9e414521250443fced72b21cc474
reliabilitylogdata <reliabilitylogdata@github.com>
2017-10-12 20:18:28 -0300

    make

---
+++ src/cuda/bfs/logs_processing.cpp
@@ -0,0 +1,10 @@
+/*
+ * logs_processing.cpp
+ *
+ *  Created on: 12/10/2017
+ *      Author: fernando
+ */
+
+
+
+
---
+++ src/cuda/bfs/logs_processing.h
@@ -0,0 +1,15 @@
+/*
+ * logs_processing.h
+ *
+ *  Created on: 12/10/2017
+ *      Author: fernando
+ */
+
+#ifndef LOGS_PROCESSING_H_
+#define LOGS_PROCESSING_H_
+
+
+
+
+
+#endif /* LOGS_PROCESSING_H_ */
