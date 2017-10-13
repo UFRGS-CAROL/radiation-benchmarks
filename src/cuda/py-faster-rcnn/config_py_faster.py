5aa04468ac8cc6a5a554e769a36b405e4a48e524
Fernando <f_157@hotmail.com>
2017-10-13 16:12:39 -0300

    py faster

--- src/cuda/py-faster-rcnn/config_py_faster.py
+++ src/cuda/py-faster-rcnn/config_py_faster.py
@@ -11,6 +11,12 @@ DATASETS = [
     {'txt': 'urban.street.1.1K.txt', 'gold': 'gold.urban.street.1.1K.test', 'mode': 'full'},
     {'txt': 'voc.2012.1K.txt', 'gold': 'gold.voc.2012.1K.test', 'mode': 'full'},
 
+    # average
+    # {'txt': 'caltech.pedestrians.100.txt', 'gold': 'gold.caltech.100.csv', 'mode': 'average'},
+    # {'txt': 'urban.street.100.txt', 'gold': 'gold.urban.street.100.csv', 'mode': 'average'},
+    # {'txt': 'voc.2012.100.txt', 'gold': 'gold.voc.2012.100.csv', 'mode': 'average'},
+
+
     # very_small for X1 and X2
     {'txt': 'caltech.pedestrians.10.txt', 'gold': 'gold.caltech.10.test', 'mode': 'small'},
     {'txt': 'urban.street.10.txt', 'gold': 'gold.urban.street.10.test', 'mode': 'small'},
