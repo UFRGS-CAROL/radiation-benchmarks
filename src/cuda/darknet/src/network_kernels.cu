c73d9ff84fa8c7086d571e59d43fc692f6f8a848
reliabilitylogdata <reliabilitylogdata@github.com>
2018-02-17 12:05:28 -0200

    dmr

--- src/cuda/darknet/src/network_kernels.cu
+++ src/cuda/darknet/src/network_kernels.cu
@@ -608,8 +608,8 @@ float *network_predict_gpu_mr(network *redundant_nets, float *input,
 	}
 
 	forward_network_gpu(redundant_nets[0], states[0], redundant_nets, states, modular_redundancy);
-	printf("The error is here\n");
 	float *out = get_network_output(redundant_nets[0]);
+	printf("The error is here\n");
 
 	for (int i = 0; i < modular_redundancy; i++)
 		if (states[i].input)
