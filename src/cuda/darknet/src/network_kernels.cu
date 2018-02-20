b8db17eddd024174dfad880155ed42afac5405f5
reliabilitylogdata <reliabilitylogdata@github.com>
2018-02-20 09:56:35 -0300

    test

--- src/cuda/darknet/src/network_kernels.cu
+++ src/cuda/darknet/src/network_kernels.cu
@@ -782,8 +782,8 @@ float *network_predict_gpu_mr(network *nets, float *input, int mr) {
 
 	forward_network_gpu_mr(nets, states, mr);
 	float *out = get_network_output_gpu(nets[0]);
-	for (int i = 0; i < mr; i++)
-		cuda_free(states[i].input);
+//	for (int i = 0; i < mr; i++)
+//		cuda_free(states[i].input);
 	return out;
 }
 
