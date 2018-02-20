8f024336b48bfd9f77607a9055922047fd344862
reliabilitylogdata <reliabilitylogdata@github.com>
2018-02-20 11:04:04 -0300

    test

--- src/cuda/darknet/src/network_kernels.cu
+++ src/cuda/darknet/src/network_kernels.cu
@@ -116,57 +116,57 @@ void forward_network_gpu_mr(network *nets, network_state *states, int mr) {
 	network net = nets[0];
 	network_state state = states[0];
 	for (int i = 0; i < net.n; ++i) {
-			state.index = i;
-			layer l = net.layers[i];
-			if (l.delta_gpu) {
-				fill_ongpu(l.outputs * l.batch, 0, l.delta_gpu, 1);
-			}
-			if (l.type == CONVOLUTIONAL) {
-				forward_convolutional_layer_gpu(l, state);
-			} else if (l.type == DECONVOLUTIONAL) {
-				forward_deconvolutional_layer_gpu(l, state);
-			} else if (l.type == ACTIVE) {
-				forward_activation_layer_gpu(l, state);
-			} else if (l.type == LOCAL) {
-				forward_local_layer_gpu(l, state);
-			} else if (l.type == DETECTION) {
-				forward_detection_layer_gpu(l, state);
-			} else if (l.type == REGION) {
-				forward_region_layer_gpu(l, state);
-			} else if (l.type == CONNECTED) {
-				forward_connected_layer_gpu(l, state);
-			} else if (l.type == RNN) {
-				forward_rnn_layer_gpu(l, state);
-			} else if (l.type == GRU) {
-				forward_gru_layer_gpu(l, state);
-			} else if (l.type == CRNN) {
-				forward_crnn_layer_gpu(l, state);
-			} else if (l.type == CROP) {
-				forward_crop_layer_gpu(l, state);
-			} else if (l.type == COST) {
-				forward_cost_layer_gpu(l, state);
-			} else if (l.type == SOFTMAX) {
-				forward_softmax_layer_gpu(l, state);
-			} else if (l.type == NORMALIZATION) {
-				forward_normalization_layer_gpu(l, state);
-			} else if (l.type == BATCHNORM) {
-				forward_batchnorm_layer_gpu(l, state);
-			} else if (l.type == MAXPOOL) {
-				forward_maxpool_layer_gpu(l, state);
-			} else if (l.type == REORG) {
-				forward_reorg_layer_gpu(l, state);
-			} else if (l.type == AVGPOOL) {
-				forward_avgpool_layer_gpu(l, state);
-			} else if (l.type == DROPOUT) {
-				forward_dropout_layer_gpu(l, state);
-			} else if (l.type == ROUTE) {
-				forward_route_layer_gpu(l, net);
-			} else if (l.type == SHORTCUT) {
-				forward_shortcut_layer_gpu(l, state);
-			}
-			state.input = l.output_gpu;
-
+		state.index = i;
+		layer l = net.layers[i];
+		if (l.delta_gpu) {
+			fill_ongpu(l.outputs * l.batch, 0, l.delta_gpu, 1);
 		}
+		if (l.type == CONVOLUTIONAL) {
+			forward_convolutional_layer_gpu(l, state);
+		} else if (l.type == DECONVOLUTIONAL) {
+			forward_deconvolutional_layer_gpu(l, state);
+		} else if (l.type == ACTIVE) {
+			forward_activation_layer_gpu(l, state);
+		} else if (l.type == LOCAL) {
+			forward_local_layer_gpu(l, state);
+		} else if (l.type == DETECTION) {
+			forward_detection_layer_gpu(l, state);
+		} else if (l.type == REGION) {
+			forward_region_layer_gpu(l, state);
+		} else if (l.type == CONNECTED) {
+			forward_connected_layer_gpu(l, state);
+		} else if (l.type == RNN) {
+			forward_rnn_layer_gpu(l, state);
+		} else if (l.type == GRU) {
+			forward_gru_layer_gpu(l, state);
+		} else if (l.type == CRNN) {
+			forward_crnn_layer_gpu(l, state);
+		} else if (l.type == CROP) {
+			forward_crop_layer_gpu(l, state);
+		} else if (l.type == COST) {
+			forward_cost_layer_gpu(l, state);
+		} else if (l.type == SOFTMAX) {
+			forward_softmax_layer_gpu(l, state);
+		} else if (l.type == NORMALIZATION) {
+			forward_normalization_layer_gpu(l, state);
+		} else if (l.type == BATCHNORM) {
+			forward_batchnorm_layer_gpu(l, state);
+		} else if (l.type == MAXPOOL) {
+			forward_maxpool_layer_gpu(l, state);
+		} else if (l.type == REORG) {
+			forward_reorg_layer_gpu(l, state);
+		} else if (l.type == AVGPOOL) {
+			forward_avgpool_layer_gpu(l, state);
+		} else if (l.type == DROPOUT) {
+			forward_dropout_layer_gpu(l, state);
+		} else if (l.type == ROUTE) {
+			forward_route_layer_gpu(l, net);
+		} else if (l.type == SHORTCUT) {
+			forward_shortcut_layer_gpu(l, state);
+		}
+		state.input = l.output_gpu;
+
+	}
 
 }
 
