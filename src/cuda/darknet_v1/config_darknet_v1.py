d5094e486fbbf86f2e152cf787b74e38da0908bb
Fernando <f_157@hotmail.com>
2018-04-28 15:26:26 -0300

    darknet

--- src/cuda/darknet_v1/config_darknet_v1.py
+++ src/cuda/darknet_v1/config_darknet_v1.py
@@ -13,15 +13,22 @@ DATASETS = [
     # {'txt': 'voc.2012.1K.txt', 'gold': 'gold.voc.2012.1K.csv', 'mode': 'full'},
 
     # average
-    #{'txt': 'caltech.pedestrians.100.txt', 'gold': 'gold.caltech.100.csv', 'mode': 'average'},
-    #{'txt': 'urban.street.100.txt', 'gold': 'gold.urban.street.100.csv', 'mode': 'average'},
-    #{'txt': 'voc.2012.100.txt', 'gold': 'gold.voc.2012.100.csv', 'mode': 'average'},
+    {'txt': 'caltech.pedestrians.100.txt', 'gold': 'gold.caltech.100.csv', 'mode': 'average'},
+    {'txt': 'urban.street.100.txt', 'gold': 'gold.urban.street.100.csv', 'mode': 'average'},
+    {'txt': 'voc.2012.100.txt', 'gold': 'gold.voc.2012.100.csv', 'mode': 'average'},
 
     # very_small for X1 and X2
-    {'txt': 'caltech.pedestrians.10.txt', 'gold': 'gold.caltech.10.csv', 'mode': 'small'},
-    {'txt': 'urban.street.10.txt', 'gold': 'gold.urban.street.10.csv', 'mode': 'small'},
-    {'txt': 'voc.2012.10.txt', 'gold': 'gold.voc.2012.10.csv', 'mode': 'small'},
+    # {'txt': 'caltech.pedestrians.10.txt', 'gold': 'gold.caltech.10.csv', 'mode': 'small'},
+    # {'txt': 'urban.street.10.txt', 'gold': 'gold.urban.street.10.csv', 'mode': 'small'},
+    # {'txt': 'voc.2012.10.txt', 'gold': 'gold.voc.2012.10.csv', 'mode': 'small'},
 ]
+
+BINARY_NAME = "darknet_v1"
+DEBUG_MODE = False
+SAVE_LAYER = [0, 1]
+# 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
+ABFT = [0, 2]
+
 
 def download_weights(src_dir, data_dir):
     os.chdir(data_dir)
@@ -35,23 +42,20 @@ def download_weights(src_dir, data_dir):
 def main(board):
     print "Generating darknet for CUDA, board:" + board
 
-    confFile = '/etc/radiation-benchmarks.conf'
+    conf_file = '/etc/radiation-benchmarks.conf'
     try:
         config = ConfigParser.RawConfigParser()
-        config.read(confFile)
+        config.read(conf_file)
 
-        installDir = config.get('DEFAULT', 'installdir') + "/"
-        varDir = config.get('DEFAULT', 'vardir') + "/"
-        logDir = config.get('DEFAULT', 'logdir') + "/"
-        tmpDir = config.get('DEFAULT', 'tmpdir') + "/"
+        install_dir = config.get('DEFAULT', 'installdir') + "/"
 
     except IOError as e:
         print >> sys.stderr, "Configuration setup error: " + str(e)
         sys.exit(1)
 
-    data_path = installDir + "data/darknet"
-    bin_path = installDir + "bin"
-    src_darknet = installDir + "src/cuda/darknet"
+    data_path = install_dir + "data/darknet"
+    bin_path = install_dir + "bin"
+    src_darknet = install_dir + "src/cuda/" + BINARY_NAME
 
     if not os.path.isdir(data_path):
         os.mkdir(data_path, 0777)
@@ -60,79 +64,89 @@ def main(board):
     # executing weights test first
     download_weights(src_dir=src_darknet, data_dir=data_path)
 
-
-    generate = ["mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet, "make clean GPU=1", "make -j 4 GPU=1 ", "mv ./darknet_v1 " + bin_path + "/"]
+    generate = ["sudo mkdir -p " + bin_path, "mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet,
+                "make clean GPU=1", "make -j 4 GPU=1 ",
+                "mv ./" + BINARY_NAME + " " + bin_path + "/"]
     execute = []
 
     # datasets = DATASETS_BIG
     # if board in ['X1', 'K1', 'X2']:
     datasets = DATASETS
 
-    # 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
-    for save_layer in [0, 1]:
-        for abft in [0, 2]:
-                for i in datasets:
-                    if (save_layer == 1 and i['mode'] == 'full') or (save_layer == 0 and i['mode'] == 'small'):
-                        continue
+    for save_layer in SAVE_LAYER:
+        for abft in ABFT:
+            for i in datasets:
+                if (save_layer == 1 and i['mode'] == 'full') or (save_layer == 0 and i['mode'] == 'small'):
+                    continue
 
-                    gold = data_path + '/' + 'darknet_v1_' + i['gold']
-                    txt_list = installDir + 'data/networks_img_list/' + i['txt']
-                    gen = {
-                        'bin': [bin_path, "/darknet_v1"],
-                        'e': [' -e ', 'yolo'],  # execution_type =
-                        'aa': ['test_radiation', ''],
-                        'm': [' -m ', 'valid'],  # execution_model =
-                        'c': [' -c ', data_path + '/yolo_v1.cfg'],  # config_file =
-                        'w': [' -w ', data_path + '/yolo_v1.weights'],  # weights =
-                        'n': [' -n ', '1'],  # iterations =  #it is not so much, since each dataset have at least 10k of images
-                        'g': [' -g ', gold],  # base_caltech_out = base_voc_out = src_darknet
-                        'l': [' -l ', txt_list],
-                        'b': [' -b ', src_darknet],
-                        'x': [' -x ', 0],
-                        's': [' -s ', save_layer],
-                        'a': [' -a ', abft],
-                    }
+                gold = data_path + '/' + BINARY_NAME + '_' + i['gold']
+                txt_list = install_dir + 'data/networks_img_list/' + i['txt']
+                gen = {
+                    'bin': [bin_path, "/" + BINARY_NAME],
+                    'e': [' -e ', 'yolo'],  # execution_type =
+                    'aa': ['test_radiation', ''],
+                    'm': [' -m ', 'valid'],  # execution_model =
+                    'c': [' -c ', data_path + '/yolo_v1.cfg'],  # config_file =
+                    'w': [' -w ', data_path + '/yolo_v1.weights'],  # weights =
+                    'n': [' -n ', '1'],
+                    'g': [' -g ', gold],
+                    'l': [' -l ', txt_list],
+                    'b': [' -b ', src_darknet],
+                    'x': [' -x ', 0],
+                    's': [' -s ', save_layer],
+                    'a': [' -a ', abft],
+                }
 
-                    exe = copy.deepcopy(gen)
-                    exe['n'][1] = 10000
-                    exe['g'][0] = ' -d '
+                exe = copy.deepcopy(gen)
+                exe['n'][1] = 10000
+                exe['g'][0] = ' -d '
 
-                    exe_save = copy.deepcopy(exe)
-                    exe_save['s'][1] = save_layer
+                exe_save = copy.deepcopy(exe)
+                exe_save['s'][1] = save_layer
 
-                    if abft == 0:
-                        generate.append(" ".join([''.join(map(str, value)) for key, value in gen.iteritems()]))
+                if abft == 0:
+                    generate.append(" ".join([''.join(map(str, value)) for key, value in gen.iteritems()]))
 
-                    execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
-                    # execute.append(" ".join([''.join(map(str, value)) for key, value in exe_save.iteritems()]))
-
-
-
-    # end for generate
-
+                execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
+    
     generate.append("make clean GPU=1 ")
     generate.append("make -C ../../include/")
     generate.append("make -j 4 GPU=1 LOGS=1")
-    generate.append("mv ./darknet_v1 " + bin_path + "/")
+    generate.append("mv ./" + BINARY_NAME + " " + bin_path + "/")
 
+    execute_and_write_json_to_file(execute, generate, install_dir, BINARY_NAME)
+
+
+def execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin):
     for i in generate:
-        if os.system(str(i)) != 0:
-            print "Something went wrong with generate of ", str(i)
-            exit(1)
         print i
+        if not DEBUG_MODE:
+            if os.system(str(i)) != 0:
+                print "Something went wrong with generate of ", str(i)
+                exit(1)
 
-    fp = open(installDir + "scripts/how_to_run_darknet_v1_cuda_" + board, 'w')
+    list_to_print = ["[\n"]
+    for ii, i in enumerate(execute):
+        command = "{\"killcmd\": \"killall -9 " + benchmark_bin + "\", \"exec\": \"" + str(i) + "\"}"
+        if ii != len(execute) - 1:
+            command += ',\n'
+        list_to_print.append(command)
+    list_to_print.append("\n]")
 
-    for i in execute:
-        print >> fp, "[\"""sudo " + str(i) + "\" , 0.016, \"darknet_v1\"],"
+    with open(install_dir + "scripts/json_files/" + benchmark_bin + ".json", 'w') as fp:
+        fp.writelines(list_to_print)
 
-    print "\nConfiguring done, to run check file: " + installDir + "scripts/how_to_run_darknet_v1_cuda_" + board + "\n"
-
-    sys.exit(0)
+    print "\nConfiguring done, to run check file: " + install_dir + "scripts/json_files/" + benchmark_bin + ".json"
 
 
 if __name__ == "__main__":
+    global DEBUG_MODE
+
     parameter = sys.argv[1:]
+    try:
+        DEBUG_MODE = sys.argv[2:]
+    except:
+        DEBUG_MODE = False
     if len(parameter) < 1:
         print "./config_generic <k1/x1/x2/k40/titan>"
     else:
