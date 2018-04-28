97af93c582fdc0142d8ebb039500f48bea7b4f82
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-27 22:44:50 -0300

    config

--- src/cuda/CHAI/SSSP/config_sssp.py
+++ src/cuda/CHAI/SSSP/config_sssp.py
@@ -6,8 +6,12 @@ import os
 import sys
 import ConfigParser
 
-INPUT = ["lakes_graph_in"]
+INPUT = "colorado_graph_in"
 ITERATIONS = 100000
+# ./sssp -f /home/carol/radiation-benchmarks/data/sssp/colorado_graph_in \
+# 			-c /home/carol/radiation-benchmarks/data/sssp/colorado_graph.gold -m 0 \
+# 			-r 1 -l 0  #CPU
+# TITAN X (Pascal)	.ITERATION 0
 
 #  ./bfs -f input/lakes_graph_in -c output/lakes_graph_out -r 1 -l 900000000  #CPU
 # ./bfs -f input/lakes_graph_in -c output/lakes_graph_out -r 1 -l 0  #GPU
@@ -22,10 +26,10 @@ DEBUG_MODE = False
 def untar_graphs(file_path):
     tries = 3
 
-    while not os.path.isfile(file_path + "/lakes_graph_in"):
-        print "tar xzf " + file_path + "/lakes_graph_in.tar.gz -C " + file_path + "/"
-        if os.system("tar xzf " + file_path + "/lakes_graph_in.tar.gz -C " + file_path + "/") != 0:
-            print "Something went wrong with untar of " + file_path + " file. Trying again"
+    while not os.path.isfile(file_path + "/" + INPUT):
+        print "tar xzf " + file_path + "/" + INPUT + ".tar.gz -C " + file_path + "/"
+        if os.system("tar xzf " + file_path + "/" + INPUT + ".tar.gz -C " + file_path + "/") != 0:
+            print "Something went wrong with untar of " + file_path + "/" + INPUT + " file. Trying again"
 
         if tries == 0:
             return False
@@ -35,7 +39,7 @@ def untar_graphs(file_path):
 
 
 def main(board):
-    print "Generating BFS for CUDA on " + str(board)
+    print "Generating SSSP for CUDA on " + str(board)
 
     conf_file = '/etc/radiation-benchmarks.conf'
     try:
@@ -48,10 +52,10 @@ def main(board):
         print >> sys.stderr, "Configuration setup error: " + str(e)
         sys.exit(1)
 
-    benchmark_bin = "bfs"
+    benchmark_bin = "sssp"
     data_path = install_dir + "data/" + benchmark_bin
     bin_path = install_dir + "bin"
-    src_bfs = install_dir + "src/cuda/CHAI/" + benchmark_bin
+    src_bfs = install_dir + "src/cuda/CHAI/SSSP"
 
     if not untar_graphs(data_path):
         raise ValueError("Error on untar the file")
@@ -64,30 +68,29 @@ def main(board):
                 "mv -f ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
 
-    for i in INPUT:
-        for j in THREADS_HOST:
-            if j > 0 and board not in EMBEDDED_HOSTS:
-                continue
+    for j in THREADS_HOST:
+        if j > 0 and board not in EMBEDDED_HOSTS:
+            continue
 
-            input_file = data_path + "/" + i
-            output_file = data_path + "/lakes_graph_out"
-            # $(RAD_BENCH)/src/cuda/bfs/$(EXE) -t 0 -f $(RAD_BENCH)/data/bfs/graph1MW_6.txt -c temp.gold -m 1 -r 100
-            gen = [None] * 7
-            gen[0] = ['sudo ', bin_path + "/" + benchmark_bin + " "]
-            gen[1] = ['-l ', j]
-            gen[2] = ['-f ', input_file]
-            gen[3] = ['-p ', input_file + ".gold"]
-            gen[4] = ['-m ', 0]  # change for execute
-            gen[5] = ['-r ', 1]
-            gen[6] = ['-c ', output_file]
+        input_file = data_path + "/" + INPUT
+        output_file = data_path + "/graph_" + INPUT + "_proportion_" + str(j) + ".gold"
 
-            # change mode and iterations for exe
-            exe = copy.deepcopy(gen)
-            exe[4][1] = 1
-            exe[5][1] = ITERATIONS
+        gen = [None] * 7
+        gen[0] = ['sudo ', bin_path + "/" + benchmark_bin + " "]
+        gen[1] = ['-l ', j]
+        gen[2] = ['-f ', input_file]
+        gen[3] = []
+        gen[4] = ['-m ', 0]  # change for execute
+        gen[5] = ['-r ', 1]
+        gen[6] = ['-c ', output_file]
 
-            generate.append(' '.join(str(r) for v in gen for r in v))
-            execute.append(' '.join(str(r) for v in exe for r in v))
+        # change mode and iterations for exe
+        exe = copy.deepcopy(gen)
+        exe[4][1] = 1
+        exe[5][1] = ITERATIONS
+
+        generate.append(' '.join(str(r) for v in gen for r in v))
+        execute.append(' '.join(str(r) for v in exe for r in v))
 
     generate.extend(
         ["make clean", "make -C ../../../include/",
