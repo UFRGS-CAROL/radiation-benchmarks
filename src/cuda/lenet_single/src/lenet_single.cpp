428856df8001921b793e1a23eab7f7cdf74422d2
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-29 13:02:04 -0300

    make

--- src/cuda/lenet_single/src/lenet_single.cpp
+++ src/cuda/lenet_single/src/lenet_single.cpp
@@ -713,6 +713,7 @@ gflags	::SetUsageMessage("command line brew\n"
 //		int argc_copy = argc - RADIATION_PARAMETERS;
 //		caffe::GlobalInit(&argc_copy, &argv_copy);
 //		free(argv_copy);
+		printf("passou\n");
 	} else {
 		caffe::GlobalInit(&argc, &argv);
 	}
