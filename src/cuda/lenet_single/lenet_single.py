553f8bab4c21c0fcd915465db3005493cc663694
reliabilitylogdata <reliabilitylogdata@github.com>
2018-04-29 22:40:10 -0300

    lenet single

---
+++ src/cuda/lenet_single/lenet_single.py
@@ -0,0 +1,118 @@
+import caffe
+import argparse
+import os
+import lmdb
+import numpy as np
+from caffe.proto import caffe_pb2
+
+
+def test(prototxt, lenet_model, lmdb_file):
+    layer_output = open("./caffe_to_vhdl/generated/tb/caffe_layer_output.txt", "w")
+
+    # load the model
+    net = caffe.Net(prototxt,
+                    lenet_model,
+                    caffe.TEST)
+
+    # load input and configure preprocessing
+    lmdb_env = lmdb.open(lmdb_file)
+    lmdb_txn = lmdb_env.begin()
+    lmdb_cursor = lmdb_txn.cursor()
+    datum = caffe_pb2.Datum()
+
+    instance = 0
+    for key, value in lmdb_cursor:
+        instance += 1
+
+        if instance <= 1:
+
+            datum.ParseFromString(value)
+
+            label = datum.label
+            data = caffe.io.datum_to_array(datum)
+            image = data.astype(np.float32)
+
+            dim1 = len(image)
+            dim2 = len(image[0])
+            dim3 = len(image[0][0])
+            for i in range(0, dim1):
+                for j in range(0, dim2):
+                    for k in range(0, dim3):
+                        image[i][j][k] *= 0.00390625
+
+            # load the image in the data layer
+            net.blobs['data'].data[...] = image
+
+            # compute
+            # out = net.forward()
+            out = net.forward(start='data', end='ip2')
+
+            # predicted predicted class
+            # print out['prob'].argmax()
+            print out['ip2']
+            layer_results = out['ip2']
+
+            dim1 = len(layer_results)
+            dim2 = len(layer_results[0])
+            # dim3 = len(layer_results[0][0])
+            # dim4 = len(layer_results[0][0][0])
+
+            # print(str(dim1) + " | " + str(dim2) + " | " + str(dim3) + " | " + str(dim4) + "\n\n\n")
+
+            for i in range(0, dim1):
+                for j in range(0, dim2):
+                    layer_output.write(str(layer_results[i][j]) + "\n")
+                    # for k in range(0, dim3):
+                    # for l in range(0, dim4):
+                    # layerOutput.write(str(layer_results[i][j][k][l]) + "\n")
+
+
+def set_device():
+    caffe.set_device(0)
+    caffe.set_mode_gpu()
+
+
+def train_model(model, weights, db_path):
+    net = caffe.Net(model, weights, caffe.TEST)
+    set_device()
+    # db_path = './examples/mnist/mnist_test_lmdb'
+
+    lmdb_env = lmdb.open(db_path)
+    lmdb_txn = lmdb_env.begin()
+    lmdb_cursor = lmdb_txn.cursor()
+    count = 0
+    correct = 0
+    for key, value in lmdb_cursor:
+        print "Count:", count
+        count += 1
+        datum = caffe.proto.caffe_pb2.Datum()
+        datum.ParseFromString(value)
+        label = int(datum.label)
+        image = caffe.io.datum_to_array(datum)
+        image = image.astype(np.uint8)
+        net.blobs['data'].data[...] = np.asarray([image])
+        # out = net.forward_all(data=np.asarray([image]))
+        out = net.forward()
+
+        predicted_label = out['prob'][0].argmax(axis=0)
+        if label == predicted_label[0][0]:
+            correct += 1
+        print("Label is class " + str(label) + ", predicted class is " + str(predicted_label[0][0]))
+        if count == 3:
+            break
+    print(str(correct) + " out of " + str(count) + " were classified correctly")
+
+
+# Before keep going execute
+# data/mnist/get_mnist.sh
+# examples/mnist/create_mnist.sh
+
+if __name__ == '__main__':
+    solver = "/home/carol/radiation-benchmarks/src/cuda/LeNetCUDA/caffe/examples/mnist/lenet_solver.prototxt"
+    model = "/home/carol/radiation-benchmarks/src/cuda/LeNetCUDA/caffe/examples/mnist/lenet_train_test.prototxt"
+    weights = "/home/carol/radiation-benchmarks/src/cuda/LeNetCUDA/caffe/examples/mnist/lenet_iter_10000.caffemodel"
+    db_path = "/home/carol/radiation-benchmarks/src/cuda/LeNetCUDA/caffe/examples/mnist/mnist_test_lmdb/"
+    # Training(model)
+    train_model(model, weights, db_path)
+    test()
+test()
