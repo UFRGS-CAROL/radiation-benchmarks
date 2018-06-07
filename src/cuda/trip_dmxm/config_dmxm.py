3c41df827d2e6722b2137844ad86392455dde5c3
carol <fernandofernandesantos@gmail.com>
2018-06-07 17:48:40 +0000

    cuda dmxm

--- src/cuda/trip_dmxm/config_dmxm.py
+++ src/cuda/trip_dmxm/config_dmxm.py
@@ -5,16 +5,18 @@ import copy
 import os
 import sys
 
-SIZES = [2048, 4096, 8192]
+sys.path.insert(0, '../../include')
+from common_config import discover_board, execute_and_write_json_to_file
+
+SIZES = [8192, 4096, 2048]
 ITERATIONS = 10000
 
-DEBUG_MODE = False
-BENCHMARK_BIN = "cudaDMXM"
-DATA_PATH_BASE = "dmxm"
+BENCHMARK_BIN = "cuda_trip_dmxm"
+DATA_PATH_BASE = "trip_dmxm"
 GENERATE_BIN_NAME = "generateMatricesDouble"
 
 
-def main(board):
+def config(board, debug):
     benchmark_bin = BENCHMARK_BIN
     print "Generating " + benchmark_bin + " for CUDA, board:" + board
 
@@ -41,14 +43,16 @@ def main(board):
                 "mv -f ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
 
+    # gen only for max size
+    max_size = max(SIZES)
     for i in SIZES:
         input_file = data_path + "/"
 
         gen = [None] * 6
         gen[0] = ['sudo ', src_dgemm + "/" + GENERATE_BIN_NAME + " "]
         gen[1] = ['-size=' + str(i)]
-        gen[2] = ['-input_a=' + input_file + benchmark_bin + 'A_' + str(i) + '.matrix']
-        gen[3] = ['-input_b=' + input_file + benchmark_bin + 'B_' + str(i) + '.matrix']
+        gen[2] = ['-input_a=' + input_file + benchmark_bin + 'A_' + str(max_size) + '.matrix']
+        gen[3] = ['-input_b=' + input_file + benchmark_bin + 'B_' + str(max_size) + '.matrix']
         gen[4] = ['-gold=' + input_file + "GOLD_" + str(i) + ".matrix"]  # change for execute
         gen[5] = []
 
@@ -60,40 +64,16 @@ def main(board):
         generate.append(' '.join(str(r) for v in gen for r in v))
         execute.append(' '.join(str(r) for v in exe for r in v))
 
-    execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin)
-
-
-def execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin):
-    for i in generate:
-        print i
-        if not DEBUG_MODE:
-            if os.system(str(i)) != 0:
-                print "Something went wrong with generate of ", str(i)
-                exit(1)
-
-    list_to_print = ["[\n"]
-    for ii, i in enumerate(execute):
-        command = "{\"killcmd\": \"killall -9 " + benchmark_bin + "\", \"exec\": \"" + str(i) + "\"}"
-        if ii != len(execute) - 1:
-            command += ',\n'
-        list_to_print.append(command)
-    list_to_print.append("\n]")
-
-    with open(install_dir + "scripts/json_files/" + benchmark_bin + ".json", 'w') as fp:
-        fp.writelines(list_to_print)
-
-    print "\nConfiguring done, to run check file: " + install_dir + "scripts/json_files/" + benchmark_bin + ".json"
+    execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)
 
 
 if __name__ == "__main__":
-    global DEBUG_MODE
-
-    parameter = sys.argv[1:]
     try:
-        DEBUG_MODE = sys.argv[2:]
+        parameter = str(sys.argv[1:][0]).upper() 
+        if parameter == 'DEBUG':
+            debug_mode = True
     except:
-        DEBUG_MODE = False
-    if len(parameter) < 1:
-        print "./config_generic <k1/x1/x2/k40/titan>"
-    else:
-        main(str(parameter[0]).upper())
+        debug_mode = False
+    
+    board, _ = discover_board()
+    config(board=board, debug=debug_mode)
