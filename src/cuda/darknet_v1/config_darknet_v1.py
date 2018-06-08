d85be01fa2514ff5d8cd45b1425cec334f09120b
carol <fernandofernandesantos@gmail.com>
2018-06-08 10:03:22 +0000

    config

--- src/cuda/darknet_v1/config_darknet_v1.py
+++ src/cuda/darknet_v1/config_darknet_v1.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-
 import os
-import sys
 import ConfigParser
 import sys
 import copy
@@ -31,6 +29,8 @@ SAVE_LAYER = [0, 1]
 USE_TENSOR_CORES = [0, 1]
 # 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
 ABFT = [0]  # , 2]
+WEIGHTS = "yolo_v1.weights"
+CFG = "yolo_v1.cfg"
 
 
 def download_weights(src_dir, data_dir):
@@ -43,22 +43,22 @@ def download_weights(src_dir, data_dir):
 
 
 def config(board, debug, download_data):
-    print "Generating darknet for CUDA, board:" + board
+    print "Generating darknet v1 for CUDA, board:" + board
 
     conf_file = '/etc/radiation-benchmarks.conf'
     try:
         config = ConfigParser.RawConfigParser()
         config.read(conf_file)
-
         install_dir = config.get('DEFAULT', 'installdir') + "/"
 
     except IOError as e:
         print >> sys.stderr, "Configuration setup error: " + str(e)
         sys.exit(1)
 
+    benchmark_bin = BINARY_NAME
     data_path = install_dir + "data/darknet"
     bin_path = install_dir + "bin"
-    src_darknet = install_dir + "src/cuda/" + BINARY_NAME
+    src_darknet = install_dir + "src/cuda/" + benchmark_bin
 
     if not os.path.isdir(data_path):
         os.mkdir(data_path, 0777)
@@ -68,10 +68,13 @@ def config(board, debug, download_data):
     if download_data:
         download_weights(src_dir=src_darknet, data_dir=data_path)
 
-    generate = ["sudo mkdir -p " + bin_path, "mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet,
-                "make clean GPU=1", "make -j 4 GPU=1 SAFE_MALLOC=1",
-                "mv ./" + BINARY_NAME + " " + bin_path + "/"]
+    # change it for darknetv2
+    generate = ["mkdir -p " + bin_path, "mkdir -p /var/radiation-benchmarks/data", "cd " + src_darknet,
+                "make clean GPU=1", "make -j4 GPU=1  SAFE_MALLOC=1",
+                "mv ./" + benchmark_bin + "  " + bin_path + "/"]
     execute = []
+
+    # 0 - "none",  1 - "gemm", 2 - "smart_pooling", 3 - "l1", 4 - "l2", 5 - "trained_weights"}
 
     for save_layer in SAVE_LAYER:
         for abft in ABFT:
@@ -83,12 +86,12 @@ def config(board, debug, download_data):
                     gold = data_path + '/' + BINARY_NAME + '_tensor_cores_mode_' + str(tc) + '_' + i['gold']
                     txt_list = install_dir + 'data/networks_img_list/' + i['txt']
                     gen = {
-                        'bin': ["sudo " + bin_path, "/" + BINARY_NAME],
+                        'bin': ["sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} " + bin_path, "/" + benchmark_bin],
                         'e': [' -e ', 'yolo'],  # execution_type =
                         'aa': ['test_radiation', ''],
                         'm': [' -m ', 'valid'],  # execution_model =
-                        'c': [' -c ', data_path + '/yolo_v1.cfg'],  # config_file =
-                        'w': [' -w ', data_path + '/yolo_v1.weights'],  # weights =
+                        'c': [' -c ', data_path + '/' + CFG],  # config_file =
+                        'w': [' -w ', data_path + '/' + WEIGHTS],  # weights =
                         'n': [' -n ', '1'],
                         'g': [' -g ', gold],
                         'l': [' -l ', txt_list],
@@ -96,7 +99,7 @@ def config(board, debug, download_data):
                         'x': [' -x ', 0],
                         's': [' -s ', save_layer],
                         'a': [' -a ', abft],
-                        't': [' -t ', tc],
+                        't': [' -t ', tc]
                     }
 
                     exe = copy.deepcopy(gen)
@@ -110,14 +113,15 @@ def config(board, debug, download_data):
                         generate.append(" ".join([''.join(map(str, value)) for key, value in gen.iteritems()]))
 
                     execute.append(" ".join([''.join(map(str, value)) for key, value in exe.iteritems()]))
-
+                   
     generate.append("make clean GPU=1 SAFE_MALLOC=1")
     generate.append("make -C ../../include/")
     generate.append("make -j 4 GPU=1 SAFE_MALLOC=1 LOGS=1")
-    generate.append("mv ./" + BINARY_NAME + " " + bin_path + "/")
+    generate.append("mv ./" + benchmark_bin + " " + bin_path + "/")
 
     execute_and_write_json_to_file(execute=execute, generate=generate, install_dir=install_dir,
-                                   benchmark_bin=BINARY_NAME, debug=debug)
+                                   benchmark_bin=benchmark_bin, debug=debug)
+
 
 if __name__ == "__main__":
     try:
