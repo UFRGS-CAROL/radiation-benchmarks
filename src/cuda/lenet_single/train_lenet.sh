6dda83e406436f0bb477f4fbfb3e79aa110b278c
Fernando Fernandes <fernandofernandesantos@gmail.com>
2018-06-16 15:56:50 +0000

    lenet

---
+++ src/cuda/lenet_single/train_lenet.sh
@@ -0,0 +1,3 @@
+sudo env PYTHONPATH=/home/carol/radiation-benchmarks/src/cuda/lenet_double/caffe/python:/home/carol/radiation-benchmarks/src/include/log_helper_swig_wraper:$PYTHONPATH LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} /home/carol/radiation-benchmarks/src/cuda/lenet_single/lenet_single.py   --ite 1  --testmode  3 
+
+# --ite  1 --prototxt  /home/carol/radiation-benchmarks/src/cuda/lenet_double/caffe/examples/mnist/lenet.prototxt --lenet_model  /home/carol/radiation-benchmarks/src/cuda/lenet_double/caffe/examples/mnist/lenet_iter_10000.caffemodel --lmdb  /home/carol/radiation-benchmarks/src/cuda/lenet_double/caffe/examples/mnist/mnist_test_lmdb/ --gold  /home/carol/radiation-benchmarks/data/lenet/gold_lenet_double_10k.gold
