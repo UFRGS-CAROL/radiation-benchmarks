2db03a9bc973be9e155d612d05a3c3355e5d448d
Caio Lunardi <caio.b.lunardi@gmail.com>
2018-09-12 14:53:10 +0200

    fixes on mxm-hard

--- src/cuda/mxm-hard/cuda_mxm-hard.cu
+++ src/cuda/mxm-hard/cuda_mxm-hard.cu
@@ -353,8 +353,8 @@ void generateInputMatrices() {
 
 	std::random_device rd; //Will be used to obtain a seed for the random number engine
 	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-	std::uniform_real_distribution<double> dis(-GENERATOR_MAXABSVALUE,
-			GENERATOR_MAXABSVALUE);
+	// std::uniform_real_distribution<double> dis(-GENERATOR_MAXABSVALUE, GENERATOR_MAXABSVALUE);
+	std::uniform_real_distribution<double> dis(GENERATOR_MINABSVALUE, GENERATOR_MAXABSVALUE);
 
 	if (!generator_debug) {
 		for (int i = 0; i < DEFAULT_INPUT_SIZE; i++) {
