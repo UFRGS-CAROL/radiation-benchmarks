0fcc3e07b70b62c279c10b5557547aaa36b58552
carol <fernandofernandesantos@gmail.com>
2018-10-04 21:58:59 +0000

    makefile, gemm and main modified

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -9,8 +9,11 @@
 #define GEMMWMMA_H_
 
 #include <type_traits>
-
+#include <string>
 #include <cstdio>
+#include <iostream>
+#include <mma.h>
+#include <cuda_fp16.h> // For half precision computation
 
 
 // The only dimensions currently supported by WMMA
@@ -19,10 +22,11 @@
 #define WMMA_K 16
 
 
-//ERROR functions definitions
-#define check_framework_errors(error) __check_framework_errors(error, __LINE__, __FILE__)
-#define error(error) __error(error, __LINE__, __FILE__)
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 32
+#endif
 
+//ERROR functions definitions
 void __check_framework_errors(cudaError_t error, int line, const char* file) {
 	if (error == cudaSuccess) {
 		return;
@@ -39,16 +43,103 @@ void __error(const char* error, int line, const char* file) {
 	exit (EXIT_FAILURE);
 }
 
+__device__ unsigned long long int is_memory_bad = 0;
+
+template<class real_t>
+__device__ real_t inline read_voter(real_t *v1, real_t *v2, real_t *v3,
+		int offset) {
+
+	register real_t in1 = v1[offset];
+	register real_t in2 = v2[offset];
+	register real_t in3 = v3[offset];
+
+	if (in1 == in2 || in1 == in3) {
+		return in1;
+	}
+
+	if (in2 == in3) {
+		return in2;
+	}
+
+	if (in1 != in2 && in2 != in3 && in1 != in3) {
+		atomicAdd(&is_memory_bad, 1);
+	}
+
+	return in1;
+}
+
+//__device__ half inline read_voter(half *v1, half *v2, half *v3, int offset) {
+//
+//	register half in1 = v1[offset];
+//	register half in2 = v2[offset];
+//	register half in3 = v3[offset];
+//
+//	if (__heq(in1, in2) || __heq(in1, in3)) {
+//		return in1;
+//	}
+//
+//	if (__heq(in2, in3)) {
+//		return in2;
+//	}
+//
+//	if (__hne(in1, in2) && __hne(in2, in3) && __hne(in1, in3)) {
+//		atomicAdd(&is_memory_bad, 1);
+//	}
+//
+//	return in1;
+//}
+
+
+template<class half_t, class real_t>
+__global__ void wmma_matrix_mul(
+		half_t *a0, half_t *a1, half_t *a2,
+		half_t *b0, half_t *b1, half_t *b2,
+		real_t *c0, real_t *c1, real_t *c2,
+		real_t*d0, real_t *d1, real_t *d2,
+		size_t M, size_t N, size_t K, float alpha, float beta){
+
+	register int tx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+	register int ty = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+	register int k;
+//	printf("entrou wmma mult");
+	register real_t acc = 0.0;
+	for (k = 0; k < N; k++) {
+		half_t tmp = read_voter(a0, a1, a2, ty * N + k)
+						* read_voter(b0, b1, b2, k * N + tx);
+		acc = real_t(tmp) + acc;
+	}
+//	printf("passou for wmma mult");
+	acc += read_voter(c0, c1, c2, ty * N + tx);
+
+	d0[ty * N + tx] = acc;
+	d1[ty * N + tx] = acc;
+	d2[ty * N + tx] = acc;
+}
+
+#define check_framework_errors(error) __check_framework_errors(error, __LINE__, __FILE__)
+#define error(error) __error(error, __LINE__, __FILE__)
+
 template<class host_half_t, class half_t, class real_t>
 class GEMMWMMA {
 
 public:
 
 	// Memory pointers to device and host data
-	half_t* device_ptr_a = nullptr;
-	half_t* device_ptr_b = nullptr;
-	real_t* device_ptr_c = nullptr;
-	real_t* device_ptr_d = nullptr;
+	half_t* device_ptr_a0 = nullptr;
+	half_t* device_ptr_a1 = nullptr;
+	half_t* device_ptr_a2 = nullptr;
+
+	half_t* device_ptr_b0 = nullptr;
+	half_t* device_ptr_b1 = nullptr;
+	half_t* device_ptr_b2 = nullptr;
+
+	real_t* device_ptr_c0 = nullptr;
+	real_t* device_ptr_c1 = nullptr;
+	real_t* device_ptr_c2 = nullptr;
+
+	real_t* device_ptr_d0 = nullptr;
+	real_t* device_ptr_d1 = nullptr;
+	real_t* device_ptr_d2 = nullptr;
 
 	// Size of the matrix
 	size_t cols_a, rows_a;
@@ -57,23 +148,291 @@ public:
 
 	size_t byte_size_c;
 
-	GEMMWMMA(const host_half_t* host_ptr_a, const host_half_t* host_ptr_b,
-			const host_half_t* host_ptr_c, size_t rows_a, size_t cols_a,
-			size_t cols_b);
+	bool to_debug = false;
 
-	virtual ~GEMMWMMA();
+//	//const host_half_t* host_ptr_a1, const host_half_t*host_ptr_a2,
+//	//const host_half_t* host_ptr_b1, const host_half_t* host_ptr_b2,
+//	//const host_half_t* host_ptr_c1, const host_half_t* host_ptr_c2,
+//	GEMMWMMA(const host_half_t* host_ptr_a0, const host_half_t* host_ptr_b0,
+//			const real_t* host_ptr_c0, size_t rows_a, size_t cols_a,
+//			size_t cols_b);
+//
+//	virtual ~GEMMWMMA();
+//	/**
+//	 * Template multiplication
+//	 */
+//	void mul();
+//
+//	//const host_half_t* host_ptr_a1, const host_half_t* host_ptr_a2,
+//	//const host_half_t* host_ptr_b1, const host_half_t* host_ptr_b2,
+//	//const host_half_t* host_ptr_c1, const host_half_t* host_ptr_c2
+//
+//	void push_arrays(const host_half_t* host_ptr_a0, const host_half_t* host_ptr_b0,
+//			const real_t* host_ptr_c0);
+//
+//	void pull_array(real_t* host_ptr_d0, real_t* host_ptr_d1,
+//			real_t* host_ptr_d2);
+//
+//	void debug(std::string str);
+//
+//	unsigned long long int get_memory_errors();
+
+	void mul() {
+		//		//No double multiplication is allowed
+		if (std::is_same<half_t, double>::value
+				|| std::is_same<half_t, float>::value) {
+			throw "Double/Float multiplication is not allowed with tensor cores, use GEMM base class instead";
+		}
+
+		this->debug("thread dim allocation");
+		//		// Setup execution parameters
+		dim3 gridDim;
+		dim3 blockDim;
+
+		// blockDim.x must be a multple of warpSize
+		// 128x4 means we have 16 warps and a block computes a 64x64 output tile
+		blockDim.x = 128;
+		blockDim.y = 4;
+
+		gridDim.x = (this->rows_a + (WMMA_M * blockDim.x / 32 - 1))
+				/ (WMMA_M * blockDim.x / 32);
+
+		gridDim.y = (this->cols_b + WMMA_N * blockDim.y - 1)
+				/ (WMMA_N * blockDim.y);
+
+		this->debug("matrix multiplication");
+
+		printf("entrou funcao mul");
+		wmma_matrix_mul<half_t, real_t> <<<gridDim, blockDim>>>(
+				this->device_ptr_a0, this->device_ptr_a1, this->device_ptr_a2,
+				this->device_ptr_b0, this->device_ptr_b1, this->device_ptr_b2,
+				this->device_ptr_c0, this->device_ptr_c1, this->device_ptr_c2,
+				this->device_ptr_d0, this->device_ptr_d1, this->device_ptr_d2,
+				this->rows_a, this->cols_b, this->rows_b, 1.0, 1.0);
+
+		printf("passou chamada wmma mult");		
+		this->debug("device synchronize");
+		check_framework_errors(cudaDeviceSynchronize());
+
+		printf("passou sync mul");
+		this->byte_size_c = this->rows_c * this->cols_c * sizeof(float);
+
+	}
+
+	GEMMWMMA(const host_half_t* host_ptr_a0, const host_half_t* host_ptr_b0,
+			const real_t* host_ptr_c0, size_t rows_a, size_t cols_a,
+			size_t cols_b) {
+
+		this->rows_a = rows_a;
+		this->cols_a = cols_a;
+		this->rows_b = this->cols_a;
+		this->cols_b = cols_b;
+		this->cols_c = this->rows_a;
+		this->rows_c = this->cols_b;
+
+		if (rows_a > 0 && cols_a > 0 && cols_b > 0) {
+			this->debug("device memory allocation");
+			// A's allocation
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_a0),
+							this->rows_a * this->cols_a * sizeof(half_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_a1),
+							this->rows_a * this->cols_a * sizeof(half_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_a2),
+							this->rows_a * this->cols_a * sizeof(half_t)));
+
+			// B's allocation
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_b0),
+							this->rows_b * this->cols_b * sizeof(half_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_b1),
+							this->rows_b * this->cols_b * sizeof(half_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_b2),
+							this->rows_b * this->cols_b * sizeof(half_t)));
+
+			// C's allocation
+			//Alloc memory for real_t DOUBLE OR SINGLE
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_c0),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_c1),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_c2),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+
+			// D's allocation
+			//Alloc memory for real_t DOUBLE OR SINGLE
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d0),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d1),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+			check_framework_errors(
+					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d2),
+							this->rows_c * this->cols_c * sizeof(real_t)));
+
+			//to pull C array directly
+			this->byte_size_c = this->rows_c * this->cols_c * sizeof(real_t);
+
+			this->debug("push memory to device");
+			//set 0 to C matrix
+			this->push_arrays(host_ptr_a0, host_ptr_b0, host_ptr_c0);
+		} else {
+			error("columns or rows equal to zero, or less than zero");
+		}
+
+	}
+
 	/**
-	 * Template multiplication
+	 * PUSH arrays to gpu and set 0x0 to C matrix
 	 */
-	void mul();
 
-	void push_arrays(const host_half_t* host_ptr_a,
-			const host_half_t* host_ptr_b, const host_half_t* host_ptr_c);
+	void push_arrays(const host_half_t* host_ptr_a0,
+			const host_half_t* host_ptr_b0, const real_t* host_ptr_c0) {
 
+		this->debug("memset array D");
+		//set 0 to C's matrix
+		check_framework_errors(
+				cudaMemset(this->device_ptr_d0, 0x00,
+						this->rows_c * this->cols_c * sizeof(real_t)));
+		check_framework_errors(
+				cudaMemset(this->device_ptr_d1, 0x00,
+						this->rows_c * this->cols_c * sizeof(real_t)));
+		check_framework_errors(
+				cudaMemset(this->device_ptr_d2, 0x00,
+						this->rows_c * this->cols_c * sizeof(real_t)));
 
-	void pull_array(host_half_t* host_ptr_d);
+		this->debug("memcpy arrays A");
+
+		//PUSH A
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_a0, host_ptr_a0,
+						this->rows_a * this->cols_a * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_a0, host_ptr_a0,
+						this->rows_a * this->cols_a * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_a0, host_ptr_a0,
+						this->rows_a * this->cols_a * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+
+		this->debug("memcpy arrays B");
+
+		//PUSH B's
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_b0, host_ptr_b0,
+						this->rows_b * this->cols_b * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_b0, host_ptr_b0,
+						this->rows_b * this->cols_b * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_b0, host_ptr_b0,
+						this->rows_b * this->cols_b * sizeof(half_t),
+						cudaMemcpyHostToDevice));
+
+		this->debug("memcpy arrays C");
+		//PUSH C's
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_c0, host_ptr_c0,
+						this->rows_c * this->cols_c * sizeof(real_t),
+						cudaMemcpyHostToDevice));
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_c0, host_ptr_c0,
+						this->rows_c * this->cols_c * sizeof(real_t),
+						cudaMemcpyHostToDevice));
+		check_framework_errors(
+				cudaMemcpy(this->device_ptr_c0, host_ptr_c0,
+						this->rows_c * this->cols_c * sizeof(real_t),
+						cudaMemcpyHostToDevice));
+	}
+
+	/**
+	 * PULL D array to host
+	 */
+
+	void pull_array(real_t* host_ptr_d0, real_t* host_ptr_d1,
+			real_t* host_ptr_d2) {
+
+		this->debug("memcpy array D to host");
+		// PULL D's
+		check_framework_errors(
+				cudaMemcpy(host_ptr_d0, this->device_ptr_d0, this->byte_size_c,
+						cudaMemcpyDeviceToHost));
+		check_framework_errors(
+				cudaMemcpy(host_ptr_d1, this->device_ptr_d1, this->byte_size_c,
+						cudaMemcpyDeviceToHost));
+		check_framework_errors(
+				cudaMemcpy(host_ptr_d2, this->device_ptr_d2, this->byte_size_c,
+						cudaMemcpyDeviceToHost));
+	}
+
+	/**
+	 * Destructor for the GEMM class
+	 */
+
+	virtual ~GEMMWMMA() {
+
+		this->debug("destructor");
+		// A's destructors
+		if (this->device_ptr_a0 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_a0));
+		if (this->device_ptr_a1 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_a1));
+		if (this->device_ptr_a2 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_a2));
+
+		// B's destructors
+		if (this->device_ptr_b0 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_b0));
+		if (this->device_ptr_b1 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_b1));
+		if (this->device_ptr_b2 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_b2));
+
+		// C's destructors
+		if (this->device_ptr_c0 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_c0));
+		if (this->device_ptr_c1 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_c1));
+		if (this->device_ptr_c2 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_c2));
+
+		// D's destructors
+		if (this->device_ptr_d0 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_d0));
+		if (this->device_ptr_d1 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_d1));
+		if (this->device_ptr_d2 != nullptr)
+			check_framework_errors(cudaFree(this->device_ptr_d2));
+	}
+
+	void debug(std::string str) {
+		if (this->to_debug) {
+			std::cout << str << std::endl;
+		}
+	}
+
+	unsigned long long int get_memory_errors() {
+		unsigned long long int host_is_memory_bad;
+		check_framework_errors(
+				cudaMemcpyFromSymbol(&host_is_memory_bad, "is_memory_bad",
+						sizeof(unsigned long long int), 0,
+						cudaMemcpyDeviceToHost));
+		return host_is_memory_bad;
+	}
 
 };
-
 
 #endif /* GEMMWMMA_H_ */
--- src/cuda/gemm_tensorcores/main.cu
+++ src/cuda/gemm_tensorcores/main.cu
@@ -1,78 +1,379 @@
-#include "GEMM.cuh"
-#include "GEMMWMMA.cuh"
-
 #include <iostream>
-#include <cuda_fp16.h>
 #include <string>
+#include <vector>
+#include <random>
+#include <fstream>      // std::ifstream
 
-void del_arg(int argc, char **argv, int index) {
-	int i;
-	for (i = index; i < argc - 1; ++i)
-		argv[i] = argv[i + 1];
-	argv[i] = 0;
+#include "half.hpp"
+#include "Log.h"
+#include "GEMMWMMA.h"
+
+#define DEFAULT_INPUT_SIZE 8192
+#define GENERATOR_MAXABSVALUE 2.0
+#define GENERATOR_MINABSVALUE 0
+
+typedef half_float::half host_half;
+
+typedef std::vector<host_half> half_vector;
+typedef std::vector<float> float_vector;
+typedef std::vector<double> double_vector;
+
+template<class real_t> void generate_matrices_files(std::string a_path,
+		std::string b_path, std::string c_path, half_vector& a_host_vector,
+		half_vector& b_host_vector, std::vector<real_t>& c_host_vector,
+		int matrix_size) {
+
+	std::ofstream f_a(a_path, std::ios::out | std::ios::binary);
+	std::ofstream f_b(b_path, std::ios::out | std::ios::binary);
+	std::ofstream f_c(c_path, std::ios::out | std::ios::binary);
+//	std::cout << "entrou generate" << std::endl;
+
+	if (f_a.is_open() && f_b.is_open() && f_c.is_open()) {
+		std::random_device rd; //Will be used to obtain a seed for the random number engine
+		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+		std::uniform_real_distribution<double> dis(-GENERATOR_MAXABSVALUE,
+		GENERATOR_MAXABSVALUE);
+		std::cout << "entrou if generate1" << std::endl;
+
+		for (size_t i = 0; i < matrix_size; i++) {
+			for (size_t j = 0; j < matrix_size; j++) {
+				a_host_vector[i * matrix_size + j] = host_half(dis(gen));
+				b_host_vector[i * matrix_size + j] = host_half(dis(gen));
+				c_host_vector[i * matrix_size + j] = real_t(dis(gen));
+			}
+		}
+		std::cout << "entrou generate1" << std::endl;
+		host_half zero(0.0);
+		host_half nan_ = host_half(half_float::nanh("0"));
+		host_half inf_ = host_half(host_half(0x7C00));
+		std::cout << "entrou generate2" << std::endl;
+
+		int numZeros = std::count(a_host_vector.begin(), a_host_vector.end(),
+				zero);
+		int numNans = std::count(a_host_vector.begin(), a_host_vector.end(),
+				nan_);
+
+		int numInfs = std::count(a_host_vector.begin(), a_host_vector.end(),
+				inf_);
+		std::cout << "Number of zeros/NaNs/INFs on matrix A: " << numZeros
+				<< numNans << numInfs << std::endl;
+
+		std::cout << "entrou generate3" << std::endl;
+		numZeros = std::count(b_host_vector.begin(), b_host_vector.end(),zero);
+		numNans = std::count(b_host_vector.begin(), b_host_vector.end(), nan_);
+		numInfs = std::count(b_host_vector.begin(), b_host_vector.end(), inf_);
+
+		std::cout << "Number of zeros/NaNs/INFs on matrix B: " << numZeros
+				<< numNans << numInfs << std::endl;
+
+		numZeros = std::count(c_host_vector.begin(), c_host_vector.end(), zero);
+		numNans = std::count(c_host_vector.begin(), c_host_vector.end(),nan_);
+		numInfs = std::count(c_host_vector.begin(), c_host_vector.end(), inf_);
+
+		std::cout << "Number of zeros/NaNs/INFs on matrix C: " << numZeros
+				<< numNans << numInfs << std::endl;
+
+		f_a.write(reinterpret_cast<char*>(a_host_vector.data()),
+				a_host_vector.size() * sizeof(host_half));
+		f_b.write(reinterpret_cast<char*>(b_host_vector.data()),
+				b_host_vector.size() * sizeof(host_half));
+		f_c.write(reinterpret_cast<char*>(c_host_vector.data()),
+				c_host_vector.size() * sizeof(real_t));
+
+		f_a.close();
+		f_b.close();
+		f_c.close();
+
+	} else {
+		throw "Some of the imput files could not be generated\n";
+	}
+
 }
 
-int find_int_arg(int argc, char **argv, std::string arg, int def) {
-	int i;
-	for (i = 0; i < argc - 1; ++i) {
-		if (!argv[i])
-			continue;
-		if (std::string(argv[i]) == arg) {
-			def = atoi(argv[i + 1]);
-			del_arg(argc, argv, i);
-			del_arg(argc, argv, i);
-			break;
-		}
+template<class real_t>
+void write_gold_to_file(std::string gold_path, std::vector<real_t>& gold) {
+	std::ofstream f_gold(gold_path, std::ofstream::out | std::ofstream::binary);
+	if (f_gold.is_open()) {
+		f_gold.write(reinterpret_cast<char*>(gold.data()),
+				sizeof(real_t) * gold.size());
+	} else {
+		throw "Could not write gold file\n";
 	}
-	return def;
 }
 
-std::string find_char_arg(int argc, char **argv, std::string arg, std::string def) {
-	int i;
-	for (i = 0; i < argc - 1; ++i) {
-		if (!argv[i])
-			continue;
-		if (std::string(argv[i]) == arg) {
-			def = std::string(argv[i + 1]);
-			del_arg(argc, argv, i);
-			del_arg(argc, argv, i);
-			break;
-		}
+template<class real_t> void retrieve_matrices(std::string a_path,
+		std::string b_path, std::string c_path, std::string gold_path,
+		half_vector& a_host_vector, half_vector& b_host_vector,
+		std::vector<real_t>& c_host_vector,
+		std::vector<real_t>& gold_host_vector, Log *log) {
+
+	double start = log->mysecond();
+	std::ifstream f_a(a_path, std::ios::in | std::ios::binary);
+	std::ifstream f_b(b_path, std::ios::in | std::ios::binary);
+	std::ifstream f_c(c_path, std::ios::in | std::ios::binary);
+	std::ifstream f_gold(gold_path, std::ifstream::in | std::ifstream::binary);
+
+	if (f_a.is_open() && f_b.is_open() && f_c.is_open() && f_gold) {
+
+		f_a.seekg(0, std::ios::beg);
+		f_a.read(reinterpret_cast<char*>(a_host_vector.data()),
+				sizeof(host_half) * a_host_vector.size());
+
+		f_b.seekg(0, std::ios::beg);
+		f_b.read(reinterpret_cast<char*>(b_host_vector.data()),
+				sizeof(host_half) * b_host_vector.size());
+
+		f_c.seekg(0, std::ios::beg);
+		f_c.read(reinterpret_cast<char*>(c_host_vector.data()),
+				sizeof(host_half) * c_host_vector.size());
+
+		f_gold.seekg(0, std::ios::beg);
+		f_gold.read(reinterpret_cast<char*>(gold_host_vector.data()),
+				sizeof(host_half) * gold_host_vector.size());
+
+		f_a.close();
+		f_b.close();
+		f_c.close();
+		f_gold.close();
+	} else {
+		if (log != nullptr)
+			log->log_error("Could not retrieve the matrices");
+		throw "Could not retrieve the matrices\n";
 	}
-	return def;
+
+	std::cout << "Done with reading matrices " << log->mysecond() - start
+			<< "s\n";
 }
 
+template<class real_t>
+bool compare_output_matrices(long long host_is_memory_bad, bool generate,
+		std::vector<real_t>& gold, std::vector<real_t>& c0,
+		std::vector<real_t>& c1, std::vector<real_t>& c2,
+		std::vector<real_t>& voted_output, Log *log, int matrix_size,
+		bool check = true, bool verbose = true) {
 
+	int host_errors = 0;
+	int memory_errors = 0;
 
-int find_arg(int argc, char* argv[], std::string arg) {
-	int i;
-	for (i = 0; i < argc; ++i) {
-		if (!argv[i])
-			continue;
-		if (std::string(argv[i]) == arg) {
-			del_arg(argc, argv, i);
-			return 1;
+	if (host_is_memory_bad != 0) {
+		std::string info_detail = "b: is_memory_bad: "
+				+ std::to_string(host_is_memory_bad);
+		if (verbose)
+			std::cout << info_detail << std::endl;
+
+		if (!generate)
+			log->log_error(info_detail);
+		memory_errors++;
+	}
+
+#pragma omp parallel for shared(host_errors)
+	for (size_t i = 0; i < gold.size(); i++) {
+		register bool checkFlag = true;
+		register real_t valGold = gold[i];
+		register real_t valOutput0 = c0[i];
+		register real_t valOutput1 = c1[i];
+		register real_t valOutput2 = c2[i];
+		register real_t valOutput = valOutput0;
+		if ((valOutput0 != valOutput1) || (valOutput0 != valOutput2)) {
+#pragma omp critical
+			{
+				char info_detail[200];
+				snprintf(info_detail, 150,
+						"m: [%d, %d], r0: %1.20e, r1: %1.20e, r2: %1.20e",
+						(int) floor(i / matrix_size), i % matrix_size,
+						(double) valOutput0, (double) valOutput1,
+						(double) valOutput2);
+				if (verbose && (memory_errors < 10))
+					std::cout << info_detail << std::endl;
+
+				if (!generate)
+					log->log_info(info_detail);
+				memory_errors++;
+			}
+			if ((valOutput0 != valOutput1) && (valOutput1 != valOutput2)
+					&& (valOutput0 != valOutput2)) {
+				// All 3 values diverge
+				if (valOutput0 == valGold) {
+					valOutput = valOutput0;
+				} else if (valOutput1 == valGold) {
+					valOutput = valOutput1;
+				} else if (valOutput2 == valGold) {
+					valOutput = valOutput2;
+				} else {
+					// NO VALUE MATCHES THE GOLD AND ALL 3 DIVERGE!
+					checkFlag = false;
+#pragma omp critical
+					{
+						char info_detail[200];
+						snprintf(info_detail, 150,
+								"t: [%d, %d], r0: %1.20e, r1: %1.20e, r2: %1.20e, e: %1.20e",
+								(int) floor(i / matrix_size), i % matrix_size,
+								(double) valOutput0, (double) valOutput1,
+								(double) valOutput2, (double) valGold);
+						if (verbose && (memory_errors < 10))
+							std::cout << info_detail << std::endl;
+
+						if (!generate)
+							log->log_info(std::string(info_detail));
+
+						memory_errors++;
+					}
+				}
+			} else if (valOutput1 == valOutput2) {
+				// Only value 0 diverge
+				valOutput = valOutput1;
+			} else if (valOutput0 == valOutput2) {
+				// Only value 1 diverge
+				valOutput = valOutput0;
+			} else if (valOutput0 == valOutput1) {
+				// Only value 2 diverge
+				valOutput = valOutput0;
+			}
+		}
+		if (voted_output.size() == gold.size())
+			voted_output[i] = valOutput;
+		// if ((fabs((real_t)(valOutput-valGold)/valGold) > 1e-10)||(fabs((real_t)(valOutput-valGold)/valGold) > 1e-10)) {
+		if (check) {
+			if (valGold != valOutput) {
+				if (checkFlag) {
+#pragma omp critical
+					{
+						char error_detail[200];
+						snprintf(error_detail, 150,
+								"p: [%d, %d], r: %1.20e, e: %1.20e",
+								(int) floor(i / matrix_size), i % matrix_size,
+								(double) valOutput, (double) valGold);
+
+						if (verbose && (host_errors < 10))
+							std::cout << error_detail << std::endl;
+						if (!generate)
+							log->log_error(error_detail);
+						host_errors++;
+					}
+				}
+			}
 		}
 	}
-	return 0;
+
+	// printf("numErrors:%d", host_errors);
+
+	if (!generate) {
+		log->update_info_count(memory_errors);
+		log->update_error_count(host_errors);
+	}
+
+	if (memory_errors != 0)
+		std::cout << "M";
+	if (host_errors != 0)
+		std::cout << "#";
+
+	return (host_errors == 0) && (host_is_memory_bad == 0);
 }
 
-float find_float_arg(int argc, char **argv, char *arg, float def) {
-	int i;
-	for (i = 0; i < argc - 1; ++i) {
-		if (!argv[i])
-			continue;
-		if (0 == strcmp(argv[i], arg)) {
-			def = atof(argv[i + 1]);
-			del_arg(argc, argv, i);
-			del_arg(argc, argv, i);
-			break;
-		}
-	}
-	return def;
+void usage(char **argv){
+	std::cout << "./" << argv[0] << " --generate 0/1 --size <matrix size> --iterations <how many iterations> --input_a <input A> "
+			"--input_b <input B> --input_c <input C> --gold <gold file> --precision <float/double>" << std::endl;
 }
 
 int main(int argc, char** argv) {
 
+//	if (argc < 7){
+//		usage(argv);
+//		throw "Wrong parameters\n";
+//	}
+
+	Log *log_obj = new Log();
+
+	bool generate = log_obj->find_int_arg(argc, argv, "--generate", 0);
+
+	size_t size_matrices = log_obj->find_int_arg(argc, argv, "--size",
+	DEFAULT_INPUT_SIZE);
+
+	int iterations = log_obj->find_int_arg(argc, argv, "--iterations", 1);
+
+	std::string a_input_path = log_obj->find_char_arg(argc, argv, "--input_a",
+			"./input_a.matrix");
+	std::string b_input_path = log_obj->find_char_arg(argc, argv, "--input_b",
+			"./input_b.matrix");
+	std::string c_input_path = log_obj->find_char_arg(argc, argv, "--input_c",
+			"./input_c.matrix");
+	std::string gold_inout_path = log_obj->find_char_arg(argc, argv, "--gold",
+			"./gold.matrix");
+
+	std::string precision = log_obj->find_char_arg(argc, argv, "--precision",
+			"float");
+
+	std::cout << "Generate: " << generate << std::endl;
+	std::cout << "A input path: " << a_input_path << std::endl;
+	std::cout << "B input path: " <<  b_input_path<< std::endl;
+	std::cout << "C input path: " <<  c_input_path << std::endl;
+	std::cout << "Gold in/out path: " <<  gold_inout_path << std::endl;
+	std::cout << "Iterations: " << iterations << std::endl;
+	std::cout << "Matrix size: " << size_matrices << std::endl;
+	std::cout << "Precision: " << precision << std::endl;
+
+	// Alloc all memories on host
+	half_vector host_matrix_a(size_matrices * size_matrices);
+	half_vector host_matrix_b(size_matrices * size_matrices);
+
+	if (precision == "float") {
+
+
+		// C matrix
+		float_vector host_matrix_c(size_matrices * size_matrices);
+		float_vector host_gold(size_matrices * size_matrices);
+		// D Matrix
+		float_vector host_matrix_d1(size_matrices * size_matrices);
+		float_vector host_matrix_d2(size_matrices * size_matrices);
+		float_vector host_matrix_d3(size_matrices * size_matrices);
+//		std::cout << "passou declaracao host" << std::endl;
+
+		if (!generate) {
+			retrieve_matrices(a_input_path, b_input_path, c_input_path,
+					gold_inout_path, host_matrix_a, host_matrix_b,
+					host_matrix_c, host_gold, log_obj);
+
+		} else {
+//			std::cout << "entrou else" << std::endl;
+			generate_matrices_files(a_input_path, b_input_path, c_input_path,
+					host_matrix_a, host_matrix_b, host_matrix_c, size_matrices);
+		}
+		//GOLD Matrix
+		float_vector host_matrix_gold(size_matrices * size_matrices);
+
+		GEMMWMMA<host_half, half, float> mult_enviroment(host_matrix_a.data(),
+				host_matrix_b.data(), host_matrix_c.data(), size_matrices,
+				size_matrices, size_matrices);
+
+		int tries = 0;
+		for (int it = 0; it < iterations; it++) {
+
+			mult_enviroment.mul();
+			mult_enviroment.pull_array(host_matrix_d1.data(),
+					host_matrix_d2.data(), host_matrix_d3.data());
+
+			//TODO: FIX FOR GENERATE AND TEST
+			if (compare_output_matrices(mult_enviroment.get_memory_errors(),
+					generate, host_gold, host_matrix_d1, host_matrix_d2,
+					host_matrix_d3, host_matrix_d3, log_obj, size_matrices,
+					false, false)) {
+				tries++;
+				it--;
+			}
+
+			if (generate && tries > 5)
+				throw "More than 5 tries on matrix generate\n";
+
+		}
+
+		if (generate) {
+			write_gold_to_file(gold_inout_path, host_gold);
+		}
+	} else if (precision == "double") {
+
+	}
+
+	if (log_obj)
+		delete log_obj;
 	return 0;
 }
