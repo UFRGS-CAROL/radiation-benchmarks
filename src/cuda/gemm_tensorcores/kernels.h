607f3bb3e317596913b9fa03dec16df288e86b6c
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2018-10-06 20:58:08 -0300

    test

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -32,7 +32,8 @@ using namespace nvcuda;
 
 // __global__ void simple_wmma_gemm(float *d0, half *d1, half *d2, size_t m_ld, size_t n_ld, half alpha, half beta) {
 	// Leading dimensions. Packed with no transpositions.
-__global__ void simple_wmma_gemm(float *d0, float *d1, float *d2, size_t m_ld, size_t n_ld, float alpha, float beta){
+template<class half_t, class real_t>
+__global__ void simple_wmma_gemm(real_t *d0, real_t *d1, real_t *d2, size_t m_ld, size_t n_ld, float alpha, float beta){
 	int lda = WMMA_M;
 	int ldb = WMMA_K;
 	int ldc = WMMA_M;
