44e834d8bb1004db586800264c923c4e6a5e1c95
carol <fernandofernandesantos@gmail.com>
2018-11-27 19:19:36 +0000

    triplication apparently working, just few tests done

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -297,7 +297,27 @@ __global__ void compute_gemm(const half_t *A0, const half_t *A1, const half_t *A
 //        __syncthreads();
 //    }
 }
+template<class real_t>
+__device__ real_t inline read_voter_wmma(real_t *v1, real_t *v2, real_t *v3, int offset, unsigned long long int* is_memory_bad) {
+	
+	register real_t in1 = v1[offset];
+	register real_t in2 = v2[offset];
+	register real_t in3 = v3[offset];
 
+	if (in1 == in2 || in1 == in3) {
+		return in1;
+	}
+
+	if (in2 == in3) {
+		return in2;
+	}
+
+	if (in1 != in2 && in2 != in3 && in1 != in3) {
+		atomicAdd(is_memory_bad, 1);
+	}
+
+	return in1;
+}
 
 //-------------------------------------------------------------------------------------------------
 // Performs an MxNxK GEMM (C=alpha*A*B + beta*C) assuming:
@@ -311,95 +331,94 @@ template<class half_t, class real_t>
 __global__ void simple_wmma_gemm(half_t *a0,half_t *a1,half_t *a2, half_t *b0,half_t *b1,half_t *b2, real_t *c0,real_t *c1,real_t *c2, real_t *d0,
 		real_t *d1, real_t *d2,int m_ld, int n_ld, int k_ld, real_t alpha, real_t beta, unsigned long long int* is_memory_bad) {
 	// Leading dimensions. Packed with no transpositions.
-//	int lda = m_ld;
-//	int ldb = k_ld;
-//	int ldc = n_ld;
-//
-//	// Tile using a 2D grid
-//	int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / WARP_SIZE;
-//	int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
-//
-//	// Declare the fragments
-//	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::row_major> a0_frag;
-//	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::row_major> a1_frag;
-//	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::row_major> a2_frag;
-//
-//	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::col_major> b0_frag;
-//	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::col_major> b1_frag;
-//	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
-//	wmma::col_major> b2_frag;
-//
-//	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> acc_frag;
-//
-//	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c0_frag;
-//	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c1_frag;
-//	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c2_frag;
-//
-//	wmma::fill_fragment(acc_frag, 0.0f);
-//
-//	// Loop over k
-//	for (int i = 0; i < k_ld; i += WMMA_K) {
-//		int aCol = i;
-//		int aRow = warpM * WMMA_M;
-//
-//		int bCol = i;
-//		int bRow = warpN * WMMA_N;
-//
-//		// Bounds checking
-//		if (aRow < m_ld && aCol < k_ld && bRow < k_ld && bCol < n_ld) {
-//			// Load the inputs
-//			wmma::load_matrix_sync(a0_frag, a0 + aCol + aRow * lda, lda);
-//			wmma::load_matrix_sync(a1_frag, a1 + aCol + aRow * lda, lda);
-//			wmma::load_matrix_sync(a2_frag, a2 + aCol + aRow * lda, lda);
-//
-//			wmma::load_matrix_sync(b0_frag, b0 + bCol + bRow * ldb, ldb);
-//			wmma::load_matrix_sync(b1_frag, b1 + bCol + bRow * ldb, ldb);
-//			wmma::load_matrix_sync(b2_frag, b2 + bCol + bRow * ldb, ldb);
+	int lda = m_ld;
+	int ldb = k_ld;
+	int ldc = n_ld;
+
+	// Tile using a 2D grid
+	int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / WARP_SIZE;
+	int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
+
+	// Declare the fragments
+	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::row_major> a0_frag;
+	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::row_major> a1_frag;
+	wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::row_major> a2_frag;
+
+	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::col_major> b0_frag;
+	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::col_major> b1_frag;
+	wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::col_major> b2_frag;
+
+	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> acc_frag;
+
+	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c0_frag;
+	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c1_frag;
+	wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t> c2_frag;
+
+	wmma::fill_fragment(acc_frag, 0.0f);
+
+	// Loop over k
+	for (int i = 0; i < k_ld; i += WMMA_K) {
+		int aCol = i;
+		int aRow = warpM * WMMA_M;
+
+		int bCol = i;
+		int bRow = warpN * WMMA_N;
+
+		// Bounds checking
+		if (aRow < m_ld && aCol < k_ld && bRow < k_ld && bCol < n_ld) {
+			// Load the inputs
+			wmma::load_matrix_sync(a0_frag, a0 + aCol + aRow * lda, lda);
+			wmma::load_matrix_sync(a1_frag, a1 + aCol + aRow * lda, lda);
+			wmma::load_matrix_sync(a2_frag, a2 + aCol + aRow * lda, lda);
+
+			wmma::load_matrix_sync(b0_frag, b0 + bCol + bRow * ldb, ldb);
+			wmma::load_matrix_sync(b1_frag, b1 + bCol + bRow * ldb, ldb);
+			wmma::load_matrix_sync(b2_frag, b2 + bCol + bRow * ldb, ldb);
 
 
-			read_voter_wmma(a0_frag,a1_frag,a2_frag);
-//
-//			// Perform the matrix multiplication
-//			wmma::mma_sync(acc_frag, read_voter_wmma_if(a0_frag , a1_frag, a2_frag, a0_frag.x, a0_frag.num_elements, is_memory_bad),
-//					read_voter_wmma_if(b0_frag , b1_frag, b2_frag, b0_frag.x, c0_frag.num_elements, is_memory_bad), acc_frag);
-//
-//
-//
-//		}
-//
-//		// Load in the current value of c, scale it by beta, and add this our result scaled by alpha
-//		int cCol = warpN * WMMA_N;
-//		int cRow = warpM * WMMA_M;
-//
-//		if (cRow < m_ld && cCol < n_ld) {
-//			wmma::load_matrix_sync(c0_frag, c0 + cCol + cRow * ldc, ldc,
-//					wmma::mem_row_major);
-//			wmma::load_matrix_sync(c1_frag, c1 + cCol + cRow * ldc, ldc,
-//					wmma::mem_row_major);
-//			wmma::load_matrix_sync(c2_frag, c2 + cCol + cRow * ldc, ldc,
-//					wmma::mem_row_major);
-//
-//
-//			for (int i = 0; i < c0_frag.num_elements; i++) {
-//				read_voter_wmma(c0_frag , c1_frag, c2_frag, c0_frag.x, is_memory_bad).x[i] = 
-//						alpha * acc_frag.x[i] + beta * read_voter_wmma(c0_frag , c1_frag, c2_frag, c0_frag.x, is_memory_bad).x[i];
-//
-//			}
-//
-//			// Store the output
-//			wmma::store_matrix_sync(d0 + cCol + cRow * ldc, c0_frag, ldc,
-//					wmma::mem_row_major);
-//			wmma::store_matrix_sync(d1 + cCol + cRow * ldc, c1_frag, ldc,
-//					wmma::mem_row_major);
-//			wmma::store_matrix_sync(d2 + cCol + cRow * ldc, c2_frag, ldc,
-//					wmma::mem_row_major);
-//		}
-//	}
+			//read_voter_wmma(a0_frag,a1_frag,a2_frag);			
+			
+			// Perform the matrix multiplication
+//			wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+			
+			wmma::mma_sync(acc_frag,read_voter_wmma_for_a(a0_frag,a1_frag,a2_frag,is_memory_bad),read_voter_wmma_for_b(b0_frag, b1_frag, b2_frag,is_memory_bad), acc_frag);
+
+
+
+		}
+
+		// Load in the current value of c, scale it by beta, and add this our result scaled by alpha
+		int cCol = warpN * WMMA_N;
+		int cRow = warpM * WMMA_M;
+
+		if (cRow < m_ld && cCol < n_ld) {
+			wmma::load_matrix_sync(c0_frag, c0 + cCol + cRow * ldc, ldc,
+					wmma::mem_row_major);
+			wmma::load_matrix_sync(c1_frag, c1 + cCol + cRow * ldc, ldc,
+					wmma::mem_row_major);
+			wmma::load_matrix_sync(c2_frag, c2 + cCol + cRow * ldc, ldc,
+					wmma::mem_row_major);
+
+
+			for (int i = 0; i < c0_frag.num_elements; i++) {
+				c0_frag.x[i] = alpha * acc_frag.x[i] + beta * read_voter_wmma<real_t>(c0_frag.x, c1_frag.x,c2_frag.x,i, is_memory_bad);
+			}
+
+			// Store the output
+			wmma::store_matrix_sync(d0 + cCol + cRow * ldc, c0_frag, ldc,
+					wmma::mem_row_major);
+			wmma::store_matrix_sync(d1 + cCol + cRow * ldc, c0_frag, ldc,
+					wmma::mem_row_major);
+			wmma::store_matrix_sync(d2 + cCol + cRow * ldc, c0_frag, ldc,
+					wmma::mem_row_major);
+		}
+	}
 }	
 	
 template<class half_t, class real_t>
@@ -440,9 +459,10 @@ __global__ void simple_wmma_gemm(half_t *a, half_t *b, real_t *c, real_t *d,
 			wmma::load_matrix_sync(a_frag, a + aCol + aRow * lda, lda);
 			wmma::load_matrix_sync(b_frag, b + bCol + bRow * ldb, ldb);
 			
-//			read_voter_wmma(*b_frag.x, b_frag.num_elements);
-			read_voter_wmma(a_frag);
-			
+		
+			//read_voter_wmma(a_frag.x, b_frag.num_elements);
+			//read_voter_wmma_for(a_frag);
+//			printf("test c_frag = %d \n", c_frag.x);
 
 			// Perform the matrix multiplication
 			wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
@@ -491,112 +511,84 @@ __device__ real_t inline read_voter(real_t *v1, real_t *v2, real_t *v3,
 	return in1;
 }
 
-//__device__ void inline read_voter_wmma(wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half,
+//__device__ wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half,
+//	wmma::row_major> inline read_voter_wmma(wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half,
 //		wmma::row_major> a_frag) {
 //	
-//	printf("entrou read /n");
+//	printf("entrou frag \n");
+//	return a_frag;
 //}
 
-// __device__ void inline read_voter_wmma(wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half,
-// 		wmma::row_major> a_frag) {
-		
-// 	printf("teste frag = %d \n", a_frag.x[1]);
+
+template<class half_t>
+__device__ wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+	wmma::row_major> inline read_voter_wmma_for_a(wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+		wmma::row_major> &v1,wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+		wmma::row_major> &v2, wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half_t,
+		wmma::row_major> &v3,unsigned long long int* is_memory_bad) {
 	
-// }
-
-
-__device__ void inline read_voter_wmma_if(half* v1, half* v2, half* v3, int num_elements) {
-	
-	//printf("X= %d \n", x[]);
-	register half in1 = v1[0]];
-	register half in2 = v2[0];
-	register half in3 = v3[0];
-
-	for(int i = 0; i < num_elements ; i ++){
-		if (in1[i] == in2[i] || in1[i] == in3[i]) {
-			return in1;
+	for(int i = 0; i < v1.num_elements ; i ++){
+		if (v1.x[i] == v2.x[i] || v1.x[i] == v3.x[i]) {
+			return v1;
 		}
 
-		if (in2[i] == in3[i]) {
-			return in2;
+		if (v2.x[i] == v3.x[i]) {
+			return v2;
 		}
 
-		if (in1[i] != in2[i] && in2[i] != in3[i] && in1[i] != in3[i]) {
+		if (v1.x[i] != v2.x[i] && v2.x[i] != v3.x[i] && v1.x[i] != v3.x[i]) {
 			atomicAdd(is_memory_bad, 1);
+		}		
+	}
+	return v1;
+}
+ 
+template<class half_t>
+__device__ wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,
+			wmma::col_major> inline read_voter_wmma_for_b(wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half,wmma::col_major> &v1,
+			wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,wmma::col_major> &v2, 
+			wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half_t,wmma::col_major> &v3, 
+			unsigned long long int* is_memory_bad) {
+	
+	for(int i = 0; i < v1.num_elements ; i ++){
+		if (v1.x[i] == v2.x[i] || v1.x[i] == v3.x[i]) {
+			return v1;
 		}
 
-		return in1;
+		if (v2.x[i] == v3.x[i]) {
+			return v2;
+		}
+
+		if (v1.x[i] != v2.x[i] && v2.x[i] != v3.x[i] && v1.x[i] != v3.x[i]) {
+			atomicAdd(is_memory_bad, 1);
+		}		
 	}
+	return v1;
 }
 
-
-__device__ void inline read_voter_wmma(half* v1, half* v2, half* v3, int offset) {
-	
-	register real_t in1 = v1[offset];
-	register real_t in2 = v2[offset];
-	register real_t in3 = v3[offset];
-
-	if (in1 == in2 || in1 == in3) {
-		return in1;
-	}
-
-	if (in2 == in3) {
-		return in2;
-	}
-
-	if (in1 != in2 && in2 != in3 && in1 != in3) {
-		atomicAdd(is_memory_bad, 1);
-	}
-
-	return in1;
-}
-//template<class half_t, class real_t>
-//__device__ real_t inline read_voter_wmma(real_t *v1, real_t *v2, real_t *v3,
-//		int* offset, unsigned long long int* is_memory_bad) {
+//__device__ half inline read_voter_wmma_for(half* v1, half* v2, half* v3, int num_elements,  unsigned long long int* is_memory_bad) {
+//	
+//	
+//	
+//	for(int i = 0; i < num_elements ; i ++){
+//		if (v1[i] == v2[i] || v1[i] == v3[i]) {
+//			return v1;
+//		}
 //
-//	register real_t in1 = v1[offset];
-//	register real_t in2 = v2[offset];
-//	register real_t in3 = v3[offset];
+//		if (v2[i] == v3[i]) {
+//			return v2;
+//		}
 //
-//	if (in1 == in2 || in1 == in3) {
-//		return in1;
+//		if (v1[i] != v2[i] && v2[i] != v3[i] && v1[i] != v3[i]) {
+//			atomicAdd(is_memory_bad, 1);
+//		}
+//
+//		return v1;
 //	}
-//
-//	if (in2 == in3) {
-//		return in2;
-//	}
-//
-//	if (in1 != in2 && in2 != in3 && in1 != in3) {
-//		atomicAdd(is_memory_bad, 1);
-//	}
-//
-//	return in1;
 //}
-
-//template<class real_t, class half_t>
-//__device__ real_t inline read_voter_wmma_if(half_t wmma::fragment<v1>, half_t wmma::fragment<v2>, half_t wmma::fragment<v3>,
-//		int* offset, int num_elements, unsigned long long int* is_memory_bad) {
 //
-	// register half_t in1 = v1[offset];
-	// register half_t in2 = v2[offset];
-	// register half_t in3 = v3[offset];
 
-	// for(int i = 0; i < num_elements ; i ++){
-	// 	if (in1[i] == in2[i] || in1[i] == in3[i]) {
-	// 		return in1;
-	// 	}
 
-	// 	if (in2[i] == in3[i]) {
-	// 		return in2;
-	// 	}
-
-	// 	if (in1[i] != in2[i] && in2[i] != in3[i] && in1[i] != in3[i]) {
-	// 		atomicAdd(is_memory_bad, 1);
-	// 	}
-
-	// 	return in1;
-	// }
-//}	
 
 template<class half_t, class real_t>
 __global__ void matrix_mul(half_t *a0, half_t *a1, half_t *a2, half_t *b0,
