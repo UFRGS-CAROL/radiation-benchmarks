2237dc9baef66cb68a68fc5b521d1fcaaac5af19
Pedro Basso <pmbasso@inf.ufrgs.br>
2019-01-16 18:47:32 -0200

    testing

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -221,7 +221,7 @@ __global__ void compute_gemm(real_t *D, float alpha, float beta)
 				wmma::fragment<wmma::matrix_b, M, N, K, half, wmma::col_major> b[WARP_ROW_TILES];
 				wmma::fill_fragment(a[WARP_COL_TILES], 2.0f);
  				wmma::fill_fragment(b[WARP_ROW_TILES], 2.0f);
- 				printf("frag A=  %f \n", a->x[0]);
+ 				printf(a);
  				//printf("frag B=  %f \n",b.x[0]);
 
 #pragma unroll
