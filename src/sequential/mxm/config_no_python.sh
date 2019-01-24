9bce31b6b4b747c4b5b8cd2e72d55c5198f12196
Fernando <fernandofernandesantos@gmail.com>
2019-01-24 10:04:05 -0200

    config no python

---
+++ src/sequential/mxm/config_no_python.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RADIATION_BENCHMARKS="/home/carol/radiation-benchmarks"
+SIZE=1024
+ARCH=x86
+ 
+make ARCH=x86 LOGS=1 MATRIXSIZE=$SIZE generate
+
+mkdir -p $RADIATION_BENCHMARKS/bin
+mkdir -p $RADIATION_BENCHMARKS/data/mxm
+
+mv -f matmul $RADIATION_BENCHMARKS/bin/
+mv matmul_input_${SIZE}.txt  matmul_gold_${SIZE}.txt $RADIATION_BENCHMARKS/data/mxm/
+
+str='[
+{
+    "killcmd": "killall -9 matmul", "exec": '${RADIATION_BENCHMARKS}'/bin/matmul  '${RADIATION_BENCHMARKS}'/data/mxm/matmul_input_'${SIZE}'.txt '${RADIATION_BENCHMARKS}'/data/mxm/matmul_gold_'${SIZE}'.txt 0 '${SIZE}'"
+}
+]'
+
+echo $str
