161f4e5ee6c0b5a9f156fdb683bd323bfffe9c87
Fernando <fernandofernandesantos@gmail.com>
2019-01-25 17:24:32 -0200

    nvml wrapper

---
+++ src/cuda/cache/include/NVMLWrapper.h
@@ -0,0 +1,17 @@
+/*
+ * NVMLWrapper.h
+ *
+ *  Created on: 25/01/2019
+ *      Author: fernando
+ */
+
+#ifndef NVMLWRAPPER_H_
+#define NVMLWRAPPER_H_
+
+class NVMLWrapper {
+public:
+	NVMLWrapper();
+	virtual ~NVMLWrapper();
+};
+
+#endif /* NVMLWRAPPER_H_ */
---
+++ src/cuda/cache/src/NVMLWrapper.cpp
@@ -0,0 +1,18 @@
+/*
+ * NVMLWrapper.cpp
+ *
+ *  Created on: 25/01/2019
+ *      Author: fernando
+ */
+
+#include "NVMLWrapper.h"
+
+NVMLWrapper::NVMLWrapper() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NVMLWrapper::~NVMLWrapper() {
+	// TODO Auto-generated destructor stub
+}
+
