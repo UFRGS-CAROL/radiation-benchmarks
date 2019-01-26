7e11a6a22b3172f2303cf473da7f4b3804c6078b
reliabilitylogdata <reliabilitylogdata@github.com>
2019-01-26 17:52:39 -0200

    nvml

--- src/cuda/cache/src/NVMLWrapper.h
+++ src/cuda/cache/src/NVMLWrapper.h
@@ -9,13 +9,15 @@
 #define NVMLWRAPPER_H_
 
 #include <nvml.h>
+#include <string>
+
 
 class NVMLWrapper {
 private:
-	nvmlReturn_t init_ok;
-	nvmlReturn_t shutdown_ok;
+
 	unsigned device_index;
 
+	void check_nvml_return(std::string info, nvmlReturn_t result);
 public:
 	NVMLWrapper(unsigned device_index);
 	virtual ~NVMLWrapper();
