6194c6c0edc58f2a690322fc237323bb449b1687
reliabilitylogdata <reliabilitylogdata@github.com>
2019-05-01 21:38:57 -0300

    mkefile

--- src/cuda/micro_mp_hard/cuda_micro.cu
+++ src/cuda/micro_mp_hard/cuda_micro.cu
@@ -31,8 +31,8 @@ void usage(int argc, char* argv[]) {
 
 // Returns true if no errors are found. False if otherwise.
 // Set votedOutput pointer to retrieve the voted matrix
-template<typename T, int OUTPUT_R>
-bool checkOutputErrors(std::vector<T> &R, bool verbose) {
+template<typename T>
+int check_output_errors(std::vector<T> &R, T OUTPUT_R, bool verbose) {
 	int host_errors = 0;
 #pragma omp parallel for shared(host_errors)
 	for (int i = 0; i < R.size(); i++) {
@@ -63,7 +63,7 @@ bool checkOutputErrors(std::vector<T> &R, bool verbose) {
 		log_error_count(host_errors);
 #endif
 	}
-	return host_errors == 0;
+	return host_errors;
 }
 
 template<typename full, typename incomplete = void>
@@ -74,48 +74,44 @@ void test_radiation(const incomplete OUTPUT_R, const incomplete INPUT_A,
 	double total_kernel_time = 0;
 	double min_kernel_time = UINT_MAX;
 	double max_kernel_time = 0;
-	double global_time;
 
 	parameters.print_details();
 	//====================================
 	// FULL PRECIISON
-	std::vector<full> host_vector_ful(parameters.r_size, 0);
-	DeviceVector<full> device_vector_ful;
+	std::vector<full> host_vector_full(parameters.r_size, 0);
+	DeviceVector<full> device_vector_full(parameters.r_size);
 
 	//====================================
 	// SECOND PRECISION ONLY IF IT IS DEFINED
 	DeviceVector<incomplete> device_vector_inc;
 	std::vector<incomplete> host_vector_inc;
-	if (std::is_void < incomplete > ::value != true) {
-		host_vector_inc = std::vector < incomplete > (parameters.r_size, 0);
-		device_vector_inc = host_vector_inc;
+	if (std::is_void<incomplete>::value != true) {
+		host_vector_inc = std::vector<incomplete>(parameters.r_size, 0);
+		device_vector_inc = DeviceVector<incomplete>(parameters.r_size);
 	}
-	device_vector_ful = host_vector_ful;
 
-	for (int loop2 = 0; loop2 < parameters.iterations; loop2++) {
+	for (int iteration = 0; iteration < parameters.iterations; iteration++) {
 		//================== Global test loop
-
-		global_time = mysecond();
 		double kernel_time = mysecond();
 #ifdef LOGS
 		start_iteration();
 #endif
 		//================== Device computation
-		if (std::is_void < incomplete > ::value) {
+		if (std::is_void<incomplete>::value) {
 			switch (parameters.micro) {
 			case ADD:
 				MicroBenchmarkKernel_ADD<full> <<<parameters.grid_size,
-						parameters.block_size>>>(device_vector_ful.data,
+						parameters.block_size>>>(device_vector_full.data,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			case MUL:
 				MicroBenchmarkKernel_MUL<full> <<<parameters.grid_size,
-						parameters.block_size>>>(device_vector_ful.data,
+						parameters.block_size>>>(device_vector_full.data,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			case FMA:
 				MicroBenchmarkKernel_FMA<full> <<<parameters.grid_size,
-						parameters.block_size>>>(device_vector_ful.data,
+						parameters.block_size>>>(device_vector_full.data,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			}
@@ -124,19 +120,19 @@ void test_radiation(const incomplete OUTPUT_R, const incomplete INPUT_A,
 			case ADD:
 				MicroBenchmarkKernel_ADD<incomplete, full> <<<
 						parameters.grid_size, parameters.block_size>>>(
-						device_vector_inc.data, device_vector_ful.data, 0.1,
+						device_vector_inc.data, device_vector_full.data, 0.1,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			case MUL:
 				MicroBenchmarkKernel_MUL<incomplete, full> <<<
 						parameters.grid_size, parameters.block_size>>>(
-						device_vector_inc.data, device_vector_ful.data, 0.1,
+						device_vector_inc.data, device_vector_full.data, 0.1,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			case FMA:
 				MicroBenchmarkKernel_FMA<incomplete, full> <<<
 						parameters.grid_size, parameters.block_size>>>(
-						device_vector_inc.data, device_vector_ful.data, 0.1,
+						device_vector_inc.data, device_vector_full.data, 0.1,
 						OUTPUT_R, INPUT_A, INPUT_B);
 				break;
 			}
@@ -146,44 +142,37 @@ void test_radiation(const incomplete OUTPUT_R, const incomplete INPUT_A,
 		checkFrameworkErrors(cudaDeviceSynchronize());
 		checkFrameworkErrors(cudaPeekAtLastError());
 
-		std::printf("ERRORS %lld\n", copy_errors());
+		kernel_time = mysecond() - kernel_time;
 
 		//====================================
 #ifdef LOGS
 		end_iteration();
 #endif
-		kernel_time = mysecond() - kernel_time;
 
 		total_kernel_time += kernel_time;
 		min_kernel_time = std::min(min_kernel_time, kernel_time);
 		max_kernel_time = std::max(max_kernel_time, kernel_time);
 
-		if (parameters.verbose)
-			std::printf("Device kernel time for iteration %d: %.3fs\n", loop2,
-					kernel_time);
-
-		double gold_check_time = mysecond();
-
 		std::cout << ".";
-
-		if (parameters.verbose)
-			std::printf("Gold check time for iteration %d: %.3fs\n", loop2,
-					mysecond() - gold_check_time);
-
 		if (parameters.verbose) {
+			//check output
+			host_vector_full = device_vector_full.to_vector();
+			int errors = check_output_errors<full>(
+					host_vector_full, OUTPUT_R, parameters.verbose);
+
+			std::cout << "ERRORS " << copy_errors() << std::endl;
+
 			/////////// PERF
 			double flops = parameters.r_size * OPS;
 			double gflops = flops / kernel_time;
-			double outputpersec = (double) parameters.r_size / kernel_time;
-			std::printf("SIZE:%d OUTPUT/S:%f FLOPS:%f (GFLOPS:%.2f)\n",
-					parameters.r_size, outputpersec, gflops,
-					gflops / 1000000000);
-			///////////
-		}
+			double outputpersec = double(parameters.r_size) / kernel_time;
+			std::cout << "SIZE:" << parameters.r_size << " OUTPUT/S:"
+					<< outputpersec << " FLOPS: " << gflops << " (GFLOPS:"
+					<< gflops / 1e9 << ")";
+			std::cout << "Iteration " << iteration << " time: " << kernel_time
+					<< std::endl;
 
-		if (parameters.verbose)
-			std::printf("Iteration #%d time: %.3fs\n\n\n", loop2,
-					mysecond() - global_time);
+		}
 	}
 
 	double gflops = parameters.r_size * OPS / 1e9; // Billion FLoating-point OPerationS
@@ -239,15 +228,15 @@ void dmr(Parameters& parameters) {
 		switch (parameters.precision) {
 		case DOUBLE:
 			test_radiation<double, double>(OUTPUT_R_DOUBLE, INPUT_A_DOUBLE,
-					INPUT_B_DOUBLE, parameters);
+			INPUT_B_DOUBLE, parameters);
 			break;
 		case SINGLE:
 			test_radiation<float, float>(OUTPUT_R_SINGLE, INPUT_A_SINGLE,
-					INPUT_B_SINGLE, parameters);
+			INPUT_B_SINGLE, parameters);
 			break;
 		case HALF:
 			test_radiation<half, half>(OUTPUT_R_HALF, INPUT_A_HALF,
-					INPUT_B_HALF, parameters);
+			INPUT_B_HALF, parameters);
 			break;
 		}
 		break;
@@ -261,17 +250,16 @@ int main(int argc, char* argv[]) {
 //================== Set block and grid size for MxM kernel
 	cudaDeviceProp prop = GetDevice();
 	Parameters parameters(argc, argv, prop.multiProcessorCount, 256);
-	parameters.print_details();
 //================== Init logs
 #ifdef LOGS
 	std::string test_info = std::string("ops:") + std::to_string(OPS)
-			+ " gridsize:" + std::to_string(parameters.grid_size)
-			+ " blocksize:" + std::to_string(parameters.block_size) + " type:"
-			+ parameters.instruction_str + "-" + parameters.precision_str
-			+ "-precision hard:" + parameters.hardening_str;
+	+ " gridsize:" + std::to_string(parameters.grid_size)
+	+ " blocksize:" + std::to_string(parameters.block_size) + " type:"
+	+ parameters.instruction_str + "-" + parameters.precision_str
+	+ "-precision hard:" + parameters.hardening_str;
 
 	std::string test_name = std::string("cuda_") + parameters.precision_str
-			+ "_micro-" + parameters.instruction_str;
+	+ "_micro-" + parameters.instruction_str;
 	start_log_file(const_cast<char*>(test_name.c_str()),
 			const_cast<char*>(test_info.c_str()));
 #endif
--- src/cuda/micro_mp_hard/cuda_utils.h
+++ src/cuda/micro_mp_hard/cuda_utils.h
@@ -18,56 +18,56 @@
 #define checkFrameworkErrors(error) __checkFrameworkErrors(error, __LINE__, __FILE__)
 
 void __checkFrameworkErrors(cudaError_t error, int line, const char* file) {
-if (error == cudaSuccess) {
-	return;
-}
+	if (error == cudaSuccess) {
+		return;
+	}
 
-char errorDescription[250];
-snprintf(errorDescription, 250, "CUDA Framework error: %s. Bailing.",
-		cudaGetErrorString(error));
+	char errorDescription[250];
+	snprintf(errorDescription, 250, "CUDA Framework error: %s. Bailing.",
+			cudaGetErrorString(error));
 #ifdef LOGS
-log_error_detail((char *)errorDescription); end_log_file();
+	log_error_detail((char *)errorDescription); end_log_file();
 #endif
-printf("%s - Line: %d at %s\n", errorDescription, line, file);
-exit (EXIT_FAILURE);
+	printf("%s - Line: %d at %s\n", errorDescription, line, file);
+	exit (EXIT_FAILURE);
 }
 
 cudaDeviceProp GetDevice() {
 //================== Retrieve and set the default CUDA device
-cudaDeviceProp prop;
-int count = 0;
-printf("Get device:");
-checkFrameworkErrors(cudaGetDeviceCount(&count));
-for (int i = 0; i < count; i++) {
-	checkFrameworkErrors(cudaGetDeviceProperties(&prop, i));
-	printf("Name: %s\n", prop.name);
-}
-int *ndevice;
-int dev = 0;
-ndevice = &dev;
-checkFrameworkErrors(cudaGetDevice(ndevice));
+	cudaDeviceProp prop;
+	int count = 0;
+	printf("Get device:");
+	checkFrameworkErrors(cudaGetDeviceCount(&count));
+	for (int i = 0; i < count; i++) {
+		checkFrameworkErrors(cudaGetDeviceProperties(&prop, i));
+		printf("Name: %s\n", prop.name);
+	}
+	int *ndevice;
+	int dev = 0;
+	ndevice = &dev;
+	checkFrameworkErrors(cudaGetDevice(ndevice));
 
-checkFrameworkErrors(cudaSetDevice(0));
-checkFrameworkErrors(cudaGetDeviceProperties(&prop, 0));
-printf("\ndevice: %d %s\n", *ndevice, prop.name);
-return prop;
+	checkFrameworkErrors(cudaSetDevice(0));
+	checkFrameworkErrors(cudaGetDeviceProperties(&prop, 0));
+	printf("\ndevice: %d %s\n", *ndevice, prop.name);
+	return prop;
 }
 
 double mysecond() {
-struct timeval tp;
-struct timezone tzp;
-int i = gettimeofday(&tp, &tzp);
-return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
+	struct timeval tp;
+	struct timezone tzp;
+	int i = gettimeofday(&tp, &tzp);
+	return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
 }
 
 __device__ unsigned long long errors = 0;
 
 unsigned long long copy_errors() {
-unsigned long long errors_host = 0;
-checkFrameworkErrors(
-		cudaMemcpyFromSymbol(&errors_host, errors, sizeof(unsigned long long),
-				0, cudaMemcpyDeviceToHost));
-return errors_host;
+	unsigned long long errors_host = 0;
+	checkFrameworkErrors(
+			cudaMemcpyFromSymbol(&errors_host, errors,
+					sizeof(unsigned long long), 0, cudaMemcpyDeviceToHost));
+	return errors_host;
 }
 
 #endif /* CUDA_UTILS_H_ */
