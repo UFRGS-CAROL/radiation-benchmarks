d712708f9ec7b2e33464d6ccd59f5ac792ae3223
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-05-17 14:50:36 -0300

    test

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -339,23 +339,23 @@ __global__ void simple_wmma_gemm(real_t *d0, real_t *d1, real_t *d2,
 	__shared__ real_t c_shared[WMMA_M][WMMA_N];
 	__shared__ real_t d_shared[WMMA_M][WMMA_N];
 
-	a_shared[threadIdx.x][threadIdx.y] = half_t(2.0f);
+	// a_shared[threadIdx.x][threadIdx.y] = half_t(2.0f);
 
-	b_shared[threadIdx.x][threadIdx.y] = half_t(2.0f);
+	// b_shared[threadIdx.x][threadIdx.y] = half_t(2.0f);
 
-	c_shared[threadIdx.x][threadIdx.y] = real_t(2.0f);
+	// c_shared[threadIdx.x][threadIdx.y] = real_t(2.0f);
 
-	d_shared[threadIdx.x][threadIdx.y] = real_t(0.0f);
+	// d_shared[threadIdx.x][threadIdx.y] = real_t(0.0f);
 	real_t acc = 0;
 
 	
-	// __syncthreads();
-	// for(int i = 0; i < WMMA_N; i++){
-	// 	 acc += real_t(a_shared[threadIdx.y][i] * b_shared[i][threadIdx.x]);
+	__syncthreads();
+	for(int i = 0; i < WMMA_N; i++){
+		 acc += real_t(a_shared[threadIdx.y][i] * b_shared[i][threadIdx.x]);
 
-	// }
+	}
 
-	// d_shared[threadIdx.x][threadIdx.y] = acc + c_shared[threadIdx.x][threadIdx.y];
+	d_shared[threadIdx.x][threadIdx.y] = acc + c_shared[threadIdx.x][threadIdx.y];
 	
 
 
