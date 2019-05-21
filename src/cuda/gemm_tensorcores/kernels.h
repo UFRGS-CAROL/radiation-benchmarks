f2626f69c93b84e387ba0b0cdadc2437bccac38d
pedro basso <pedro.basso@inf.ufrgs.br>
2019-05-21 11:45:28 -0300

    dmr tests

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -476,10 +476,10 @@ __global__ void simple_wmma_gemm(half_t *a, half_t *b, real_t *c, real_t *d,
 	}
 }
 
-template<class real_t, class half_t>
-__device__ real_t inline error_voter (real_t d_shared, wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, real_t,
+
+__device__ real_t inline error_voter (float d_shared, wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float,
 wmma::row_major> &acc_frag){
-	__device__ real_t errors = 0;
+	__device__ float errors = 0;
 	register real_t error_checker = d_shared - acc_frag;
 	if (error_checker > 0) {
 		atomicAdd(&errors, 1);
