9a180f764d183e5e3e7656d0bdd15581e8c5caf2
Fernando <fernandofernandesantos@gmail.com>
2019-05-21 16:43:18 -0300

    device compare

--- src/cuda/hotspot_mp/device_functions.h
+++ src/cuda/hotspot_mp/device_functions.h
@@ -16,29 +16,12 @@
 
 #define BLOCK_SIZE 32
 
+#define ZERO_FLOAT 1e-37
+#define ZERO_HALF 1e-13
+
 #ifdef __NVCC__
 
 __device__ unsigned long long errors;
-
-template<typename T>
-__device__   __forceinline__ T smallest() {
-	return 0;
-}
-
-template<>
-__device__   __forceinline__ half smallest<half>() {
-	return 0.0001;
-}
-
-template<>
-__device__ __forceinline__ float smallest<float>() {
-	return 0.0000001;
-}
-
-template<>
-__device__ __forceinline__ double smallest<double>() {
-	return 0.0000001;
-}
 
 __device__ __forceinline__ double abs__(double a) {
 	return fabs(a);
@@ -52,10 +35,27 @@ __device__    __forceinline__ half abs__(half a) {
 	return fabsf(a);
 }
 
-template<typename full, typename incomplete>
-__device__ __forceinline__ void compare(const full lhs, const incomplete rhs) {
-	const full diff = abs__(lhs - full(rhs));
-	const full zero = full(0.000000001);
+template<typename full>
+__device__ __forceinline__ void compare(const full lhs, const full rhs) {
+	const full diff = abs__(lhs - rhs);
+	const full zero = 0.0;
+	if (diff > zero) {
+		atomicAdd(&errors, 1);
+	}
+}
+
+__device__ __forceinline__ void compare(const float lhs, const half rhs) {
+	const float diff = abs__(lhs - float(rhs));
+	const float zero = float(ZERO_HALF);
+	if (diff > zero) {
+		atomicAdd(&errors, 1);
+	}
+}
+
+
+__device__ __forceinline__ void compare(const double lhs, const float rhs) {
+	const double diff = abs__(lhs - double(rhs));
+	const double zero = double(ZERO_FLOAT);
 	if (diff > zero) {
 		atomicAdd(&errors, 1);
 	}
