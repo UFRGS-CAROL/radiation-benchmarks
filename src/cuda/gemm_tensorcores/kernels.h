1d5e0c5be259094c9faac1ba8dc1673ca0596dc1
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-05-22 20:09:09 -0300

    dmr test

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -490,7 +490,7 @@ __global__ void simple_wmma_gemm(half_t *a, half_t *b, real_t *c, real_t *d,
 // 	return 0;
 // }
 
-__device__ float inline error_voter (float* acc_frag){
+__device__ float inline error_voter (float *acc_frag){
 	
 	register float error_checker = acc_frag;
 	if (error_checker > 0) {
