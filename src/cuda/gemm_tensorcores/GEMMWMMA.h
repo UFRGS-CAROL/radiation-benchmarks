1c03b6e91f82dbcdbbd6bc080fcc1d32d348b75d
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-01 14:16:39 -0300

    test

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -227,10 +227,10 @@ public:
 				cudaMemset(this->device_is_memory_bad, 0x0,
 						sizeof(unsigned long long int)));
 
-		 simple_wmma_gemm<half_t, real_t> <<<grid_dim, block_dim>>>(
-		 		this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
-		 		this->device_ptr_d0, this->rows_a, this->cols_b, this->cols_c,
-		 		this->alpha, this->beta);
+		 // simple_wmma_gemm<half_t, real_t> <<<grid_dim, block_dim>>>(
+		 // 		this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
+		 // 		this->device_ptr_d0, this->rows_a, this->cols_b, this->cols_c,
+		 // 		this->alpha, this->beta);
 
 		
 		this->debug("device synchronize");
@@ -253,11 +253,11 @@ public:
 					(BLOCK_COL_WARPS * WARP_COL_TILES) * sizeof(float))
 		};
 
-		printf("Required shared memory size: %lu Kb\n", SHMEM_SZ / 1024UL);
+		//printf("Required shared memory size: %lu Kb\n", SHMEM_SZ / 1024UL);
 		checkCudaErrors(cudaFuncSetAttribute(compute_gemm<half_t, real_t> , cudaFuncAttributeMaxDynamicSharedMemorySize, SHMEM_SZ));
-		//checkKernelErrors((compute_gemm<half_t, real_t> <<<deviceProp.multiProcessorCount, THREADS_PER_BLOCK,SHMEM_SZ>>>
-		//		(this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
-		//		 this->device_ptr_d0, this->alpha, this->beta)));
+		checkKernelErrors((compute_gemm<half_t, real_t> <<<deviceProp.multiProcessorCount, THREADS_PER_BLOCK,SHMEM_SZ>>>
+				(this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
+				 this->device_ptr_d0, this->alpha, this->beta)));
 		this->debug("device synchronize");
 		check_framework_errors(cudaDeviceSynchronize());
 
