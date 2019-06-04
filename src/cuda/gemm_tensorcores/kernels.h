80ecc5e486aebceddb75e8cab0316400454f63f0
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-04 01:47:24 -0300

    dmr

--- src/cuda/gemm_tensorcores/kernels.h
+++ src/cuda/gemm_tensorcores/kernels.h
@@ -899,7 +899,7 @@ __global__ void matrix_mul_dmr(half_t *a0, half_t *b0, real_t *c0, real_t*d0,rea
 	if (tx * ty > mul_M * mul_N)
 		return;
 
-	register real_t acc = 0.0;
+	register half_t acc = 0.0;
 	register real_t acc1 = 0.0;
 	for (k = 0; k < mul_N; k++) {
 		acc = half_t(a0[ty * mul_N + k] * b0[k * mul_N + tx]) + acc;
