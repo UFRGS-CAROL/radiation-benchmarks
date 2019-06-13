576aedfd87ecea526279d5645592ea438c85eaf4
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-13 02:31:17 -0300

    matrixmul half dmr

--- src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
+++ src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
@@ -172,7 +172,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
   // Csub is used to store the element of the block sub-matrix
   // that is computed by the thread
   half2 Csub = __float2half2_rn(0.0);
-    //volatile half Csub1= __float2half2_rn(0.0);
+  half2 Csub1= __float2half2_rn(0.0);
 
   // Loop over all the sub-matrices of A and B
   // required to compute the block sub-matrix
@@ -203,7 +203,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
 
     for (int k = 0; k < BLOCK_SIZE; ++k) {
       Csub = __hfma2(__half2half2(As[ty][k]), __half2half2(Bs[k][tx]),Csub);
-      //Csub1 = __hfma2(As[ty][k], Bs[k][tx],Csub1);
+      Csub1 = __hfma2(__half2half2(As[ty][k]), __half2half2(Bs[k][tx]),Csub1);
 
       
     }
@@ -218,6 +218,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
   // each thread writes one element
   int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
   ((half2*)C)[c + wB/2 * ty + tx] = Csub;
+  ((half2*)C1)[c + wB/2 * ty + tx] = Csub1;
   //C1[c + wB * ty + tx] = Csub1;
 }
 
