aa7271aa54bd74147ce80d9b6f92c50dc3fa88dc
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-13 02:36:09 -0300

    matrixmul half dmr]

--- src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
+++ src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
@@ -171,8 +171,8 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
 
   // Csub is used to store the element of the block sub-matrix
   // that is computed by the thread
-  half2 Csub = __float2half2_rn(0.0);
-  half2 Csub1= __float2half2_rn(0.0);
+  volatile half2 Csub = __float2half2_rn(0.0);
+  volatile half2 Csub1= __float2half2_rn(0.0);
 
   // Loop over all the sub-matrices of A and B
   // required to compute the block sub-matrix
@@ -219,7 +219,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
   int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
   ((half2*)C)[c + wB/2 * ty + tx] = Csub;
   ((half2*)C1)[c + wB/2 * ty + tx] = Csub1;
-  //C1[c + wB * ty + tx] = Csub1;
+ 
 }
 
 
