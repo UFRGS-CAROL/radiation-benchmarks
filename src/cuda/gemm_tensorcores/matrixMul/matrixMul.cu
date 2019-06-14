304c5bdeb311fbdf5792d8e573ba1457a4cd82ff
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-14 14:34:49 -0300

    mixed dmr

--- src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
+++ src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
@@ -60,8 +60,8 @@ typedef half_float::half half_h;
  * Matrix multiplication (CUDA Kernel) on the device: C = A * B
  * wA is A's width and wB is B's width
  */
-template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(half *C, half *C1, half *A,
-    half *B, int wA,
+template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(double *C, double *C1, double *A,
+    double *B, int wA,
     int wB) {
   // Block index
   int bx = blockIdx.x;
@@ -90,8 +90,8 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(half *C, half *C1, half 
 
   // Csub is used to store the element of the block sub-matrix
   // that is computed by the thread
-    volatile half Csub = 0;
-    volatile half Csub1= 0;
+    volatile float Csub = 0;
+    volatile double Csub1= 0;
 
   // Loop over all the sub-matrices of A and B
   // required to compute the block sub-matrix
@@ -100,11 +100,11 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(half *C, half *C1, half 
        a += aStep, b += bStep) {
     // Declaration of the shared memory array As used to
     // store the sub-matrix of A
-    __shared__ half As[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ double As[BLOCK_SIZE][BLOCK_SIZE];
 
     // Declaration of the shared memory array Bs used to
     // store the sub-matrix of B
-    __shared__ half Bs[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ double Bs[BLOCK_SIZE][BLOCK_SIZE];
 
     // Load the matrices from device memory
     // to shared memory; each thread loads
@@ -122,7 +122,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(half *C, half *C1, half 
 
     for (int k = 0; k < BLOCK_SIZE; ++k) {
       
-      Csub = fma_dmr(As[ty][k], Bs[k][tx],Csub);
+      Csub = fma_dmr((float)As[ty][k], (float)Bs[k][tx],Csub);
       Csub1 = fma_dmr(As[ty][k], Bs[k][tx],Csub1);
 
       
@@ -137,7 +137,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA(half *C, half *C1, half 
   // Write the block sub-matrix to device memory;
   // each thread writes one element
   int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-  C[c + wB * ty + tx] = Csub;
+  C[c + wB * ty + tx] = (float)Csub;
   C1[c + wB * ty + tx] = Csub1;
 }
 
@@ -226,7 +226,7 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA_Half(half *C, half *C1, 
 }
 
 
-void ConstantInit(half_h *data, int size, half_h val) {
+void ConstantInit(double *data, int size, double val) {
   for (int i = 0; i < size; ++i) {
     data[i] = val;
   }
@@ -240,26 +240,26 @@ int MatrixMultiply(int argc, char **argv,
                    const dim3 &dimsB) {
   // Allocate host memory for matrices A and B
   unsigned int size_A = dimsA.x * dimsA.y;
-  unsigned int mem_size_A = sizeof(half) * size_A;
-  half_h *h_A = reinterpret_cast<half_h *>(malloc(mem_size_A));
+  unsigned int mem_size_A = sizeof(double) * size_A;
+  double *h_A = reinterpret_cast<double *>(malloc(mem_size_A));
   unsigned int size_B = dimsB.x * dimsB.y;
-  unsigned int mem_size_B = sizeof(half) * size_B;
-  half_h *h_B = reinterpret_cast<half_h *>(malloc(mem_size_B));
+  unsigned int mem_size_B = sizeof(double) * size_B;
+  double *h_B = reinterpret_cast<double *>(malloc(mem_size_B));
 
   // Initialize host memory
   
-  const half_h valA = (half_h)2.0;
-  const half_h valB = (half_h)2.0;
+  const double valA = 2.0f;
+  const double valB = 2.0f;
   ConstantInit(h_A, size_A, valA);
   ConstantInit(h_B, size_B, valB);
   //printf("h_A = %f\n", h_A[0]);
   // Allocate device memory
-  half *d_A, *d_B, *d_C, *d_C1;
+  double *d_A, *d_B, *d_C, *d_C1;
 
   // Allocate host matrix C
   dim3 dimsC(dimsB.x, dimsA.y, 1);
-  unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(half);
-  half_h *h_C = reinterpret_cast<half_h *>(malloc(mem_size_C));
+  unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(double);
+  double *h_C = reinterpret_cast<double *>(malloc(mem_size_C));
 
   if (h_C == NULL) {
     fprintf(stderr, "Failed to allocate host matrix C!\n");
@@ -302,21 +302,13 @@ int MatrixMultiply(int argc, char **argv,
   // Create and start timer
   printf("Computing result using CUDA Kernel...\n");
 
-  // Performs warmup operation using matrixMul CUDA kernel
-  if (block_size == 16) {
-    //MatrixMulCUDA<16> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-    //                                        dimsA.x, dimsB.x);
 
-    MatrixMulCUDA_Half<16> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
+  MatrixMulCUDA<32> <<< grid, threads >>>(d_C, d_C1, d_A, d_B,
+                                         dimsA.x, dimsB.x);
+  //MatrixMulCUDA_Half<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
                                             dimsA.x, dimsB.x);
 
-  } else {
-    //MatrixMulCUDA<32> <<< grid, threads >>>(d_C, d_C1, d_A, d_B,
-    //                                       dimsA.x, dimsB.x);
-    MatrixMulCUDA_Half<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-                                            dimsA.x, dimsB.x);
-
-  }
+  
 
   printf("done\n");
 
@@ -336,18 +328,12 @@ int MatrixMultiply(int argc, char **argv,
   int nIter = 10;
 
   for (int j = 0; j < nIter; j++) {
-    if (block_size == 16) {
-      //MatrixMulCUDA<16> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-      //                                        dimsA.x, dimsB.x);
-
-      MatrixMulCUDA_Half<16> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-                                            dimsA.x, dimsB.x);
-    } else {
-      // MatrixMulCUDA<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-      //                                         dimsA.x, dimsB.x);
-      MatrixMulCUDA_Half<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
-                                            dimsA.x, dimsB.x);
-    }
+   
+      MatrixMulCUDA<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
+                                              dimsA.x, dimsB.x);
+      // MatrixMulCUDA_Half<32> <<< grid, threads >>>(d_C,d_C1, d_A, d_B,
+      //                                       dimsA.x, dimsB.x);
+    
   }
 
   // Record the stop event
