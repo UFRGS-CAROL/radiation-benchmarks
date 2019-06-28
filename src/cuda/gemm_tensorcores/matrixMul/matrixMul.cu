0d673061a0a79c7bb56fb4e902b015dd04d23830
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-06-28 19:34:56 -0300

    mixed dmr test with diff matrices

--- src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
+++ src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
@@ -272,10 +272,10 @@ int MatrixMultiply(int argc, char **argv,
   const float valB = 2.0f;
   const double valB1 = 4.0f;
   ConstantInit(h_A, size_A, valA);
-  ConstantInit(h_A1, size_A, valA);
+  ConstantInit(h_A1, size_A, valA1);
   
   ConstantInit(h_B, size_B, valB); 
-  ConstantInit(h_B1, size_B, valB);
+  ConstantInit(h_B1, size_B, valB1);
   //printf("h_A = %f\n", h_A[0]);
   // Allocate device memory
   float *d_A, *d_B, *d_C;
