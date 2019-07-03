6ef441c96602c8c61a8e51afeed68a7be6342a7a
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-02 23:44:07 -0300

    gemm_optimized float dmr

--- src/cuda/gemm_tensorcores/gemm_optimized/sgemm_nn_64_16_16_16_4.h
+++ src/cuda/gemm_tensorcores/gemm_optimized/sgemm_nn_64_16_16_16_4.h
@@ -9,9 +9,6 @@
 #define SGEMM_NN_64_16_16_16_4_H_
 
 
-typedef float real_t;
-typedef float half_real_t;
-
 
 void sgemm(cudaStream_t stream, float *C, const float *A, const float *B,
 		int32_t m, int32_t n, int32_t k, int32_t lda, int32_t ldb, int32_t ldc,
