1b1220ffa3c3600f57b18ecc3f5f17d581436936
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-06 15:11:52 -0300

    tensor tensor dmr half

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -236,10 +236,10 @@ public:
 				cudaMemset(this->device_is_memory_bad, 0x0,
 						sizeof(unsigned long long int)));
 
-		 // simple_wmma_gemm<half_t, real_t> <<<grid_dim, block_dim>>>(
-		 // 		this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
-		 // 		this->device_ptr_d0, this->rows_a, this->cols_b, this->cols_c,
-		 // 		this->alpha, this->beta);
+		 simple_wmma_gemm<half_t, real_t> <<<grid_dim, block_dim>>>(
+		 		this->device_ptr_a0, this->device_ptr_b0, this->device_ptr_c0,
+		 		this->device_ptr_d0, this->rows_a, this->cols_b, this->cols_c,
+		 		this->alpha, this->beta);
 
 		
 		this->debug("device synchronize");
