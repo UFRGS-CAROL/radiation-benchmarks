490fc44c879e8c77a9d1cbf22b610a7d96497760
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-07 18:46:10 -0300

    optimized tensor DMR

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -134,8 +134,8 @@ public:
 	real_t* device_ptr_c2 = nullptr;
 
 	real_t* device_ptr_d0 = nullptr;
-	// real_t* device_ptr_d1 = nullptr;
-	half_t* device_ptr_d1 = nullptr;
+	real_t* device_ptr_d1 = nullptr;
+	// half_t* device_ptr_d1 = nullptr;
 	real_t* device_ptr_d2 = nullptr;
 
 	// Size of the matrix
@@ -531,12 +531,12 @@ public:
 			check_framework_errors(
 					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d0),
 							this->rows_c * this->cols_c * sizeof(real_t)));
-			// check_framework_errors(
-			// 		cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d1),
-			// 				this->rows_c * this->cols_c * sizeof(real_t)));
 			check_framework_errors(
 					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d1),
-							this->rows_c * this->cols_c * sizeof(half_t)));
+							this->rows_c * this->cols_c * sizeof(real_t)));
+			// check_framework_errors(
+			// 		cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d1),
+			// 				this->rows_c * this->cols_c * sizeof(half_t)));
 			check_framework_errors(
 					cudaMalloc(reinterpret_cast<void **>(&this->device_ptr_d2),
 							this->rows_c * this->cols_c * sizeof(real_t)));
@@ -571,12 +571,12 @@ public:
 		check_framework_errors(
 				cudaMemset(this->device_ptr_d0, 0x00,
 						this->rows_c * this->cols_c * sizeof(real_t)));
-		// check_framework_errors(
-		// 		cudaMemset(this->device_ptr_d1, 0x00,
-		// 				this->rows_c * this->cols_c * sizeof(real_t)));
-			check_framework_errors(
+		check_framework_errors(
 				cudaMemset(this->device_ptr_d1, 0x00,
-						this->rows_c * this->cols_c * sizeof(half_t)));
+						this->rows_c * this->cols_c * sizeof(real_t)));
+			// check_framework_errors(
+			// 	cudaMemset(this->device_ptr_d1, 0x00,
+			// 			this->rows_c * this->cols_c * sizeof(half_t)));
 		check_framework_errors(
 				cudaMemset(this->device_ptr_d2, 0x00,
 						this->rows_c * this->cols_c * sizeof(real_t)));
