0fe4bf417c33ce3044698bca824e76de3fd13eb7
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-09 18:46:40 -0300

    tensor dmr with diff inputs

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -333,10 +333,10 @@ public:
 				// printf("Required shared memory size: %lu Kb\n", SHMEM_SZ / 1024UL);
 
 
-				checkCudaErrors(cudaFuncSetAttribute(compute_gemm_DMR<half_t, real_t> , cudaFuncAttributeMaxDynamicSharedMemorySize, SHMEM_SZ));
-				checkKernelErrors((compute_gemm_DMR<half_t, real_t> <<<deviceProp.multiProcessorCount, THREADS_PER_BLOCK,SHMEM_SZ>>>
-						(this->device_ptr_a0, this->device_ptr_a1, this->device_ptr_b0, this->device_ptr_c0,
-						 this->device_ptr_d0, this->device_ptr_d1, this->alpha, this->beta)));
+				// checkCudaErrors(cudaFuncSetAttribute(compute_gemm_DMR<half_t, real_t> , cudaFuncAttributeMaxDynamicSharedMemorySize, SHMEM_SZ));
+				// checkKernelErrors((compute_gemm_DMR<half_t, real_t> <<<deviceProp.multiProcessorCount, THREADS_PER_BLOCK,SHMEM_SZ>>>
+				// 		(this->device_ptr_a0, this->device_ptr_a1, this->device_ptr_b0, this->device_ptr_c0,
+				// 		 this->device_ptr_d0, this->device_ptr_d1, this->alpha, this->beta)));
 
 
 				this->debug("device synchronize");
