2559df90ede9c0248bd7c3b6b11ebceccefb3527
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-12 14:58:54 -0300

    dmr fault injection

--- src/cuda/gemm_tensorcores/main.cu
+++ src/cuda/gemm_tensorcores/main.cu
@@ -489,7 +489,7 @@ void call_mxm(half_vector& host_matrix_a, half_vector& host_matrix_b,
 		if (log_obj.triplicated)
 			write_gold_to_file<host_real_t>(log_obj.gold_inout_path, host_gold);
 		else
-			write_gold_to_file<host_real_t>(log_obj.gold_inout_path, host_matrix_d0);
+			write_gold_to_file<host_real_t>(log_obj.gold_inout_path, host_matrix_d1);
 	}
 }
 
