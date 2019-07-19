d72c350a1d2eff4191c4041fc3e9039fb0c65830
Pedro Martins Basso <pedro.basso@inf.ufrgs.br>
2019-07-19 19:09:51 -0300

    setup

--- src/cuda/gemm_tensorcores/GEMMWMMA.h
+++ src/cuda/gemm_tensorcores/GEMMWMMA.h
@@ -544,7 +544,7 @@ public:
 						this->rows_c * this->cols_c * sizeof(real_t)));
 		check_framework_errors(
 				cudaMemset(this->device_ptr_d1, 0x00,
-						this->rows_c * this->cols_c * sizeof(half_real_t)));
+						this->rows_c * this->cols_c * sizeof(real_t)));
 
 		check_framework_errors(
 				cudaMemset(this->device_ptr_d2, 0x00,
