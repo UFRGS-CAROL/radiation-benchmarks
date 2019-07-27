2ee88f0f4d8f3601954158294d963e21ed77ee48
Your Name <you@example.com>
2019-07-27 11:50:59 -0600

    gemm

--- src/cuda/gemm_tensorcores/GEMM.h
+++ src/cuda/gemm_tensorcores/GEMM.h
@@ -20,17 +20,19 @@ public:
 			const std::vector<real_t>&host_c0, // Matric C
 			const std::vector<real_t>& host_d0, size_t k, real_t alpha,
 			real_t beta, GEMMTYPE gemm_type) :
-			GEMMBase<real_t, real_t, real_t>(host_a0, host_b0, host_c0, host_d0, k,
-					alpha, beta, gemm_type) {
+			GEMMBase<real_t, real_t, real_t>(host_a0, host_b0, host_c0, host_d0,
+					k, alpha, beta, gemm_type) {
 
 	}
 	void gemm() {
-		sw_mxm_kernel<<<this->grid_dim, this->block_dim, this->shared_memory>>>(
+		sw_mxm_kernel<<<this->grid_dim, this->block_dim>>>(
 				this->device_ptr_d0.data(), this->device_ptr_c0.data(),
 				this->device_ptr_a0.data(), this->device_ptr_b0.data(),
 				this->alpha, this->beta, this->k, this->k);
 
 		this->debug("sw_mxm device synchronize");
+		rad::checkFrameworkErrors(cudaPeekAtLastError());
+
 		rad::checkFrameworkErrors(cudaDeviceSynchronize());
 	}
 };
@@ -45,26 +47,25 @@ public:
 			const std::vector<real_t>&host_c0, // Matric C
 			const std::vector<real_t>& host_d0, size_t k, real_t alpha,
 			real_t beta, GEMMTYPE gemm_type) :
-			GEMMBase<real_t, real_t, mixed_t>(host_a0, host_b0, host_c0, host_d0, k,
-					alpha, beta, gemm_type) {
+			GEMMBase<real_t, real_t, mixed_t>(host_a0, host_b0, host_c0,
+					host_d0, k, alpha, beta, gemm_type) {
 
 	}
 
 	void gemm() {
 		this->device_is_memory_bad.clear();
-		sw_mxm_dmr_kernel<<<this->grid_dim, this->block_dim, this->shared_memory>>>(
+		sw_mxm_dmr_kernel<<<this->grid_dim, this->block_dim>>>(
 				this->device_ptr_d0.data(), this->device_ptr_mixed_dmr.data(),
 				this->device_ptr_c0.data(), this->device_ptr_a0.data(),
 				this->device_ptr_b0.data(), this->alpha, this->beta, this->k,
 				this->k);
 
 		this->debug("device synchronize");
+		rad::checkFrameworkErrors(cudaPeekAtLastError());
+
 		rad::checkFrameworkErrors(cudaDeviceSynchronize());
 
 	}
-
-
-
 
 };
 
@@ -78,14 +79,14 @@ public:
 			const std::vector<real_t>&host_c0, // Matric C
 			const std::vector<real_t>& host_d0, size_t k, real_t alpha,
 			real_t beta, GEMMTYPE gemm_type) :
-			GEMMBase<real_t, real_t, real_t>(host_a0, host_b0, host_c0, host_d0, k,
-					alpha, beta, gemm_type) {
+			GEMMBase<real_t, real_t, real_t>(host_a0, host_b0, host_c0, host_d0,
+					k, alpha, beta, gemm_type) {
 
 	}
 
 	void gemm() {
 		this->device_is_memory_bad.clear();
-		sw_mxm_dmr_kernel<<<this->grid_dim, this->block_dim, this->shared_memory>>>(
+		sw_mxm_dmr_kernel<<<this->grid_dim, this->block_dim>>>(
 				this->device_ptr_d0.data(), this->device_ptr_mixed_dmr.data(),
 				this->device_ptr_c0.data(), this->device_ptr_a0.data(),
 				this->device_ptr_b0.data(), this->alpha, this->beta, this->k,
