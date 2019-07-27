027a4723f2735adf920bdf238b1624dff1582cfb
Your Name <you@example.com>
2019-07-27 14:31:48 -0600

    gemm kernesl

--- src/cuda/gemm_tensorcores/dmr_kernels.h
+++ src/cuda/gemm_tensorcores/dmr_kernels.h
@@ -18,8 +18,8 @@
 #include "device_functions.h"
 
 template<class real_t>
-__global__ void hw_mxm_dmr_kernel(real_t *D_r, real_t *D_h, real_t *C, real_t *A,
-		real_t *B,  real_t alpha,  real_t beta, int wA, int wB) {
+__global__ void hw_mxm_dmr_kernel(real_t *D_r, real_t *D_h, real_t *C,
+		real_t *A, real_t *B, real_t alpha, real_t beta, int wA, int wB) {
 	/**
 	 * Uma vez que eles sao inline functions eu espero que funcione
 	 */
@@ -28,8 +28,8 @@ __global__ void hw_mxm_dmr_kernel(real_t *D_r, real_t *D_h, real_t *C, real_t *A
 }
 
 template<class half_t, class real_t>
-__global__ void sw_mxm_dmr_kernel(real_t *D_r, half_t *D_h, real_t *C, real_t *A,
-		real_t *B,  real_t alpha,  real_t beta, int wA, int wB) {
+__global__ void sw_mxm_dmr_kernel(real_t *D_r, half_t *D_h, real_t *C,
+		real_t *A, real_t *B, real_t alpha, real_t beta, int wA, int wB) {
 	// Block index
 	int bx = blockIdx.x;
 	int by = blockIdx.y;
@@ -58,6 +58,7 @@ __global__ void sw_mxm_dmr_kernel(real_t *D_r, half_t *D_h, real_t *C, real_t *A
 	real_t Csub = 0;
 
 	half_t Csub_half = 0;
+	double threshold = -2222;
 	// Loop over all the sub-matrices of A and B
 	// required to compute the block sub-matrix
 	for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
@@ -88,13 +89,13 @@ __global__ void sw_mxm_dmr_kernel(real_t *D_r, half_t *D_h, real_t *C, real_t *A
 			Csub_half += half_t(As[ty][k]) * half_t(Bs[k][tx]);
 
 #if CHECKBLOCK >= 1
-		if((k % CHECKBLOCK) == 0) {
-			check_relative_error(Csub_half, Csub);
+			if((k % CHECKBLOCK) == 0) {
+				check_relative_error(Csub_half, Csub);
 
-//			theshold = fmax(theshold, fabs(double(acc_full) - double(acc_incomplete)));
+				threshold = fmax(threshold, fabs(double(Csub) - double(Csub_half)));
 
-			Csub_half = half_t(Csub);
-		}
+				Csub_half = half_t(Csub);
+			}
 #endif
 
 		}
@@ -107,9 +108,11 @@ __global__ void sw_mxm_dmr_kernel(real_t *D_r, half_t *D_h, real_t *C, real_t *A
 
 #if CHECKBLOCK == 0
 	check_relative_error(Csub_half, Csub);
-//	theshold = fmax(theshold, fabs(double(acc_full) - double(acc_incomplete)));
+	threshold = fmax(threshold, fabs(double(Csub) - double(Csub_half)));
 
 #endif
+
+	printf("%.20e\n", threshold);
 
 // Write the block sub-matrix to device memory;
 // each thread writes one element
