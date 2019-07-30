833671c4ea1ae888da7b1ec8065699fa565c4c8d
Your Name <you@example.com>
2019-07-29 22:22:13 -0600

    non dmr

--- src/cuda/gemm_tensorcores/nondmr_kernels.h
+++ src/cuda/gemm_tensorcores/nondmr_kernels.h
@@ -226,4 +226,81 @@ __global__ void sw_mxm_kernel(real_t *D, real_t *C, real_t *A, real_t *B,
 	D[c + wB * ty + tx] = alpha * Csub + beta * C[c + wB * ty + tx];
 }
 
+__global__ void sw_mxm_kernel(half *D, half *C, half *A, half *B, half alpha,
+		half beta, int wA, int wB) {
+	// Block index
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+
+	// Thread index
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+
+	// Index of the first sub-matrix of A processed by the block
+	int aBegin = wA * BLOCK_SIZE * by;
+
+	// Index of the last sub-matrix of A processed by the block
+	int aEnd = aBegin + wA - 1;
+
+	// Step size used to iterate through the sub-matrices of A
+	int aStep = BLOCK_SIZE;
+
+	// Index of the first sub-matrix of B processed by the block
+	int bBegin = BLOCK_SIZE * bx;
+
+	// Step size used to iterate through the sub-matrices of B
+	int bStep = BLOCK_SIZE * wB;
+
+	// Csub is used to store the element of the block sub-matrix
+	// that is computed by the thread
+	half Csub = 0;
+
+	// Loop over all the sub-matrices of A and B
+	// required to compute the block sub-matrix
+	for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
+		// Declaration of the shared memory array As used to
+		// store the sub-matrix of A
+		__shared__ half As[BLOCK_SIZE][BLOCK_SIZE];
+
+		// Declaration of the shared memory array Bs used to
+		// store the sub-matrix of B
+		__shared__ half Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+		// Load the matrices from device memory
+		// to shared memory; each thread loads
+		// one element of each matrix
+		As[ty][tx] = A[a + wA * ty + tx];
+		Bs[ty][tx] = B[b + wB * ty + tx];
+
+		// Synchronize to make sure the matrices are loaded
+		__syncthreads();
+
+		half2 acc(0, 0);
+		// Multiply the two matrices together;
+		// each thread computes one element
+		// of the block sub-matrix
+#pragma unroll
+		for (int k = 0; k < BLOCK_SIZE; k += 2) {
+//			Csub += As[ty][k] * Bs[k][tx];
+
+			half2 a = __halves2half2(As[ty][k + 0], As[ty][k + 1]);
+			half2 b = __halves2half2(Bs[k + 0][tx], Bs[k + 1][tx]);
+
+			acc = __hfma2(a, b, acc);
+		}
+
+		Csub += acc.x + acc.y;
+		// Synchronize to make sure that the preceding
+		// computation is done before loading two new
+		// sub-matrices of A and B in the next iteration
+		__syncthreads();
+	}
+
+	// Write the block sub-matrix to device memory;
+	// each thread writes one element
+	int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+
+	D[c + wB * ty + tx] = alpha * Csub + beta * C[c + wB * ty + tx];
+}
+
 #endif /* NONDMR_KERNELS_H_ */
