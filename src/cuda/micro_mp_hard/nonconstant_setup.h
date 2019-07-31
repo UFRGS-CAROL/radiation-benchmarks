e68de7d5064adbe595e8a6884074a539de32ae1e
Your Name <you@example.com>
2019-07-30 19:17:08 -0600

    non constant

--- src/cuda/micro_mp_hard/nonconstant_setup.h
+++ src/cuda/micro_mp_hard/nonconstant_setup.h
@@ -262,8 +262,8 @@ void test_radiation(Parameters& parameters, std::vector<real_t>& input_array,
 			std::cout << "MAX THRESHOLD: " << max_threshold << std::endl;
 			std::cout << "MIN THRESHOLD: " << min_threshold << std::endl;
 			std::cout << "MEDIAN THRESHOLD: " << median << std::endl;
-			std::cout << "input[" << last_i << std::fixed
-					<< "] for MAX THRESHOLD: " << std::scientific
+			std::cout << "input[" << std::fixed << last_i;
+			std::cout << "] for MAX THRESHOLD: " << std::scientific
 					<< input_array[last_i] << std::endl;
 			std::cout << "-----------------------------------------------"
 					<< std::endl;
