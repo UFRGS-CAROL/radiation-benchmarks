6c42057fb4468a0d57e8050a805c87471313c17e
Fernando <fernandofernandesantos@gmail.com>
2019-09-12 16:30:24 -0300

    config cache

--- src/cuda/cache/config_cache.py
+++ src/cuda/cache/config_cache.py
@@ -43,14 +43,15 @@ def config(board, debug):
 
     # gen only for max size, defined on cuda_trip_mxm.cu
     for i in TYPES:
-        exe = [None] * 5
-        exe[0] = ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ', bin_path + "/" + benchmark_bin + " "]
-        exe[1] = ['--iterations {}'.format(ITERATIONS)]
-        exe[2] = ['--sleepongpu {}'.format(SLEEPONGPU)]
-        exe[3] = ['--memtotest ' + i]
-        exe[4] = ['--verbose 1']
+        if i != "L2":
+            exe = [None] * 5
+            exe[0] = ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ', bin_path + "/" + benchmark_bin + " "]
+            exe[1] = ['--iterations {}'.format(ITERATIONS)]
+            exe[2] = ['--sleepongpu {}'.format(SLEEPONGPU)]
+            exe[3] = ['--memtotest ' + i]
+            exe[4] = ['--verbose 1']
 
-        execute.append(' '.join(str(r) for v in exe for r in v))
+            execute.append(' '.join(str(r) for v in exe for r in v))
 
     execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)  
 
