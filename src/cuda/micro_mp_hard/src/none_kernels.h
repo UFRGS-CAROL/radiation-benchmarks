11cfdf0ad807a7610f2d2bd3b6695169e0c21147
Fernando <fernandofernandesantos@gmail.com>
2019-09-16 16:04:42 -0300

    micro mp hard

--- src/cuda/micro_mp_hard/src/none_kernels.h
+++ src/cuda/micro_mp_hard/src/none_kernels.h
@@ -23,8 +23,10 @@ __global__ void microbenchmark_kernel_fma(real_t* output_1, real_t* output_2,
 		real_t* output_3) {
 
 	__shared__ real_t shared_mem[MAXSHAREDMEMORY / sizeof(real_t)];
-	const uint32 thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+	shared_mem[threadIdx.x] = 0;
 
+
+	const uint32 thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 	register real_t acc_real_t = 0.0;
 	register real_t this_thread_input_real_t = real_t(
 			input_constant[blockIdx.x]);
@@ -38,7 +40,7 @@ __global__ void microbenchmark_kernel_fma(real_t* output_1, real_t* output_2,
 	output_1[thread_id] = acc_real_t;
 	output_2[thread_id] = acc_real_t;
 	output_3[thread_id] = acc_real_t;
-	shared_mem[threadIdx.x] = acc_real_t;
+	acc_real_t = shared_mem[threadIdx.x];
 }
 
 /**
@@ -51,6 +53,7 @@ template<typename real_t>
 __global__ void microbenchmark_kernel_add(real_t* output_1, real_t* output_2,
 		real_t* output_3) {
 	__shared__ real_t shared_mem[MAXSHAREDMEMORY / sizeof(real_t)];
+	shared_mem[threadIdx.x] = 0;
 
 	const uint32 thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -66,7 +69,7 @@ __global__ void microbenchmark_kernel_add(real_t* output_1, real_t* output_2,
 	output_1[thread_id] = acc_real_t;
 	output_2[thread_id] = acc_real_t;
 	output_3[thread_id] = acc_real_t;
-	shared_mem[threadIdx.x] = acc_real_t;
+	acc_real_t = shared_mem[threadIdx.x];
 }
 
 /**
@@ -79,6 +82,7 @@ template<typename real_t>
 __global__ void microbenchmark_kernel_mul(real_t* output_1, real_t* output_2,
 		real_t* output_3) {
 	__shared__ real_t shared_mem[MAXSHAREDMEMORY / sizeof(real_t)];
+	shared_mem[threadIdx.x] = 0;
 
 	const uint32 thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -94,7 +98,8 @@ __global__ void microbenchmark_kernel_mul(real_t* output_1, real_t* output_2,
 	output_1[thread_id] = acc_real_t;
 	output_2[thread_id] = acc_real_t;
 	output_3[thread_id] = acc_real_t;
-	shared_mem[threadIdx.x] = acc_real_t;
+
+	acc_real_t = shared_mem[threadIdx.x];
 }
 
 #endif /* NONE_KERNELS_H_ */
