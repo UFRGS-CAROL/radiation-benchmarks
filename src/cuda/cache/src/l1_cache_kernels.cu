c76581a189d16330a43363cd88789f497b93bb72
reliabilitylogdata <reliabilitylogdata@github.com>
2019-09-20 23:21:48 -0300

    l1 cache

--- src/cuda/cache/src/l1_cache_kernels.cu
+++ src/cuda/cache/src/l1_cache_kernels.cu
@@ -14,88 +14,51 @@
 #include "Memory.h"
 #include "L1Cache.h"
 
-#define NUMBEROFELEMENTS 96
-#include "l1_move_function.h"
+#define NUMBEROFELEMENTS 48
 
-template<const uint32 SHARED_PER_SM>
+/*
+ * l1_size size of the L1 cache
+ * V_size = l1_size / sizeof(CacheLine)
+ */
+template<const uint32 COUNT, const uint32 SHARED_PER_SM>
 __global__ void test_l1_cache_kernel(uint64 *in, uint64 *out, int64 *hits,
 		int64 *miss, const int64 sleep_cycles) {
 
 	__shared__ int64 l1_t_hit[SHARED_PER_SM];
 	__shared__ int64 l1_t_miss[SHARED_PER_SM];
 
-	const register uint64 i = (blockIdx.x * blockDim.x + threadIdx.x) * NUMBEROFELEMENTS;
+	const uint64 i = (blockIdx.x * blockDim.x + threadIdx.x) * NUMBEROFELEMENTS;
 
-	register uint64 rs[NUMBEROFELEMENTS]; //, rt[NUMBEROFELEMENTS];
+	uint64 rs[COUNT], rt[COUNT];
 
 	const int64 t1_miss = clock64();
-	mov_cache_data(rs, in + i);
-	const int64 t2_miss = clock64();
-
-	rs[NUMBEROFELEMENTS / 2] = 34;
+#pragma unroll COUNT
+	for(uint32 k = 0; k < COUNT; k++){
+		rs[k] = in[i + k];
+	}
+	l1_t_miss[threadIdx.x] = clock64() - t1_miss;
 
 	//wait for exposition to neutrons
 	sleep_cuda(sleep_cycles);
 
 	//last checking
 	const register int64 t1_hit = clock64();
-	mov_cache_data(rs, in + i);
-	const register int64 t2_hit = clock64();
+#pragma unroll COUNT
+	for(uint32 k = 0; k < COUNT; k++){
+		rt[k] = in[i + k];
+	}
+	l1_t_hit[threadIdx.x] = clock64() - t1_hit;
 
-	mov_cache_data(out + i, rs);
-//	mov_cache_data(in + i, rs);
+#pragma unroll COUNT
+	for(uint32 k = 0; k < COUNT; k++){
+		in[i + k] = rs[k];
+		out[i + k] = rt[k];
+	}
 
-//saving miss and hit
-	l1_t_miss[threadIdx.x] = t2_miss - t1_miss;
-	l1_t_hit[threadIdx.x] = t2_hit - t1_hit;
+	//saving miss and hit
 	miss[i] = l1_t_miss[threadIdx.x];
-	hits[i] = l1_t_hit[threadIdx.x];
-
+    hits[i] = l1_t_hit[threadIdx.x];
 }
-
-/*
- * l1_size size of the L1 cache
- * V_size = l1_size / sizeof(CacheLine)
- */
-//template<const uint32 V_SIZE, const uint32 SHARED_PER_SM>
-//__global__ void test_l1_cache_kernel(uint64 *in, uint64 *out, int64 *hits,
-//		int64 *miss, const int64 sleep_cycles) {
-//
-//	__shared__ int64 l1_t_hit[SHARED_PER_SM];
-//	__shared__ int64 l1_t_miss[SHARED_PER_SM];
-//	const register uint64 i = blockIdx.x * V_SIZE + threadIdx.x;
-//
-//	if (threadIdx.x < V_SIZE && blockIdx.y == 0) {
-//
-//		const register uint64 index = i * CACHE_LINE_SIZE_BY_INT32;
-//
-//		register uint64 rs[CACHE_LINE_SIZE_BY_INT32];
-//		register uint64 rt[CACHE_LINE_SIZE_BY_INT32];
-//
-//		const int64 t1_miss = clock64();
-//		move_cache_line(rs, in + index);
-//		const int64 t2_miss = clock64();
-//
-//		//wait for exposition to neutrons
-//		sleep_cuda(sleep_cycles);
-//
-//		//last checking
-//		const register int64 t1_hit = clock64();
-//		move_cache_line(rt, in + index);
-//		const register int64 t2_hit = clock64();
-//
-//		//triplication
-//		move_cache_line(out + index, rt);
-//		move_cache_line(in + index, rs);
-//
-////saving miss and hit
-//		l1_t_miss[threadIdx.x] = t2_miss - t1_miss;
-//		l1_t_hit[threadIdx.x] = t2_hit - t1_hit;
-//		miss[i] = l1_t_miss[threadIdx.x];
-//		hits[i] = l1_t_hit[threadIdx.x];
-//	}
-//
-//}
 
 L1Cache::L1Cache(const Parameters& parameters) :
 		Memory<uint64>(parameters) {
@@ -145,7 +108,7 @@ void L1Cache::test(const uint64& mem) {
 		//to force alloc maximum shared memory
 //		constexpr uint32 v_size = MAX_KEPLER_L1_MEMORY / CACHE_LINE_SIZE;
 
-		test_l1_cache_kernel<MAX_KEPLER_SHARED_MEMORY_TO_TEST_L1> <<<
+		test_l1_cache_kernel<NUMBEROFELEMENTS, MAX_KEPLER_SHARED_MEMORY_TO_TEST_L1> <<<
 				block_size, threads_per_block>>>(input_device_1.data(),
 				output_device_1.data(), hit_vector_device.data(),
 				miss_vector_device.data(), cycles);
