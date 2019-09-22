4473fd1dd5480a4b449ab80c71178c6939518cf3
reliabilitylogdata <reliabilitylogdata@github.com>
2019-09-22 16:15:19 -0300

    config

--- src/cuda/micro_mp_hard/config_cuda_trip_micro.py
+++ src/cuda/micro_mp_hard/config_cuda_trip_micro.py
@@ -2,7 +2,6 @@
 
 import ConfigParser
 import copy
-import os
 import sys
 
 sys.path.insert(0, '../../include')
@@ -10,12 +9,14 @@ from common_config import discover_board, execute_and_write_json_to_file
 
 ITERATIONS = int(1e10)
 PRECISIONS = ["double"]
+DMR = ["dmr", "dmrmixed", "none"]
 TYPES = ["fma", "add", "mul"]
+CHECK_BLOCK = [1, 10, 100, 1000]
 
 
-def config(board, test_type, arith_type, debug):
-    benchmark_bin = "cuda_micro-" + test_type + "_" + arith_type
-    print "Generating " + benchmark_bin + " for CUDA, board:" + board
+def config(board, debug):
+    benchmark_bin = "cuda_micro_mp_hardening"
+    print("Generating {} for CUDA, board: {}".format(benchmark_bin, board))
 
     conf_file = '/etc/radiation-benchmarks.conf'
     try:
@@ -24,40 +25,62 @@ def config(board, test_type, arith_type, debug):
         install_dir = config.get('DEFAULT', 'installdir') + "/"
 
     except IOError as e:
-        print >> sys.stderr, "Configuration setup error: " + str(e)
-        sys.exit(1)
+        raise ValueError("Configuration setup error: " + str(e))
 
     bin_path = install_dir + "bin"
-    src_benchmark = install_dir + "src/cuda/trip_micro"
+    src_benchmark = install_dir + "src/cuda/micro_mp_hard"
+    data_path = install_dir + "data/micro_mp_hard"
 
     generate = ["sudo mkdir -p " + bin_path,
+                "sudo mkdir -p " + data_path,
                 "cd " + src_benchmark,
                 "make clean",
                 "make -C ../../include ",
-                "make TYPE=" + test_type + " PRECISION=" + arith_type + " -j 4 OPS=" + str(ops),
+                "make",
                 "sudo mv -f ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
 
-    exe = [None] * 2
-    exe[0] = ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ',
-              bin_path + '/' + benchmark_bin + " "]
-    exe[1] = ['-iterations=' + str(ITERATIONS)]
+    for precision in PRECISIONS:
+        for type_ in TYPES:
+            gold_path = data_path + "/gold_{}_{}.data".format(type_, precision)
+            gen = [None] * 8
 
-    execute.append(' '.join(str(r) for v in exe for r in v))
+            for dmr in DMR:
+                    for op in CHECK_BLOCK:
+                        gen[0] = ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ',
+                                  bin_path + '/' + benchmark_bin + " "]
+                        gen[1] = ['--iterations {}'.format(ITERATIONS)]
+                        gen[2] = ['--redundancy {}'.format(dmr)]
+                        gen[3] = ['--inst {}'.format(type_)]
+                        gen[4] = ['--gold {}'.format(gold_path)]
+                        gen[5] = ['--opnum {}'.format(op)]
+                        gen[6] = ['--precision {}'.format(precision)]
+                        gen[7] = ['--generate']
 
-    execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)
+                        exe = copy.deepcopy(gen)
+                        exe[-1] = []
+                        execute.append(' '.join(str(r) for v in exe for r in v))
+
+            generate.append(' '.join(str(r) for v in gen for r in v))
+
+    if debug is False:
+        execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)
+    else:
+        for g in generate:
+            print(g)
+        print()
+        for e in execute:
+            print(e)
 
 
 if __name__ == "__main__":
+    debug_mode = False
     try:
-        parameter = str(sys.argv[1:][1]).upper()
+        parameter = str(sys.argv[1:][0]).upper()
         if parameter == 'DEBUG':
             debug_mode = True
     except:
-        debug_mode = False
+        pass
 
     board, _ = discover_board()
-    for test_type in TYPES:
-        for arith_type in PRECISIONS:
-            config(board=board, test_type=test_type, arith_type=arith_type, debug=debug_mode)
-    print "Multiple jsons may have been generated."
+    config(board=board, debug=debug_mode)
