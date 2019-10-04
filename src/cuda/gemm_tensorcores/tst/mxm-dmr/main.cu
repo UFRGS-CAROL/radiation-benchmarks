3bae6da176c53abd5a339f8bcef55b1926d3f7a1
pedrombasso <pedro.basso@inf.ufrgs.br>
2019-10-04 10:31:04 +0200

    mxm test

--- src/cuda/gemm_tensorcores/tst/mxm-dmr/main.cu
+++ src/cuda/gemm_tensorcores/tst/mxm-dmr/main.cu
@@ -37,8 +37,8 @@ int main(int argc, char **argv) {
 	cudaMemcpy(device_a, host_a, m * k * sizeof(real_t), cudaMemcpyHostToDevice);
 	cudaMemcpy(device_b, host_b, k * n * sizeof(real_t), cudaMemcpyHostToDevice);
 	cudaMemcpy(device_c, host_c, m * n * sizeof(real_t), cudaMemcpyHostToDevice);
-	cudaMemcpy(device_c, host_d, m * n * sizeof(real_t), cudaMemcpyHostToDevice);
-	cudaMemcpy(device_c_half, host_d_half, m * n * sizeof(half_t), cudaMemcpyHostToDevice);
+	cudaMemcpy(device_d, host_d, m * n * sizeof(real_t), cudaMemcpyHostToDevice);
+	cudaMemcpy(device_d_half, host_d_half, m * n * sizeof(half_t), cudaMemcpyHostToDevice);
 
 	matrix_mult_dmr<THRESHOLD, CHECK_BLOCK>(device_a, device_b, m, n, k, device_d, device_d_half, alpha, beta, device_c);
 
