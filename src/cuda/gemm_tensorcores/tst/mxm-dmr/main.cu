44094367e32b9692c08025acd3b883d6781e77c8
pedrombasso <pedro.basso@inf.ufrgs.br>
2019-10-13 16:59:46 +0200

    threshold

--- src/cuda/gemm_tensorcores/tst/mxm-dmr/main.cu
+++ src/cuda/gemm_tensorcores/tst/mxm-dmr/main.cu
@@ -22,10 +22,10 @@ int main(int argc, char **argv) {
 
 	std::random_device rd; // obtain a random number from hardware
     std::mt19937 eng(rd()); // seed the generator
-    std::uniform_int_distribution<> distr(-1000, 1000); 
+    std::uniform_int_distribution<> distr(-1000.00, 1000.00); 
 
-	real_t alpha = 1;
-	real_t beta = 1;
+  	const real_t alpha = 1.1f;
+  	const real_t beta = 1.2f;
 
 	real_t* host_a = (real_t*)calloc(m * k, sizeof(real_t));
 	real_t* host_b = (real_t*)calloc(k * n, sizeof(real_t));
