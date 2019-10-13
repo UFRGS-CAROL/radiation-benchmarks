1b681e6f73ad24372c09c7fb888847ab20a5ab26
carol <fernandofernandesantos@gmail.com>
2019-10-13 14:44:06 +0000

    threshold

--- src/cuda/gemm_tensorcores/tst/mxm-dmr/kernels.h
+++ src/cuda/gemm_tensorcores/tst/mxm-dmr/kernels.h
@@ -94,8 +94,8 @@ __device__ __forceinline__ void check_bit_error(const float& lhs, const double& 
     const uint32_t lhs_data = __float_as_uint(lhs);
     const uint32_t rhs_data = (uint32_t)__float_as_uint(rhs);
     
- 
-     printf("Data rhs: %.20e \n", rhs_data);
+    printf( " lhs: %f  --- rhs : %f \n ", lhs, rhs);  
+    //printf("Data rhs: %.20e --- data lhs: %.20e \n", rhs_data, lhs_data);
 
 
     uint32_t sub_res;
@@ -106,7 +106,7 @@ __device__ __forceinline__ void check_bit_error(const float& lhs, const double& 
     }
     
     
-    //printf("THRESHOLD: %.20u \n", sub_res);
+    printf("THRESHOLD: %u \n", sub_res);
     const double zero = double(ZERO_DOUBLE);
     if (sub_res > zero) {
         atomicAdd(&errors, 1);
