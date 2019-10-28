d2b9dfb33eccc69c1e9cae373c854b4fde9d28e8
reliabilitylogdata <reliabilitylogdata@github.com>
2019-10-28 15:25:16 -0300

    testing volatile

--- src/cuda/gemm_tensorcores/src/device_functions.h
+++ src/cuda/gemm_tensorcores/src/device_functions.h
@@ -30,7 +30,7 @@ half abs__(half a) {
 }
 
 template<typename real_t> __DEVICE_FUNCTION_INLINE__
-void check_relative_error(real_t& lhs, real_t& rhs, const uint32_t threshold) {
+void check_relative_error(volatile real_t& lhs, volatile  real_t& rhs, const uint32_t threshold) {
 	real_t diff = abs__(lhs - rhs);
 	if (diff > real_t(ZERO_DOUBLE)) {
 		atomicAdd(&errors, 1);
@@ -39,7 +39,7 @@ void check_relative_error(real_t& lhs, real_t& rhs, const uint32_t threshold) {
 }
 
 __DEVICE_FUNCTION_INLINE__
-void check_relative_error(float& lhs, double& rhs, const uint32_t threshold) {
+void check_relative_error(volatile float& lhs, volatile double& rhs, const uint32_t threshold) {
 	const float rhs_as_float = float(rhs);
 	const uint32_t lhs_data = __float_as_uint(rhs_as_float);
 	const uint32_t rhs_data = __float_as_uint(rhs);
@@ -65,6 +65,17 @@ __DEVICE_FUNCTION_INLINE__
 void fma__(const float& a, const float& b, float& acc) {
 	acc = __fmaf_rn(a, b, acc);
 }
+
+__DEVICE_FUNCTION_INLINE__
+void fma__(const volatile float& a, const volatile float& b, volatile float& acc) {
+	acc = __fmaf_rn(a, b, acc);
+}
+
+__DEVICE_FUNCTION_INLINE__
+void fma__(const volatile double& a, const volatile double& b, volatile double& acc) {
+	acc = __fma_rn(a, b, acc);
+}
+
 
 __DEVICE_FUNCTION_INLINE__
 void fma__(const half& a, const half& b, half& acc) {
