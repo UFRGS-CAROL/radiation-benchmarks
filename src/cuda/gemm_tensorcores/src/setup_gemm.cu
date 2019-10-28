047123f708b8ffc4845aef1c63bdd56c874d1f71
reliabilitylogdata <reliabilitylogdata@github.com>
2019-10-28 13:30:41 -0300

    setup gemm

--- src/cuda/gemm_tensorcores/src/setup_gemm.cu
+++ src/cuda/gemm_tensorcores/src/setup_gemm.cu
@@ -49,8 +49,6 @@ struct UnhardenedGemmCaller: public GemmCaller<0, real_t, real_t> {
 	} //default constructor
 };
 
-
-
 template<const uint32_t COUNT, typename half_t, typename real_t>
 struct DMRMixedGemmCaller: public GemmCaller<COUNT, half_t, real_t> {
 	void gemm(
@@ -61,7 +59,7 @@ struct DMRMixedGemmCaller: public GemmCaller<COUNT, half_t, real_t> {
 			rad::DeviceVector<half_t>& d_dev_half_t,  	//D_Half matrix
 			real_t alpha, real_t beta, int wA, int wB,
 			const uint32_t threshold) {
-		matrix_mult_kernel<COUNT><<<this->dim_grid, this->dim_block>>>( //call
+		matrix_mult_kernel<COUNT> <<<this->dim_grid, this->dim_block>>>( //call
 				a_dev.data(), 				//a
 				b_dev.data(), 				//b
 				c_dev.data(), 				//c
@@ -76,9 +74,9 @@ struct DMRMixedGemmCaller: public GemmCaller<COUNT, half_t, real_t> {
 };
 
 template<const uint32_t COUNT, typename real_t>
-struct DMRGemmCaller : public DMRMixedGemmCaller<COUNT, real_t, real_t>{
+struct DMRGemmCaller: public DMRMixedGemmCaller<COUNT, real_t, real_t> {
 	DMRGemmCaller(uint32_t m, uint32_t n) :
-		DMRMixedGemmCaller<COUNT, real_t, real_t>(m, n) {
+			DMRMixedGemmCaller<COUNT, real_t, real_t>(m, n) {
 	}
 };
 
@@ -93,14 +91,12 @@ void setup_execute(Log& log_obj, GemmCaller<COUNT, half_t, real_t>& mult_env,
 			log_obj.size_matrices * log_obj.size_matrices);
 	std::vector<real_t> c_vector_host(
 			log_obj.size_matrices * log_obj.size_matrices);
-	std::vector<real_t> d_vector_host(
-			log_obj.size_matrices * log_obj.size_matrices);
 	std::vector<real_t> gold_host(
 			log_obj.size_matrices * log_obj.size_matrices);
 
-	//smaller precision
-	std::vector<half_t> d_vector_half_t_host(
-			log_obj.size_matrices * log_obj.size_matrices);
+	//Output host vectors are set after computation
+	std::vector<real_t> d_vector_host_real_t;
+	std::vector<half_t> d_vector_host_half_t;
 
 	if (log_obj.generate) {
 		std::cout << "Generating input matrices\n";
@@ -113,11 +109,13 @@ void setup_execute(Log& log_obj, GemmCaller<COUNT, half_t, real_t>& mult_env,
 				log_obj.c_input_path, log_obj.gold_inout_path);
 	}
 
-	rad::DeviceVector < real_t > a_vector_device(a_vector_host);
-	rad::DeviceVector < real_t > b_vector_device(b_vector_host);
-	rad::DeviceVector < real_t > c_vector_device(c_vector_host);
-	rad::DeviceVector < real_t > d_vector_device(d_vector_host);
-	rad::DeviceVector < half_t > d_vector_half_t_device(d_vector_half_t_host);
+	rad::DeviceVector<real_t> a_vector_device = a_vector_host;
+	rad::DeviceVector<real_t> b_vector_device = b_vector_host;
+	rad::DeviceVector<real_t> c_vector_device = c_vector_host;
+	rad::DeviceVector<real_t> d_vector_device(
+			log_obj.size_matrices * log_obj.size_matrices);
+	rad::DeviceVector<half_t> d_vector_half_t_device(
+			log_obj.size_matrices * log_obj.size_matrices);
 
 	std::cout << "Starting the setup process...\n";
 	for (int it = 0; it < log_obj.iterations; it++) {
@@ -137,16 +135,17 @@ void setup_execute(Log& log_obj, GemmCaller<COUNT, half_t, real_t>& mult_env,
 		std::cout << "Elapsed time : " << elapsedTime << " %f ms\n";
 
 		double copy_time = rad::mysecond();
-		d_vector_half_t_host = d_vector_half_t_device.to_vector();
-		d_vector_host = d_vector_device.to_vector();
+		d_vector_host_half_t = d_vector_half_t_device.to_vector();
+		d_vector_host_real_t = d_vector_device.to_vector();
 		copy_time = copy_time - rad::mysecond();
 
 		if (!log_obj.generate) {
 			double start, end;
 
 			start = rad::mysecond();
-			auto errors = check_output_errors_dmr(gold_host, d_vector_host,
-					d_vector_half_t_host, log_obj, threshold);
+			auto errors = check_output_errors_dmr(gold_host,
+					d_vector_host_real_t, d_vector_host_half_t, log_obj,
+					threshold);
 			end = rad::mysecond();
 
 			std::cout << "Iteration: " << it << " dmr errors " << errors.first
@@ -172,8 +171,8 @@ void setup_execute(Log& log_obj, GemmCaller<COUNT, half_t, real_t>& mult_env,
 				a_vector_device = a_vector_host;
 				b_vector_device = b_vector_host;
 				c_vector_device = c_vector_host;
-				d_vector_device = d_vector_host;
-				d_vector_half_t_device = d_vector_half_t_host;
+				d_vector_device = d_vector_host_real_t;
+				d_vector_half_t_device = d_vector_host_half_t;
 
 			}
 
@@ -183,9 +182,10 @@ void setup_execute(Log& log_obj, GemmCaller<COUNT, half_t, real_t>& mult_env,
 
 	std::cout << "time : " << (elapsedTime / log_obj.iterations) << " s\n";
 	if (log_obj.generate) {
-		write_gold(a_vector_host, b_vector_host, c_vector_host, d_vector_host,
-				log_obj.a_input_path, log_obj.b_input_path,
-				log_obj.c_input_path, log_obj.gold_inout_path);
+		write_gold(a_vector_host, b_vector_host, c_vector_host,
+				d_vector_host_real_t, log_obj.a_input_path,
+				log_obj.b_input_path, log_obj.c_input_path,
+				log_obj.gold_inout_path);
 	}
 }
 
