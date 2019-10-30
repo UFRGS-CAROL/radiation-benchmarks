805ee4a42c1edcd1047598821361d10ba6fe5645
reliabilitylogdata <reliabilitylogdata@github.com>
2019-10-29 21:30:53 -0300

    setu gemm

--- src/cuda/gemm_tensorcores/src/setup_gemm.cu
+++ src/cuda/gemm_tensorcores/src/setup_gemm.cu
@@ -212,9 +212,9 @@ void setup_gemm_unhardened(Log& log) {
 //	}
 
 	if (log.precision == "double") {
-//		UnhardenedGemmCaller<double> gemm_obj(log.size_matrices,
-//				log.size_matrices);
-//		setup_execute(log, gemm_obj);
+		UnhardenedGemmCaller<double> gemm_obj(log.size_matrices,
+				log.size_matrices);
+		setup_execute(log, gemm_obj);
 		return;
 	}
 }
@@ -234,9 +234,9 @@ void setup_gemm_dmr(Log& log) {
 					log.size_matrices);
 			setup_execute(log, gemm_obj, THRESHOLD_1);
 		} else {
-//			DMRGemmCaller<1, double> gemm_obj(log.size_matrices,
-//					log.size_matrices);
-//			setup_execute(log, gemm_obj);
+			DMRGemmCaller<1, double> gemm_obj(log.size_matrices,
+					log.size_matrices);
+			setup_execute(log, gemm_obj);
 		}
 		return;
 	}
