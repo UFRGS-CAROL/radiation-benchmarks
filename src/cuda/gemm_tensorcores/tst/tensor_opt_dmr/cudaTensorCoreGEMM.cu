10cbbd113e0de871301f7bc0dbbcee3e0100f1bb
pedrombasso <pedro.basso@inf.ufrgs.br>
2019-12-03 19:29:53 +0100

    tensor optimized streams

--- src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
+++ src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
@@ -143,77 +143,81 @@ __global__ void MatrixMulCUDA(const T *A, const T *B, const T *C, T* D,
 
 }
 
-__global__ void MatrixMulCUDA(const float *A, const float *B, const float *C,
-		float* D, const float alpha, const float beta, const int wA,
-		const int wB) {
-	// Block index
-	int bx = blockIdx.x;
-	int by = blockIdx.y;
+_global__ void matrix_mult(half *A, half *B, int wA,
+    int wB, half *C, half alpha, half beta) {
+  // Block index
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
 
-	// Thread index
-	int tx = threadIdx.x;
-	int ty = threadIdx.y;
+  // Thread index
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
 
-	// Index of the first sub-matrix of A processed by the block
-	int aBegin = wA * BLOCK_SIZE * by;
+  // Index of the first sub-matrix of A processed by the block
+  int aBegin = wA * BLOCK_SIZE * by;
 
-	// Index of the last sub-matrix of A processed by the block
-	int aEnd = aBegin + wA - 1;
+  // Index of the last sub-matrix of A processed by the block
+  int aEnd   = aBegin + wA - 1;
 
-	// Step size used to iterate through the sub-matrices of A
-	int aStep = BLOCK_SIZE;
+  // Step size used to iterate through the sub-matrices of A
+  int aStep  = BLOCK_SIZE;
 
-	// Index of the first sub-matrix of B processed by the block
-	int bBegin = BLOCK_SIZE * bx;
+  // Index of the first sub-matrix of B processed by the block
+  int bBegin = BLOCK_SIZE * bx;
 
-	// Step size used to iterate through the sub-matrices of B
-	int bStep = BLOCK_SIZE * wB;
+  // Step size used to iterate through the sub-matrices of B
+  int bStep  = BLOCK_SIZE * wB;
 
-	// Csub is used to store the element of the block sub-matrix
-	// that is computed by the thread
-	float Csub = 0;
+  // Csub is used to store the element of the block sub-matrix
+  // that is computed by the thread
+  half Csub = 0;
 
-	// Loop over all the sub-matrices of A and B
-	// required to compute the block sub-matrix
-	for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
-		// Declaration of the shared memory array As used to
-		// store the sub-matrix of A
-		__shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+  // Loop over all the sub-matrices of A and B
+  // required to compute the block sub-matrix
+  for (int a = aBegin, b = bBegin;
+       a <= aEnd;
+       a += aStep, b += bStep) {
+    // Declaration of the shared memory array As used to
+    // store the sub-matrix of A
+    __shared__ half As[BLOCK_SIZE][BLOCK_SIZE];
 
-		// Declaration of the shared memory array Bs used to
-		// store the sub-matrix of B
-		__shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+    // Declaration of the shared memory array Bs used to
+    // store the sub-matrix of B
+    __shared__ half Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-		// Load the matrices from device memory
-		// to shared memory; each thread loads
-		// one element of each matrix
-		As[ty][tx] = A[a + wA * ty + tx];
-		Bs[ty][tx] = B[b + wB * ty + tx];
+    // Load the matrices from device memory
+    // to shared memory; each thread loads
+    // one element of each matrix
+    As[ty][tx] = A[a + wA * ty + tx];
+    Bs[ty][tx] = B[b + wB * ty + tx];
 
-		// Synchronize to make sure the matrices are loaded
-		__syncthreads();
+    // Synchronize to make sure the matrices are loaded
+    __syncthreads();
 
-		// Multiply the two matrices together;
-		// each thread computes one element
-		// of the block sub-matrix
+    // Multiply the two matrices together;
+    // each thread computes one element
+    // of the block sub-matrix
 #pragma unroll
 
-		for (int k = 0; k < BLOCK_SIZE; ++k) {
-			Csub += As[ty][k] * Bs[k][tx];
-		}
+    for (int k = 0; k < BLOCK_SIZE; ++k) {
+      Csub += As[ty][k] * Bs[k][tx];
+    }
 
-		// Synchronize to make sure that the preceding
-		// computation is done before loading two new
-		// sub-matrices of A and B in the next iteration
-		__syncthreads();
-	}
+    // Synchronize to make sure that the preceding
+    // computation is done before loading two new
+    // sub-matrices of A and B in the next iteration
+    __syncthreads();
+  }
 
-	// Write the block sub-matrix to device memory;
-	// each thread writes one element
-	int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-
-	D[c + wB * ty + tx] = alpha * Csub + beta * C[c + wB * ty + tx];
+  // Write the block sub-matrix to device memory;
+  // each thread writes one element
+  //int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+  const int index = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx + wB * ty + tx;
+  half half_val = alpha * Csub + beta * C[index];
+  C[index] = Csub;
 }
+
+
 
 __global__ void compute_gemm(const half *A, const half *B, const half *C,
 		half *D, half alpha, half beta, int wA, int wB) {
@@ -530,8 +534,15 @@ int main(int argc, char **argv) {
 	cudaStream_t st;
 	cudaStreamCreateWithFlags(&st, cudaStreamNonBlocking);
 	std::cout << BLOCK_SIZE << " " << M_GLOBAL << std::endl;
-	dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 grid( M_GLOBAL / threads.x, M_GLOBAL / threads.y);
+	//dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+	//dim3 grid( M_GLOBAL / threads.x, M_GLOBAL / threads.y);
+
+	dim3 dim_grid, dim_block;
+  
+  	uint32_t grid_rows = (MATRIX_M + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  	uint32_t grid_cols = (MATRIX_N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  	dim_grid = dim3(grid_cols, grid_rows);
+  	dim_block = dim3(BLOCK_SIZE, BLOCK_SIZE);
 
 	checkCudaErrors(
 			cudaFuncSetAttribute(compute_gemm,
@@ -542,10 +553,9 @@ int main(int argc, char **argv) {
 
 	compute_gemm<<<deviceProp.multiProcessorCount, THREADS_PER_BLOCK, SHMEM_SZ,
 			st>>>(A, B, C, dtd, alpha, beta, M_GLOBAL,
-	M_GLOBAL);
+	M_GLOBAL);			
 
-	MatrixMulCUDA<half> <<<grid, threads, SHMEM_SZ>>>(A, B, C, D, alpha, beta,
-	M_GLOBAL, M_GLOBAL);
+	matrix_mult<<<grid, threads, SHMEM_SZ>>>(A, B, M_GLOBAL, M_GLOBAL, D, alpha, beta);
 
 	checkKernelErrors(cudaStreamSynchronize(st));
 	checkKernelErrors(cudaPeekAtLastError());
