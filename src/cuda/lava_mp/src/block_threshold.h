4352b0f8b7bd9ee466bc730eafcb61fce3e60184
Your Name <you@example.com>
2019-12-03 23:20:55 -0300

    setup double

--- src/cuda/lava_mp/src/block_threshold.h
+++ src/cuda/lava_mp/src/block_threshold.h
@@ -11,7 +11,7 @@
 //Assuming that max boxes will be 128
 #define THRESHOLD_SIZE 1 << 21 //IT Can be up to boxes * boxes * boxes
 
-#define THRESHOLD_PATH "/home/carol/radiation-benchmarks/data/threshold.data"
+#define THRESHOLD_PATH "/home/carol/radiation-benchmarks/data/lava/threshold.data"
 
 __device__ uint32_t thresholds[THRESHOLD_SIZE] = { 0 };
 
