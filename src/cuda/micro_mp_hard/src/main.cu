44024828b0b882f265c77f63d23b9ab5a288973c
Your Name <you@example.com>
2019-12-08 18:08:47 -0300

    makefile

--- src/cuda/micro_mp_hard/src/main.cu
+++ src/cuda/micro_mp_hard/src/main.cu
@@ -177,6 +177,10 @@ int main(int argc, char* argv[]) {
 
 	std::cout << log << std::endl;
 	std::cout << parameters << std::endl;
+#if BUILDRELATIVEERROR != 0
+	std::cout << "Relative error allowed " << RELATIVE_ERROR_VARIATION << std::endl;
+#endif
+
 
 	switch (parameters.operation_num) {
 	case 1:
