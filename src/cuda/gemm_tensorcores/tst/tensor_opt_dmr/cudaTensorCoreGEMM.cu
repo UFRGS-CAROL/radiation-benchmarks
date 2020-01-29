374e9120477198b36a2eb232554d553979337939
pedrombasso <pedro.basso@inf.ufrgs.br>
2020-01-30 00:53:23 +0100

    dmr test

--- src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
+++ src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
@@ -573,9 +573,9 @@ int main(int argc, char **argv) {
 
 	 matrix_mult<<<dim_grid, dim_block,0,stream2>>>(A, B, MATRIX_M, MATRIX_N, D, alpha, beta);
 
-	checkKernelErrors(cudaStreamSynchronize(st));
-	//checkKernelErrors(cudaPeekAtLastError());
-	//checkKernelErrors(cudaDeviceSynchronize());
+	// checkKernelErrors(cudaStreamSynchronize(st));
+	checkKernelErrors(cudaPeekAtLastError());
+	checkKernelErrors(cudaDeviceSynchronize());
  	}
 
 
