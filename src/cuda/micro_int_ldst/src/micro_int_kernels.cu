8755cfce64b98f1595d3bb912a7ad1a13db7c556
Your Name <you@example.com>
2020-02-02 00:05:10 -0300

    micro int

--- src/cuda/micro_int_ldst/src/micro_int_kernels.cu
+++ src/cuda/micro_int_ldst/src/micro_int_kernels.cu
@@ -12,34 +12,66 @@
  * dst is the output of the kernel
  * defined_src is defined input that has max threadIdx size
  */
-__global__ void add_int_kernel(int32_t* defined_src, int32_t* dst) {
+__global__ void add_int_kernel(int32_t* src, int32_t* dst, uint32_t op) {
+
+	int32_t output = src[threadIdx.x];
+	int32_t input = src[threadIdx.x];
+#pragma unroll
+	for (uint32_t i = 0; i < op; i++) {
+		output += input + output;
+		output -= input;
+		output += input;
+		output -= input - output;
+	}
+
+	const uint32_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	dst[thread_id] = output;
+}
+
+__global__ void mul_int_kernel(int32_t* defined_src, int32_t* dst,
+		uint32_t op) {
 
 	int32_t output_register = defined_src[threadIdx.x];
 	int32_t input_register = defined_src[threadIdx.x];
 #pragma unroll
-	for (uint32_t i = 0; i < OPS; i++) {
-		dst += input_register;
-		dst -= input_register;
-		dst += input_register;
-		dst -= input_register;
+	for (uint32_t i = 0; i < op; i++) {
+		output_register *= (input_register * 2);
+		output_register /= input_register;
+		output_register *= input_register;
+		output_register /= (input_register * 2);
 	}
 
-	uint32_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+	const uint32_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
 	dst[thread_id] = output_register;
 }
 
-__global__ void mul_int_kernel(int32_t* defined_src, int32_t* dst) {
+__global__ void mad_int_kernel(int32_t* src, int32_t* dst, uint32_t op) {
+	int32_t output = src[threadIdx.x];
+	int32_t input = src[threadIdx.x];
+#pragma unroll
+	for (uint32_t i = 0; i < op; i++) {
+		output += output * input * 2;
+		output -= output * input;
+		output += output * input;
+		output -= output * input * 2;
+	}
+
+	const uint32_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	dst[thread_id] = output;
 }
 
-__global__ void mad_int_kernel(int32_t* defined_src, int32_t* dst) {
-}
-
-__global__ void ldst_int_kernel(int32_t* defined_src, int32_t* dst) {
+__global__ void ldst_int_kernel(int32_t* src, int32_t* dst, uint32_t op) {
+	const uint32_t thread_id = (blockIdx.x * blockDim.x + threadIdx.x) * op;
+	for (uint32_t i = 0; i < op; i++) {
+		dst[i + thread_id] = src[i + thread_id];
+	}
 }
 
 void MicroInt::execute_micro() {
-	void (*kernel)(int32_t*, int32_t*);
+	void (*kernel)(int32_t*, int32_t*, uint32_t);
 	switch (this->parameters.micro) {
 	case ADD_INT:
 		kernel = add_int_kernel;
@@ -55,6 +87,6 @@ void MicroInt::execute_micro() {
 		break;
 	}
 
-	kernel<<<this->parameters.grid_size, this->parameters.block_size>>>(this->input_device.data(),
-					this->output_device.data());
+	kernel<<<this->parameters.grid_size, this->parameters.block_size>>>(
+			this->input_device.data(), this->output_device.data(), OPS);
 }
