048eb8aff0b0027232c3297bc59a26ea5441b06c
pedrombasso <pedro.basso@inf.ufrgs.br>
2020-02-08 12:35:27 +0100

    test half

--- src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
+++ src/cuda/gemm_tensorcores/matrixMul/matrixMul.cu
@@ -135,9 +135,9 @@ __global__ void MatrixMulCUDA(real_t *C, real_t *A,
   // each thread writes one element
   int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
   C[c + wB * ty + tx] = Csub;
-
-  printf(" Csub == %f \n", C[1]);
 }
+
+
 template<typename real_t>
 void ConstantInit(real_t *data, int size, real_t val) {
   for (int i = 0; i < size; ++i) {
