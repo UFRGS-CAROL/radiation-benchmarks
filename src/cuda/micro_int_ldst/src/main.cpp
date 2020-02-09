c809f6ab116fc2b9e23864c423367198df59dcdf
reliabilitylogdata <reliabilitylogdata@github.com>
2020-02-08 23:33:44 -0300

    micro int

--- src/cuda/micro_int_ldst/src/main.cpp
+++ src/cuda/micro_int_ldst/src/main.cpp
@@ -36,7 +36,6 @@ void setup_execute(Log& log, Parameters& test_parameter,
 	for (size_t iteration = 0; iteration < test_parameter.iterations;
 			iteration++) {
 
-
 		auto start_it = rad::mysecond();
 		//Start iteration
 		log.start_iteration();
@@ -59,6 +58,10 @@ void setup_execute(Log& log, Parameters& test_parameter,
 		auto errors = micro_obj.compare_output();
 		auto end_cmp = rad::mysecond();
 
+		auto start_reset_output = rad::mysecond();
+		micro_obj.reset_output_device();
+		auto end_reset_output = rad::mysecond();
+
 		//update errors
 		log.update_errors();
 		log.update_infos();
@@ -68,19 +71,22 @@ void setup_execute(Log& log, Parameters& test_parameter,
 			auto kernel_time = end_it - start_it;
 			auto cmp_time = end_cmp - start_cmp;
 			auto copy_time = end_cpy - start_cpy;
+			auto reset_time = end_reset_output - start_reset_output;
+			auto wasted_time = cmp_time + copy_time + reset_time;
 
 			std::cout << "Iteration " << iteration;
 			std::cout << " Kernel time: " << kernel_time;
 			std::cout << " Output errors: " << errors;
-			std::cout << " Wasted time (copy + compare): " << cmp_time + copy_time;
+			std::cout << " Wasted time (copy + compare): " << wasted_time;
 			std::cout << " Comparison time: " << cmp_time;
-			std::cout << " Copy time: " << copy_time << std::endl;
+			std::cout << " Copy time: " << copy_time;
+			std::cout << " Reset time: " << reset_time << std::endl;
 		}
 	}
 }
 
 template<typename int_t>
-void setup(Parameters& parameters){
+void setup(Parameters& parameters) {
 	std::cout << std::setprecision(6) << std::setfill('0');
 	std::shared_ptr<Log> log_ptr;
 
