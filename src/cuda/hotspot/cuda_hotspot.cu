45fad89174c523fbe5a349eabbbdf5585b9b7a54
Your Name <you@example.com>
2020-02-28 20:25:40 -0300

    cuda hotspot

--- src/cuda/hotspot/cuda_hotspot.cu
+++ src/cuda/hotspot/cuda_hotspot.cu
@@ -30,19 +30,19 @@
 
 //=========== DEFINE TESTED TYPE
 #if defined(PRECISION_DOUBLE)
-const char test_precision_description[] = "double";
-typedef double tested_type;
-typedef double tested_type_host;
+	const char test_precision_description[] = "double";
+	typedef double tested_type;
+	typedef double tested_type_host;
 #elif defined(PRECISION_SINGLE)
-const char test_precision_description[] = "single";
-typedef float tested_type;
-typedef float tested_type_host;
+	const char test_precision_description[] = "single";
+	typedef float tested_type;
+	typedef float tested_type_host;
 #elif defined(PRECISION_HALF)
-const char test_precision_description[] = "half";
-typedef half tested_type;
-typedef half_float::half tested_type_host;
+	const char test_precision_description[] = "half";
+	typedef half tested_type;
+	typedef half_float::half tested_type_host;
 #else 
-#error TEST TYPE NOT DEFINED OR INCORRECT. USE PRECISION=<double|single|half>.
+	#error TEST TYPE NOT DEFINED OR INCORRECT. USE PRECISION=<double|single|half>.
 #endif
 
 #define BLOCK_SIZE 16
@@ -128,8 +128,7 @@ void readInput(parameters *params) {
 			if ((sscanf(str, "%f", &val) != 1))
 				fatal(params, "invalid temp file format");
 
-			params->in_temperature[i * (params->grid_cols) + j] =
-					tested_type_host(val);
+			params->in_temperature[i * (params->grid_cols) + j] = tested_type_host(val);
 
 			if (tested_type_host(val) == 0)
 				num_zeros++;
@@ -144,8 +143,7 @@ void readInput(parameters *params) {
 			if ((sscanf(str, "%f", &val) != 1))
 				fatal(params, "invalid power file format");
 
-			params->in_power[i * (params->grid_cols) + j] = tested_type_host(
-					val);
+			params->in_power[i * (params->grid_cols) + j] = tested_type_host(val);
 
 			if (tested_type_host(val) == 0)
 				num_zeros++;
@@ -154,10 +152,8 @@ void readInput(parameters *params) {
 
 			if (!(params->generate)) {
 				assert(
-						fread(
-								&(params->gold_temperature[i
-										* (params->grid_cols) + j]),
-								sizeof(tested_type), 1, fgold) == 1);
+					fread(&(params->gold_temperature[i * (params->grid_cols) + j]), sizeof(tested_type), 1, fgold) == 1
+				);
 			}
 		}
 	}
@@ -169,7 +165,7 @@ void readInput(parameters *params) {
 	if (params->fault_injection) {
 		params->in_temperature[32] = 6.231235;
 		printf("!!!!!!!!! Injected error: in_temperature[32] = %f\n",
-				(double) params->in_temperature[32]);
+				(double)params->in_temperature[32]);
 	}
 	// ==================================
 
@@ -203,8 +199,7 @@ void writeOutput(parameters *params) {
 				num_nans++;
 
 			//-----------------------------------------------------------------------------------
-			fwrite(&(params->out_temperature[i * (params->grid_cols) + j]),
-					sizeof(tested_type), 1, fgold);
+			fwrite(&(params->out_temperature[i * (params->grid_cols) + j]), sizeof(tested_type), 1, fgold);
 		}
 	}
 	fclose(fgold);
@@ -274,7 +269,7 @@ __global__ void calculate_temp(int iteration,  //number of iteration
 	int index = grid_cols * loadYidx + loadXidx;
 
 	if (IN_RANGE(loadYidx, 0, grid_rows - 1) &&
-	IN_RANGE(loadXidx, 0, grid_cols - 1)) {
+		IN_RANGE(loadXidx, 0, grid_cols - 1)) {
 
 		temp_on_cuda[ty][tx] = temp_src[index];
 		power_on_cuda[ty][tx] = power[index];
@@ -286,13 +281,9 @@ __global__ void calculate_temp(int iteration,  //number of iteration
 	// the valid range of the input data
 	// used to rule out computation outside the boundary.
 	int validYmin = (blkY < 0) ? -blkY : 0;
-	int validYmax = (blkYmax > grid_rows - 1) ?
-	BLOCK_SIZE - 1 - (blkYmax - grid_rows + 1) :
-												BLOCK_SIZE - 1;
+	int validYmax = (blkYmax > grid_rows - 1) ? BLOCK_SIZE - 1 - (blkYmax - grid_rows + 1) : BLOCK_SIZE - 1;
 	int validXmin = (blkX < 0) ? -blkX : 0;
-	int validXmax = (blkXmax > grid_cols - 1) ?
-	BLOCK_SIZE - 1 - (blkXmax - grid_cols + 1) :
-												BLOCK_SIZE - 1;
+	int validXmax = (blkXmax > grid_cols - 1) ? BLOCK_SIZE - 1 - (blkXmax - grid_cols + 1) : BLOCK_SIZE - 1;
 
 	int N = ty - 1;
 	int S = ty + 1;
@@ -312,18 +303,46 @@ __global__ void calculate_temp(int iteration,  //number of iteration
 		IN_RANGE(tx, validXmin, validXmax) &&
 		IN_RANGE(ty, validYmin, validYmax)) {
 			computed = true;
-			register tested_type calculated = temp_on_cuda[ty][tx]
-					+ step_div_Cap
-							* (power_on_cuda[ty][tx]
-									+ (temp_on_cuda[S][tx] + temp_on_cuda[N][tx]
-											- tested_type(2.0)
-													* temp_on_cuda[ty][tx])
-											* Ry_1
-									+ (temp_on_cuda[ty][E] + temp_on_cuda[ty][W]
-											- tested_type(2.0)
-													* temp_on_cuda[ty][tx])
-											* Rx_1
-									+ (amb_temp - temp_on_cuda[ty][tx]) * Rz_1);
+			register tested_type calculated =
+				temp_on_cuda[ty][tx]
+				+
+				step_div_Cap
+				*
+				(
+					power_on_cuda[ty][tx]
+					+
+					(
+						temp_on_cuda[S][tx]
+						+
+						temp_on_cuda[N][tx]
+						-
+						tested_type(2.0)
+						*
+						temp_on_cuda[ty][tx]
+					)
+					*
+					Ry_1
+					+
+					(
+						temp_on_cuda[ty][E]
+						+
+						temp_on_cuda[ty][W]
+						-
+						tested_type(2.0)
+						*
+						temp_on_cuda[ty][tx]
+					)
+					*
+					Rx_1
+					+
+					(
+						amb_temp
+						-
+						temp_on_cuda[ty][tx]
+					)
+					*
+					Rz_1
+				);
 			t_temp[ty][tx] = calculated;
 		}
 		__syncthreads();
@@ -349,10 +368,11 @@ __global__ void calculate_temp(int iteration,  //number of iteration
  */
 long long int flops = 0;
 
-int compute_tran_temp(tested_type_host *MatrixPower,
-		tested_type_host *MatrixTemp[2], int col, int row, int sim_time,
-		int num_iterations, int blockCols, int blockRows, int borderCols,
-		int borderRows, cudaStream_t stream) {
+int compute_tran_temp(
+		tested_type_host *MatrixPower,
+		tested_type_host *MatrixTemp[2],
+		int col, int row, int sim_time, int num_iterations, int blockCols,
+		int blockRows, int borderCols, int borderRows, cudaStream_t stream) {
 
 	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
 	dim3 dimGrid(blockCols, blockRows);
@@ -378,8 +398,10 @@ int compute_tran_temp(tested_type_host *MatrixPower,
 		dst = temp;
 		//printf("[%d]", omp_get_thread_num());
 		calculate_temp<<<dimGrid, dimBlock, 0, stream>>>(
-				MIN(num_iterations, sim_time - t), (tested_type*) MatrixPower,
-				(tested_type*) MatrixTemp[src], (tested_type*) MatrixTemp[dst],
+				MIN(num_iterations, sim_time - t),
+				(tested_type*)MatrixPower,
+				(tested_type*)MatrixTemp[src],
+				(tested_type*)MatrixTemp[dst],
 				col, row, borderCols, borderRows, Cap, Rx, Ry, Rz, step,
 				time_elapsed);
 		flops += col * row * MIN(num_iterations, sim_time - t) * 15;
@@ -405,7 +427,7 @@ void getParams(int argc, char** argv, parameters *params) {
 
 	if (argc < 2) {
 		usage(argc, argv);
-		exit(EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 
 	if (checkCmdLineFlag(argc, (const char **) argv, "size")) {
@@ -416,11 +438,11 @@ void getParams(int argc, char** argv, parameters *params) {
 		if ((params->grid_cols <= 0) || (params->grid_cols % 16 != 0)) {
 			printf("Invalid input size given on the command-line: %d\n",
 					params->grid_cols);
-			exit(EXIT_FAILURE);
+			exit (EXIT_FAILURE);
 		}
 	} else {
 		usage(argc, argv);
-		exit(EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 
 	if (checkCmdLineFlag(argc, (const char **) argv, "generate")) {
@@ -436,7 +458,7 @@ void getParams(int argc, char** argv, parameters *params) {
 		if (params->sim_time < 1) {
 			printf("Invalid sim_time given on the command-line: %d\n",
 					params->sim_time);
-			exit(EXIT_FAILURE);
+			exit (EXIT_FAILURE);
 		}
 	}
 
@@ -463,8 +485,7 @@ void getParams(int argc, char** argv, parameters *params) {
 				&(params->ofile));
 	} else {
 		params->ofile = new char[100];
-		snprintf(params->ofile, 100, "gold_temp_%s_%i_%i",
-				test_precision_description, params->grid_rows,
+		snprintf(params->ofile, 100, "gold_temp_%s_%i_%i", test_precision_description, params->grid_rows,
 				params->sim_time);
 		printf("Using default gold path: %s\n", params->ofile);
 	}
@@ -489,9 +510,9 @@ void getParams(int argc, char** argv, parameters *params) {
 	}
 }
 
+
 // Returns true if no errors are found. False if otherwise.
-int check_output_errors(parameters *setup_parameters, int streamIdx,
-		rad::Log& log) {
+int check_output_errors(parameters *setup_parameters, int streamIdx, rad::Log& log) {
 	int host_errors = 0;
 
 #pragma omp parallel for shared(host_errors)
@@ -502,18 +523,19 @@ int check_output_errors(parameters *setup_parameters, int streamIdx,
 			auto valGold = setup_parameters->gold_temperature[index];
 			auto valOutput = setup_parameters->out_temperature[index];
 
+
 			if (valGold != valOutput) {
 #pragma omp critical
 				{
 					char error_detail[150];
 					snprintf(error_detail, 150,
 							"stream: %d, p: [%d, %d], r: %1.20e, e: %1.20e",
-							streamIdx, i, j, (double) valOutput,
-							(double) valGold);
+							streamIdx, i, j,
+							(double)valOutput, (double)valGold);
 					if (setup_parameters->verbose && (host_errors < 10))
 						printf("%s\n", error_detail);
 //#ifdef LOGS
-					if (!setup_parameters->generate) {
+					if (!setup_parameters->generate){
 //						log_error_detail(error_detail);
 						log.log_error_detail(std::string(error_detail));
 
@@ -532,13 +554,12 @@ int check_output_errors(parameters *setup_parameters, int streamIdx,
 		log.update_errors();
 	}
 //#endif
-	if ((host_errors != 0) && (!setup_parameters->verbose))
-		printf("#");
-	if ((host_errors != 0) && (setup_parameters->verbose))
-		printf("Output errors: %d\n", host_errors);
+	if ((host_errors != 0) && (!setup_parameters->verbose)) printf("#");
+	if ((host_errors != 0) && (setup_parameters->verbose)) printf("Output errors: %d\n", host_errors);
 
 	return (host_errors == 0);
 }
+
 
 void run(int argc, char** argv) {
 	//int streamIdx;
@@ -565,70 +586,59 @@ void run(int argc, char** argv) {
 	// =======================
 	//HARDENING AGAINST BAD BOARDS
 	//-----------------------------------------------------------------------------------
-	setupParams->in_temperature = (tested_type_host *) malloc(
-			size * sizeof(tested_type));
-	setupParams->in_power = (tested_type_host *) malloc(
-			size * sizeof(tested_type));
-	setupParams->out_temperature = (tested_type_host *) calloc(size,
-			sizeof(tested_type));
-	setupParams->gold_temperature = (tested_type_host *) calloc(size,
-			sizeof(tested_type));
+	setupParams->in_temperature = (tested_type_host *) malloc(size * sizeof(tested_type));
+	setupParams->in_power = (tested_type_host *) malloc(size * sizeof(tested_type));
+	setupParams->out_temperature = (tested_type_host *) calloc(size, sizeof(tested_type));
+	setupParams->gold_temperature = (tested_type_host *) calloc(size, sizeof(tested_type));
 
 	if (!(setupParams->in_power) || !(setupParams->in_temperature)
-			|| !(setupParams->out_temperature)
-			|| !(setupParams->gold_temperature))
+			|| !(setupParams->out_temperature) || !(setupParams->gold_temperature))
 		fatal(setupParams, "unable to allocate memory");
 
 	//-----------------------------------------------------------------------------------
 
 	char test_info[150];
 	char test_name[90];
-	snprintf(test_info, 150,
-			"streams:%d precision:%s size:%d pyramidHeight:%d simTime:%d",
-			setupParams->nstreams, test_precision_description,
-			setupParams->grid_rows, setupParams->pyramid_height,
-			setupParams->sim_time);
+	snprintf(test_info, 150, "streams:%d precision:%s size:%d pyramidHeight:%d simTime:%d", setupParams -> nstreams, test_precision_description, setupParams -> grid_rows, setupParams -> pyramid_height, setupParams -> sim_time);
 	snprintf(test_name, 90, "cuda_hotspot_%s", test_precision_description);
 //#ifdef LOGS
 //	if (!(setupParams->generate)) start_log_file(test_name, test_info);
 //#endif
 	auto t_name = std::string(test_name);
 	auto t_info = std::string(test_info);
-//	std::shared_ptr<rad::Log> log;
-//	if(setupParams->generate){
-//		log = std::make_shared<rad::Log>(t_name, t_info);
-//	}
-	rad::Log log(t_name, t_info);
-	std::cout << log << std::endl;
-	printf(
-			"\n=================================\n%s\n%s\n=================================\n\n",
-			test_name, test_info);
+	std::shared_ptr<rad::Log> log;
+	if(!setupParams->generate){
+		log = std::make_shared<rad::Log>(t_name, t_info);
+	}
+//	rad::Log log(t_name, t_info);
+//	std::cout << log << std::endl;
+	printf("\n=================================\n%s\n%s\n=================================\n\n", test_name, test_info);
 
 	timestamp = rad::mysecond();
 	readInput(setupParams);
 	if (setupParams->verbose)
 		printf("readInput time: %.4fs\n", rad::mysecond() - timestamp);
-	fflush(stdout);
+	fflush (stdout);
 
-	cudaStream_t *streams = (cudaStream_t *) malloc(
-			(setupParams->nstreams) * sizeof(cudaStream_t));
+	cudaStream_t *streams = (cudaStream_t *) malloc((setupParams->nstreams) * sizeof(cudaStream_t));
 
 	tested_type_host *MatrixTemp[setupParams->nstreams][2];
 	tested_type_host *MatrixPower[setupParams->nstreams];
 
 	for (int streamIdx = 0; streamIdx < (setupParams->nstreams); streamIdx++) {
 		checkCudaErrors(
-				cudaStreamCreateWithFlags(&(streams[streamIdx]), cudaStreamNonBlocking));
+				cudaStreamCreateWithFlags(&(streams[streamIdx]),
+						cudaStreamNonBlocking));
 
 		checkCudaErrors(
-				cudaMalloc((void** ) &(MatrixTemp[streamIdx][0]),
+				cudaMalloc((void**) &(MatrixTemp[streamIdx][0]),
 						sizeof(tested_type) * size));
 		checkCudaErrors(
-				cudaMalloc((void** ) &(MatrixTemp[streamIdx][1]),
+				cudaMalloc((void**) &(MatrixTemp[streamIdx][1]),
 						sizeof(tested_type) * size));
 
 		checkCudaErrors(
-				cudaMalloc((void** ) &(MatrixPower[streamIdx]),
+				cudaMalloc((void**) &(MatrixPower[streamIdx]),
 						sizeof(tested_type) * size));
 
 	}
@@ -642,19 +652,15 @@ void run(int argc, char** argv) {
 		// ============ PREPARE ============
 		int ret[setupParams->nstreams];
 		timestamp = rad::mysecond();
-		for (int streamIdx = 0; streamIdx < (setupParams->nstreams);
-				streamIdx++) {
+		for (int streamIdx = 0; streamIdx < (setupParams->nstreams); streamIdx++) {
 
 			// Setup inputs (Power and Temperature)
-			cudaMemcpy(MatrixTemp[streamIdx][0], setupParams->in_temperature,
-					sizeof(tested_type) * size, cudaMemcpyHostToDevice);
+			cudaMemcpy(MatrixTemp[streamIdx][0], setupParams->in_temperature, sizeof(tested_type) * size, cudaMemcpyHostToDevice);
 
-			cudaMemcpy(MatrixPower[streamIdx], setupParams->in_power,
-					sizeof(tested_type) * size, cudaMemcpyHostToDevice);
+			cudaMemcpy(MatrixPower[streamIdx], setupParams->in_power, sizeof(tested_type) * size, cudaMemcpyHostToDevice);
 
 			// Setup output (Temperature)
-			cudaMemset(MatrixTemp[streamIdx][1], 0.0,
-					sizeof(tested_type) * size);
+			cudaMemset(MatrixTemp[streamIdx][1], 0.0, sizeof(tested_type) * size);
 
 		}
 		if (setupParams->verbose)
@@ -665,16 +671,17 @@ void run(int argc, char** argv) {
 //#ifdef LOGS
 //		if (!(setupParams->generate)) start_iteration();
 //#endif
-		if (!(setupParams->generate))
-			log.start_iteration();
+		log->start_iteration();
 #pragma omp parallel for
 		for (int streamIdx = 0; streamIdx < (setupParams->nstreams);
 				streamIdx++) {
-			ret[streamIdx] = compute_tran_temp(MatrixPower[streamIdx],
-					MatrixTemp[streamIdx], setupParams->grid_cols,
-					setupParams->grid_rows, setupParams->sim_time,
-					setupParams->pyramid_height, blockCols, blockRows,
-					borderCols, borderRows, streams[streamIdx]);
+			ret[streamIdx] = compute_tran_temp(
+					MatrixPower[streamIdx],
+					MatrixTemp[streamIdx],
+					setupParams->grid_cols, setupParams->grid_rows,
+					setupParams->sim_time, setupParams->pyramid_height,
+					blockCols, blockRows, borderCols, borderRows,
+					streams[streamIdx]);
 		}
 		for (int streamIdx = 0; streamIdx < (setupParams->nstreams);
 				streamIdx++) {
@@ -683,8 +690,7 @@ void run(int argc, char** argv) {
 //#ifdef LOGS
 //		if (!(setupParams->generate)) end_iteration();
 //#endif
-		if (!(setupParams->generate))
-			log.end_iteration();
+		log->end_iteration();
 		kernel_time = rad::mysecond() - kernel_time;
 
 		// ============ MEASURE PERFORMANCE ============
@@ -694,8 +700,7 @@ void run(int argc, char** argv) {
 					* setupParams->grid_rows * setupParams->nstreams)
 					/ kernel_time);
 			printf("Kernel time: %.4lfs\n", kernel_time);
-			printf(
-					"Performance - SIZE:%d OUTPUT/S:%f FLOPS: %f (GFLOPS: %.2f)\n",
+			printf("Performance - SIZE:%d OUTPUT/S:%f FLOPS: %f (GFLOPS: %.2f)\n",
 					setupParams->grid_rows, outputpersec,
 					(double) flops / kernel_time,
 					(double) flops / (kernel_time * 1000000000));
@@ -717,7 +722,7 @@ void run(int argc, char** argv) {
 						MatrixTemp[streamIdx][ret[streamIdx]],
 						sizeof(tested_type) * size, cudaMemcpyDeviceToHost);
 
-				check_output_errors(setupParams, streamIdx, log);
+				check_output_errors(setupParams, streamIdx, *log);
 			}
 		}
 
@@ -728,8 +733,7 @@ void run(int argc, char** argv) {
 
 		double iteration_time = rad::mysecond() - globaltime;
 		if (setupParams->verbose)
-			printf("Iteration time: %.4fs (%3.1f%% Device)\n", iteration_time,
-					(kernel_time / iteration_time) * 100.0);
+			printf("Iteration time: %.4fs (%3.1f%% Device)\n", iteration_time, (kernel_time / iteration_time) * 100.0);
 		if (setupParams->verbose)
 			printf("===================================\n");
 
@@ -748,6 +752,8 @@ void run(int argc, char** argv) {
 //	if (!(setupParams->generate)) end_log_file();
 //#endif
 }
+
+
 
 int main(int argc, char** argv) {
 	printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
