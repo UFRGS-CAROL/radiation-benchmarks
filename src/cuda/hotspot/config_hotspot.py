e9d661e2b45dfa7e71afee853c7f22d01ba7a5c3
Your Name <you@example.com>
2020-02-28 21:21:11 -0300

    config hotspot

--- src/cuda/hotspot/config_hotspot.py
+++ src/cuda/hotspot/config_hotspot.py
@@ -5,21 +5,23 @@ import ConfigParser
 import copy
 import os
 import sys
+
 sys.path.insert(0, '../../include')
 from common_config import discover_board, execute_and_write_json_to_file
 
+SIZES = [1024]
+ITERATIONS = 10000
+SIMTIME = [1000]
+STREAMS = 20
+PRECISIONS = ["single"]
+BUILDPROFILER = 1
 
-SIZES=[1024]
-ITERATIONS=10000
-SIMTIME=[1000]
-STREAMS=10
-PRECISIONS = ["double", "single", "half"]
 
 def config(board, arith_type, debug):
     original_hotspot = "hotspot"
-    benchmark_bin = "cuda_hotspot_"+arith_type
+    benchmark_bin = "cuda_hotspot_" + arith_type
     benchmark_src = "hotspot"
-    print "Generating "+ benchmark_bin + " for CUDA, board:" + board
+    print("Generating " + benchmark_bin + " for CUDA, board:" + str(board))
 
     confFile = '/etc/radiation-benchmarks.conf'
     try:
@@ -28,25 +30,22 @@ def config(board, arith_type, debug):
         installDir = config.get('DEFAULT', 'installdir') + "/"
 
     except IOError as e:
-        print >> sys.stderr, "Configuration setup error: " + str(e)
-        sys.exit(1)
-
+        raise ValueError("Configuration setup error: " + str(e))
 
     data_path = installDir + "data/" + original_hotspot
     bin_path = installDir + "bin"
     src_hotspot = installDir + "src/cuda/" + benchmark_src
 
     if not os.path.isdir(data_path):
-        os.mkdir(data_path, 0777)
-        os.chmod(data_path, 0777)
-
+        os.mkdir(data_path, 0o777)
+        os.chmod(data_path, 0o777)
 
     # change it for lava
-    generate = ["cd " + src_hotspot, 
-                "make clean", 
-                "make -C ../../include ", 
-               # "make -C ../../include/safe_memory/", 
-                "make PRECISION=" + arith_type, 
+    generate = ["cd " + src_hotspot,
+                "make clean",
+                "make -C ../../include ",
+                "make -C ../common/",
+                "make PRECISION={} BUILDPROFILER={}".format(arith_type, BUILDPROFILER),
                 "mkdir -p " + data_path,
                 "mv ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
@@ -59,9 +58,9 @@ def config(board, arith_type, debug):
             gen[0] = ['sudo ', bin_path + "/" + benchmark_bin + " "]
             gen[1] = ['-size=' + str(i)]
             gen[2] = ['-generate ']
-            gen[3] = ['-input_temp=' + inputFile + "temp_" +  str(i)]
+            gen[3] = ['-input_temp=' + inputFile + "temp_" + str(i)]
             gen[4] = ['-input_power=' + inputFile + "power_" + str(i)]  # change for execute
-            gen[5] = ['-gold_temp=' + inputFile + "gold_" + str(i) + "_" + arith_type + "_"  + str(s)]
+            gen[5] = ['-gold_temp=' + inputFile + "gold_" + str(i) + "_" + arith_type + "_" + str(s)]
             gen[6] = ['-sim_time=' + str(s)]
             gen[7] = ['-iterations=1']
             gen[8] = ['-streams=' + str(STREAMS)]
@@ -74,25 +73,23 @@ def config(board, arith_type, debug):
             generate.append(' '.join(str(r) for v in gen for r in v))
             execute.append(' '.join(str(r) for v in exe for r in v))
 
-
-    #execute, generate, install_dir, benchmark_bin, debug
+    # execute, generate, install_dir, benchmark_bin, debug
     execute_and_write_json_to_file(execute=execute, generate=generate,
-                                    install_dir=installDir, 
-                                    benchmark_bin=benchmark_bin, 
-                                    debug=debug)
-
-
+                                   install_dir=installDir,
+                                   benchmark_bin=benchmark_bin,
+                                   debug=debug)
 
 
 if __name__ == "__main__":
+    debug_mode = False
     try:
-        parameter = str(sys.argv[1:][0]).upper() 
+        parameter = str(sys.argv[1:][0]).upper()
         if parameter == 'DEBUG':
             debug_mode = True
-    except:
+    except IndexError:
         debug_mode = False
-    
+
     board, _ = discover_board()
     for p in PRECISIONS:
         config(board=board, arith_type=p, debug=debug_mode)
-    print "Multiple jsons may have been generated."
+    print("Multiple jsons may have been generated.")
