18e24085caf65de7d9c3f7c57cc7e68350ceadc7
Your Name <you@example.com>
2020-03-14 18:18:44 +0000

    starting triplication

---
+++ src/cuda/micro/src/MicroInt.h
@@ -0,0 +1,13 @@
+/*
+ * MicroInt.h
+ *
+ *  Created on: Mar 14, 2020
+ *      Author: fernando
+ */
+
+#ifndef MICROINT_H_
+#define MICROINT_H_
+
+
+
+#endif /* MICROINT_H_ */
---
+++ src/cuda/micro/src/MicroReal.h
@@ -0,0 +1,13 @@
+/*
+ * MicroReal.h
+ *
+ *  Created on: Mar 14, 2020
+ *      Author: fernando
+ */
+
+#ifndef MICROREAL_H_
+#define MICROREAL_H_
+
+
+
+#endif /* MICROREAL_H_ */
