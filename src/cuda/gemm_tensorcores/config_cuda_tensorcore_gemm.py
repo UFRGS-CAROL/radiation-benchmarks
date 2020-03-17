7fa594d71b0c3a970c6b19f476248882d694b524
Your Name <you@example.com>
2020-03-17 12:23:30 +0000

    cublas tensor colres

--- src/cuda/gemm_tensorcores/config_cuda_tensorcore_gemm.py
+++ src/cuda/gemm_tensorcores/config_cuda_tensorcore_gemm.py
@@ -4,23 +4,20 @@ import ConfigParser
 import copy
 import os
 import sys
-import shutil
 
 sys.path.insert(0, '../../include')
 from common_config import discover_board, execute_and_write_json_to_file
 
-SIZES =  [8192, 16384] #4096
-PRECISIONS = ["float", "half"]
+SIZES = [8192, 1024]  # 4096
+PRECISIONS = ["float"]  # , "half"]
 ITERATIONS = 10000
-USE_TENSOR_CORES = [1] #, 0]
+USE_TENSOR_CORES = [0]
+USE_CUBLAS = [0, 1]
 
 
 def config(board, arith_type, debug):
-
-    DATA_PATH_BASE = "gemm_" + arith_type
-
-    benchmark_bin = "gemm_wmma"
-    print "Generating " + benchmark_bin + " for CUDA, board:" + board
+    benchmark_bin = "gemm"
+    print("Generating " + benchmark_bin + " for CUDA, board:" + str(board))
 
     conf_file = '/etc/radiation-benchmarks.conf'
     try:
@@ -29,67 +26,72 @@ def config(board, arith_type, debug):
         install_dir = config.get('DEFAULT', 'installdir') + "/"
 
     except IOError as e:
-        print >> sys.stderr, "Configuration setup error: " + str(e)
-        sys.exit(1)
+        raise IOError("Configuration setup error: " + str(e))
 
-    data_path = install_dir + "data/" + DATA_PATH_BASE
+    data_path = install_dir + "data/gemm"
     bin_path = install_dir + "bin"
     src_benchmark = install_dir + "src/cuda/gemm_tensorcores"
 
     if not os.path.isdir(data_path):
-        os.mkdir(data_path, 0777)
-        os.chmod(data_path, 0777)
+        os.mkdir(data_path, 0o777)
+        os.chmod(data_path, 0o777)
 
-    generate = ["sudo mkdir -p " + bin_path, 
-                "cd " + src_benchmark, 
-                "make clean", 
-                "make -C ../../include ", 
+    generate = ["sudo mkdir -p " + bin_path,
+                "cd " + src_benchmark,
+                "make clean",
+                "make -C ../../include ",
                 "make PRECISION=" + arith_type + " -j 4 LOGS=1",
-                "mkdir -p " + data_path, 
+                "mkdir -p " + data_path,
                 "sudo rm -f " + data_path + "/*" + benchmark_bin + "*",
                 "sudo mv -f ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
 
     # gen only for max size, defined on cuda_trip_mxm.cu
-    #max_size = max(SIZES)
+    # max_size = max(SIZES)
     for i in SIZES:
         for tc in USE_TENSOR_CORES:
-            input_file = data_path + "/"
+            for cublas in USE_CUBLAS:
+                input_file = data_path + "/"
 
-            gen = [None] * 12
-            gen[0] = ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ', bin_path + "/" + benchmark_bin + " "]
-            gen[1] = ['--size ' + str(i)]
-            gen[2] = ['--input_a ' + input_file + 'A_' + str(i) + "_use_tensor_" + str(tc) + '.matrix']
-            gen[3] = ['--input_b ' + input_file + 'B_' + str(i) + "_use_tensor_" + str(tc) + '.matrix']
-            gen[4] = ['--input_c ' + input_file + 'C_' + str(i) + "_use_tensor_" + str(tc) + '.matrix']
-            gen[5] = ['--gold ' + input_file + "GOLD_" +  str(i) + "_use_tensor_" + str(tc) + ".matrix"]  # change for execute
-            gen[6] = ['--verbose 1']
-            gen[7] = ['--tensor_cores ' + str(tc)]
-            gen[8] = ['--generate 1']
-            gen[9] = ['--triplicated 1']
-	    gen[10]= ['--precision ' + str(arith_type)]
-            gen[11]=[]
-	    # change mode and iterations for exe
-            exe = copy.deepcopy(gen)
-            exe[0][1] = bin_path + '/' + benchmark_bin + " "
-            exe[11] = ['--iterations ' + str(ITERATIONS)]
- 	    exe[8] = ['--generate 0']
+                gen = [
+                    ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ',
+                     bin_path + "/" + benchmark_bin + " "],
+                    ['--size ' + str(i)],
+                    ['--input_a ' + input_file + 'A_' + str(i) + "_use_tensor_" + str(tc) + '.matrix'],
+                    ['--input_b ' + input_file + 'B_' + str(i) + "_use_tensor_" + str(tc) + '.matrix'],
+                    ['--input_c ' + input_file + 'C_' + str(i) + "_use_tensor_" + str(tc) + '.matrix'],
+                    ['--gold ' + input_file + "GOLD_" + str(i) + "_use_tensor_" + str(tc) + ".matrix"],
+                    ['--verbose 0'],
+                    ['--tensor_cores ' + str(tc)],
+                    ['--triplicated 1'],
+                    ['--precision ' + str(arith_type)],
+                    ['--use_cublas' if cublas == 1 else ''],
+                    ['--iterations ' + str(ITERATIONS)],
+                    ['--generate 1']
 
-            generate.append(' '.join(str(r) for v in gen for r in v))
-            execute.append(' '.join(str(r) for v in exe for r in v))
+                ]
 
-    execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)  
-    os.rename(install_dir+"/scripts/json_files/gemm_wmma.json",install_dir+"/scripts/json_files/gemm_wmma_"+arith_type+".json") 
+                # change mode and iterations for exe
+                exe = copy.deepcopy(gen)
+                exe[-1] = ['--generate 0']
+
+                generate.append(' '.join(str(r) for v in gen for r in v))
+                execute.append(' '.join(str(r) for v in exe for r in v))
+
+    execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)
+    os.rename(install_dir + "/scripts/json_files/gemm_wmma.json",
+              install_dir + "/scripts/json_files/gemm_wmma_" + arith_type + ".json")
+
 
 if __name__ == "__main__":
+    debug_mode = False
     try:
-        parameter = str(sys.argv[1:][1]).upper() 
+        parameter = str(sys.argv[1:][1]).upper()
         if parameter == 'DEBUG':
             debug_mode = True
-    except:
+    except IndexError:
         debug_mode = False
-    
+
     board, _ = discover_board()
     for p in PRECISIONS:
         config(board=board, arith_type=p, debug=debug_mode)
-    print "Multiple jsons may have been generated."
--- src/cuda/gemm_tensorcores/src/setup_gemm.cu
+++ src/cuda/gemm_tensorcores/src/setup_gemm.cu
@@ -3,6 +3,8 @@
 #include "include/device_vector.h"
 #include "common_template_functions.h"
 #include "no_tensor_kernels.h"
+//#include <cutlass/gemm/device/gemm.h>
+
 
 template<const uint32_t COUNT, typename half_t, typename real_t>
 struct GemmCaller {
@@ -51,7 +53,6 @@ struct UnhardenedGemmCaller: public GemmCaller<0, real_t, real_t> {
 	} //default constructor
 };
 
-#include "cutlass/gemm/device/gemm.h"
 
 template<typename real_t>
 struct CUBLASGemmCaller: public GemmCaller<0, real_t, real_t> {
@@ -129,71 +130,14 @@ struct CUTLASSGemmCaller: public GemmCaller<0, real_t, real_t> {
 
 	CUTLASSGemmCaller(uint32_t m, uint32_t n, bool use_tensor_cores = false) :
 			GemmCaller<0, real_t, real_t>(m, n) {
-		rad::checkCublasErrors(cublasCreate(&this->blas_handle));
+		throw_line("Not implemented yet");
 
-		if (use_tensor_cores) {
-			throw_line("Tensor Cores CUTLASS not implemented yet\n");
-		}
 	} //default constructor
 
 	// Define a CUTLASS GEMM template and launch a GEMM kernel.
 	void cutlass_sgemm_nn(int m, int n, int k, float alpha, float const *A,
 			int lda, float const *B, int ldb, float beta, float *C, int ldc) {
-
-		// Define type definition for single-precision CUTLASS GEMM with column-major
-		// input matrices and 128x128x8 threadblock tile size (chosen by default).
-		//
-		// To keep the interface manageable, several helpers are defined for plausible compositions
-		// including the following example for single-precision GEMM. Typical values are used as
-		// default template arguments. See `cutlass/gemm/device/default_gemm_configuration.h` for more details.
-		//
-		// To view the full gemm device API interface, see `cutlass/gemm/device/gemm.h`
-
-		using ColumnMajor = cutlass::layout::ColumnMajor;
-
-		using CutlassGemm = cutlass::gemm::device::Gemm<float, // Data-type of A matrix
-		ColumnMajor,// Layout of A matrix
-		float,// Data-type of B matrix
-		ColumnMajor,// Layout of B matrix
-		float,// Data-type of C matrix
-		ColumnMajor>;
-		// Layout of C matrix
-
-		// Define a CUTLASS GEMM type
-		CutlassGemm gemm_operator;
-
-		// Construct the CUTLASS GEMM arguments object.
-		//
-		// One of CUTLASS's design patterns is to define gemm argument objects that are constructible
-		// in host code and passed to kernels by value. These may include pointers, strides, scalars,
-		// and other arguments needed by Gemm and its components.
-		//
-		// The benefits of this pattern are (1.) a structured, composable strategy for passing host-constructible
-		// arguments to kernels and (2.) minimized initialization overhead on kernel entry.
-		//
-		CutlassGemm::Arguments args( { m, n, k },  // Gemm Problem dimensions
-				{ A, lda },    // Tensor-ref for source matrix A
-				{ B, ldb },    // Tensor-ref for source matrix B
-				{ C, ldc },    // Tensor-ref for source matrix C
-				{ C, ldc }, // Tensor-ref for destination matrix D (may be different memory than source C matrix)
-				{ alpha, beta }); // Scalars used in the Epilogue
-
-		//
-		// Launch the CUTLASS GEMM kernel.
-		//
-
-		rad::checkFrameorkErrors(gemm_operator(args));
-
-		//
-		// Return a cudaError_t if the CUTLASS GEMM operator returned an error code.
-		//
-
-		//		if (status != cutlass::Status::kSuccess) {
-		//			return cudaErrorUnknown;
-		//		}
-
-		// Return success, if no errors were encountered.
-		//		return cudaSuccess;
+		throw_line("Not implemented yet");
 	}
 
 };
