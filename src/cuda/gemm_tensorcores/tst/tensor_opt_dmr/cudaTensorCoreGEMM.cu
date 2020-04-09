3c5d090cc69b542c4890abe43f8f4dacd7041fac
pedrombasso <pedro.basso@inf.ufrgs.br>
2020-04-09 22:11:50 +0200

    tensor

--- src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
+++ src/cuda/gemm_tensorcores/tst/tensor_opt_dmr/cudaTensorCoreGEMM.cu
@@ -430,7 +430,7 @@ void generate_input_matrices(std::vector<half>& a_vector,
 
     std::random_device rd; //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<float> dis(0.0, 1.0);
+    std::uniform_real_distribution<float> dis(0.000001, 1.0);
 
     a_vector.resize(M_GLOBAL * M_GLOBAL);
     b_vector.resize(M_GLOBAL * M_GLOBAL);
