fe21356f41fd4d6b1795d5580f1002ef99a9a726
Your Name <you@example.com>
2020-08-22 19:36:44 -0300

    parse gemm for flexgrip

---
+++ src/cuda/darknet_v3/src/parse_gemm_layer.h
@@ -0,0 +1,24 @@
+/*
+ * parse_gemm_layer.h
+ *
+ *  Created on: Apr 11, 2020
+ *      Author: fernando
+ */
+
+#ifndef PARSE_GEMM_LAYER_H_
+#define PARSE_GEMM_LAYER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void parse_entry(int TA, int TB, int M, int N, int K, float ALPHA, float *A,
+		int lda, float *B, int ldb, float BETA, float *C, int ldc);
+
+void inject_fault(int TA, int TB, int M, int N, int K, float *C);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PARSE_GEMM_LAYER_H_ */
