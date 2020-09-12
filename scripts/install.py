ba4729716459bb3a9f1f29e6cbdbc8084d4b4168
Your Name <you@example.com>
2020-09-12 17:52:28 -0300

    Update for python 3

--- scripts/install.py
+++ scripts/install.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import ConfigParser
+import configparser
 import sys
 import os
 import re
 
-def checkPath(path):
-    print "The install directory is '"+path+"', is that correct [Y/n]: "
-    yes = set(['yes','y', 'ye', ''])
-    no = set(['no','n'])
 
+def check_path(path):
+    print("The install directory is '" + path + "', is that correct [Y/n]: ")
+    yes = {'yes', 'y', 'ye', ''}
+    no = {'no', 'n'}
 
-    choice = raw_input().lower()
+    choice = input().lower()
     if choice in yes:
         return True
     elif choice in no:
@@ -19,59 +19,47 @@ def checkPath(path):
     else:
         sys.stdout.write("Please respond with 'yes' or 'no'")
         return False
-    
 
-def installPath():
-    path=os.getcwd()
-    path=re.sub(r'/scripts', '', path)
-    while not checkPath(path):
-        print "Please, enter the install path (radiation-benchmarks directory): "
-        path = raw_input()
+
+def install_path():
+    path = os.getcwd()
+    path = re.sub(r'/scripts', '', path)
+    while not check_path(path):
+        print("Please, enter the install path (radiation-benchmarks directory): ")
+        path = input()
     return path
 
-def check():
-    datafile = file("/proc/cpuinfo");
-    for line in datafile:
-        if 'jetson-tk1' in line:
-            return True;
-    return False;
 
-varDir = "/var/radiation-benchmarks"
-confFile = "/etc/radiation-benchmarks.conf"
+var_dir = "/var/radiation-benchmarks"
+conf_file = "/etc/radiation-benchmarks.conf"
 
-#check if it is K1
-if check():
-    #if it is save everything in sd card
-    varDir = raw_input("Where is mounted the sd card??? ") + "/radiation-benchmarks";
+log_dir = var_dir + "/log"
+mic_log = "/micNfs/carol/logs"
 
-logDir = varDir+"/log"
-micLog = "/micNfs/carol/logs"
+signal_cmd = "killall -q -USR1 killtestSignal.py;"
+signal_cmd += " killall -q -USR1 test_killtest_commands_json.py; killall -q -USR1 python"
 
-signalCmd = "killall -q -USR1 killtestSignal.py; killall -q -USR1 test_killtest_commands_json.py; killall -q -USR1 python"
+config = configparser.ConfigParser()
 
-installPath=installPath()
-
-config = ConfigParser.ConfigParser()
-
-config.set("DEFAULT", "installdir", installPath)
-config.set("DEFAULT", "vardir", varDir)
-config.set("DEFAULT", "logdir", logDir)
+config.set("DEFAULT", "installdir", install_path())
+config.set("DEFAULT", "vardir", var_dir)
+config.set("DEFAULT", "logdir", log_dir)
 config.set("DEFAULT", "tmpdir", "/tmp")
-config.set("DEFAULT", "miclogdir", micLog)
-config.set("DEFAULT", "signalcmd", signalCmd)
+config.set("DEFAULT", "miclogdir", mic_log)
+config.set("DEFAULT", "signalcmd", signal_cmd)
 try:
-    if not os.path.isdir(varDir):
-        os.mkdir(varDir, 0777)
-    os.chmod(varDir, 0777)
-    if not os.path.isdir(logDir):
-        os.mkdir(logDir, 0777)
-    os.chmod(logDir, 0777)
-    with open(confFile, 'w') as configfile:
+    if not os.path.isdir(var_dir):
+        os.mkdir(var_dir, 0o777)
+    os.chmod(var_dir, 0o777)
+    if not os.path.isdir(log_dir):
+        os.mkdir(log_dir, 0o777)
+    os.chmod(log_dir, 0o777)
+    with open(conf_file, 'w') as configfile:
         config.write(configfile)
 except IOError:
-    print "I/O Error, please make sure to run as root (sudo)"
+    print("I/O Error, please make sure to run as root (sudo)")
     sys.exit(1)
 
-print "var directory created ("+varDir+")"
-print "log directory created ("+logDir+")"
-print "Config file written ("+confFile+")"
+print("var directory created (" + var_dir + ")")
+print("log directory created (" + log_dir + ")")
+print("Config file written (" + conf_file + ")")
