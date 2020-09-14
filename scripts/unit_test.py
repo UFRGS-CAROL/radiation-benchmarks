90a796b25a24a69cddc725910198301fdc5e3f6f
Your Name <you@example.com>
2020-09-14 10:44:02 -0300

    update on unit test

--- scripts/unit_test.py
+++
@@ -1,4 +0,0 @@
-#from server_package.RebootMachine import RebootMachine
-from server_package.Machine import Machine
-
-print("foi")
---
+++ scripts/unit_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Testing unit Machine class"
+python3 server_package/Machine.py
+
+echo "Testing unit RebootMachine"
+python3 server_package/RebootMachine.py
