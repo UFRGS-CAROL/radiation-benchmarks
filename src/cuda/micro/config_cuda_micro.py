4f5cd00a2b925b692032e8bc11dedc9fc9e15f23
Your Name <you@example.com>
2020-09-15 23:26:53 -0300

    update config micro

--- src/cuda/micro/config_cuda_micro.py
+++ src/cuda/micro/config_cuda_micro.py
@@ -6,7 +6,17 @@ import sys
 sys.path.insert(0, '../../include')
 
 from common_config import discover_board, execute_and_write_json_to_file
-import common_micro_config as cm
+
+ITERATIONS = int(1e9)
+INT_MICRO = {
+    "int32": {
+        "branch": {"ops": 10000000, "block_size": 256, "sm_factor": 1}
+    }
+
+    # "float": {
+    #     "fma": {"ops": 100000000, "block_size": 256, "sm_factor": 1}
+    # }
+}
 
 
 def config(board, debug):
@@ -32,57 +42,31 @@ def config(board, debug):
                 "make clean",
                 "make -C ../../include ",
                 "make -C ../common",
-                "make BUILDPROFILER={} PRECISION=".format(cm.BUILD_PROFILER) + " -j 4",
+                "make -j 4",
                 "sudo mv -f ./" + benchmark_bin + " " + bin_path + "/"]
     execute = []
 
-    for inst_type in cm.FLOAT_MICRO:
-        print(inst_type)
-        configs = cm.FLOAT_MICRO[inst_type]
-        fast_math_list = configs["fast_math"]
-        ops_list = configs["ops_list"]
-        precisions = configs["precisions"]
-        for precision in precisions:
-            for ops in ops_list:
-                for fast_math in fast_math_list:
-                    gold_path = data_path + "/gold_{}_{}_{}_{}.data".format(ops, fast_math, inst_type, precision)
-                    gen = [
-                        ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ',
-                         bin_path + '/' + benchmark_bin + " "],
-                        ['--iterations {}'.format(cm.ITERATIONS)],
-                        ['--gold {}'.format(gold_path)],
-                        ['--precision {}'.format(precision)],
-                        ['--inst {}'.format(inst_type)],
-                        ['--opnum {}'.format(ops)],
-                    ]
-                    if fast_math == 1:
-                        gen.append(['--fast-math'])
-                    gen.append(['--generate'])
-
-                    generate.append(' '.join(str(r) for v in gen for r in v))
-                    del gen[-1]
-                    execute.append(' '.join(str(r) for v in gen for r in v))
-
-    for inst_type in cm.INT_MICRO:
-        print(inst_type)
-        configs = cm.INT_MICRO[inst_type]
-        ops_list = configs["ops_list"]
-        for ops in ops_list:
-            gold_path = data_path + "/gold_{}_{}_int32.data".format(inst_type, ops)
+    for precision, configs in INT_MICRO.items():
+        for instruction, parameters in configs.items():
+            block_size = parameters["block_size"]
+            sm_factor = parameters["sm_factor"]
+            ops = parameters["ops"]
+            gold_path = data_path + f"/gold_{precision}_{instruction}_{block_size}_{sm_factor}_{ops}.data"
             gen = [
                 ['sudo env LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} ',
                  bin_path + '/' + benchmark_bin + " "],
-                ['--iterations {}'.format(cm.ITERATIONS)],
-                ['--gold {}'.format(gold_path)],
-                ['--precision int32'],
-                ['--inst {}'.format(inst_type)],
-                ['--opnum {}'.format(ops)],
-                ['--generate']
+                [f'--iterations {ITERATIONS}'],
+                [f'--gold {gold_path}'],
+                [f'--precision {precision}'],
+                [f'--inst {instruction}'],
+                [f'--opnum {ops}'],
+                [f'--blocksize {block_size}'],
+                [f'--smmul {sm_factor}'],
             ]
 
-            generate.append(' '.join(str(r) for v in gen for r in v))
-            del gen[-1]
             execute.append(' '.join(str(r) for v in gen for r in v))
+            gen.append(['--generate'])
+            generate.append(' '.join(str(r) for v in gen for r in v))
 
     execute_and_write_json_to_file(execute, generate, install_dir, benchmark_bin, debug=debug)
 
@@ -90,7 +74,7 @@ def config(board, debug):
 if __name__ == "__main__":
     debug_mode = False
     try:
-        parameter = str(sys.argv[1:][0]).upper()
+        parameter = str(sys.argv[-1]).upper()
         if parameter == 'DEBUG':
             debug_mode = True
     except IndexError:
