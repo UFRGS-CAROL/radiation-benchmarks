12b87c1b4469009b8ebf3fd46f2157dac075afb8
Your Name <you@example.com>
2020-10-08 00:32:53 -0300

    update on parse layers

--- src/cuda/darknet_v3/src/parse_gemm_layer.cpp
+++ src/cuda/darknet_v3/src/parse_gemm_layer.cpp
@@ -22,6 +22,10 @@ std::string layers_files_base_path;
 LayerOperationType operation_type = GENERATE_GOLDEN_LAYERS;
 #define MAX_FLOAT_THRESHOLD 1.0e-5f
 
+extern "C" void cuda_pull_array(float *x_gpu, float *x, size_t n);
+extern "C" void cuda_push_array(float *x_gpu, float *x, size_t n);
+
+
 void set_layer_processing_parameters(
         const std::string &base_path,
         LayerOperationType current_operation) {
@@ -32,29 +36,6 @@ void set_layer_processing_parameters(
 }
 
 
-extern "C" void cuda_pull_array(float *x_gpu, float *x, size_t n);
-
-template<typename type_t>
-bool write_file(type_t *src, std::string &path, std::string &header, int m,
-                int n) {
-    std::ofstream of(path, std::ios::trunc);
-    if (of.good()) {
-        of << header << std::endl;
-        of << std::scientific << std::setprecision(16);
-        for (auto i = 0; i < m; i++) {
-            for (auto j = 0; j < n; j++) {
-                of << src[i * n + j];
-                if (j < (n - 1))
-                    of << ",";
-            }
-            of << std::endl;
-        }
-
-        of.close();
-        return true;
-    }
-    return false;
-}
 
 template<typename type_t>
 void write_file(const std::vector<type_t> &src, const std::string &path) {
@@ -79,40 +60,6 @@ void read_file(std::vector<type_t> &src, const std::string &path, size_t n) {
         throw std::runtime_error(
                 "Could not open file: " + path + " At line " + std::to_string(__LINE__) + " file " + __FILE__);
     }
-}
-
-void
-parse_entry_cpu(std::string &path_a, std::string &path_b, int TA, int TB, int M, int N, int K, float ALPHA, float *A,
-                int lda, float *B, int ldb, float BETA, float *C, int ldc) {
-
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (C[i * ldc + j] != 0.0f)
-                std::cout << "Vall C " << C[i * ldc + j] << std::endl;
-        }
-    }
-
-    auto header_a = "TRANSPOSE," + std::to_string(TA) + ",M,"
-                    + std::to_string(M) + ",N," + std::to_string(N) + ",K,"
-                    + std::to_string(K);
-
-    //MATRIX A is M and K
-    if (TA) {
-        write_file(A, path_a, header_a, K, M);
-    } else {
-        write_file(A, path_a, header_a, M, K);
-    }
-
-    auto header_b = "TRANSPOSE," + std::to_string(TB) + ",M,"
-                    + std::to_string(M) + ",N," + std::to_string(N) + ",K,"
-                    + std::to_string(K);
-    //MATRIX B is K and N
-    if (TB) {
-        write_file(B, path_b, header_b, N, K);
-    } else {
-        write_file(B, path_b, header_b, K, N);
-    }
-
 }
 
 std::vector<std::string> split(const std::string &str, char del) {
@@ -159,7 +106,7 @@ std::vector<std::tuple<int, int, double>> read_injection_file(
     return fault_vector;
 }
 
-void inject_fault(int TA, int TB, int M, int N, int K, float *C, int layer_count_output) {
+void inject_fault(int TA, int M, int K, std::vector<float>& C, int layer_count_output) {
     auto layer_char = std::getenv("FAULT_LAYER");
     auto fault_id = std::getenv("FAULT_ID");
     auto fault_dir = std::getenv("FAULT_DIR");
@@ -234,11 +181,10 @@ void parse_output_conv_layer_gpu(int TA, int TB, int M, int N, int K, float *C) 
 
             //compare and save the corrupted ones
             auto &gold = layers_gold_hash[layer_count_output];
-            auto &found = c_cpu;
             auto comparator = [](float &lhs, float &rhs) -> bool {
                 return std::fabs(lhs - rhs) > MAX_FLOAT_THRESHOLD;
             };
-            auto cmp_result = std::equal(gold.begin(), gold.end(), found.begin(), comparator);
+            auto cmp_result = std::equal(gold.begin(), gold.end(), c_cpu.begin(), comparator);
             if (!cmp_result) {
                 auto log_file_name = Log::get_log_path();
                 auto layer_out_path = replace(log_file_name, ".log",
@@ -250,7 +196,8 @@ void parse_output_conv_layer_gpu(int TA, int TB, int M, int N, int K, float *C) 
             break;
         }
         case INJECT_FAULT_IN_OUTPUT:
-            inject_fault(TA, TB, M, N, K, C, layer_count_output);
+            inject_fault(TA, M, K, c_cpu, layer_count_output);
+            cuda_push_array(C, c_cpu.data(), c_cpu.size());
             break;
 
     }
@@ -259,19 +206,19 @@ void parse_output_conv_layer_gpu(int TA, int TB, int M, int N, int K, float *C) 
 
 void parse_input_conv_layer_gpu(int TA, int TB, int M, int N, int K, float ALPHA, float *A,
                                 int lda, float *B, int ldb, float BETA, float *C, int ldc) {
-    static int layer_count_input = 0;
-    layer_count_input++;
-    auto size_a = K * M;
-    auto size_b = K * N;
-    auto size_c = M * N;
-
-    std::vector<float> a_cpu(size_a);
-    std::vector<float> b_cpu(size_b);
-    std::vector<float> c_cpu(size_c);
-
-    cuda_pull_array(A, a_cpu.data(), size_a);
-    cuda_pull_array(B, b_cpu.data(), size_b);
-    cuda_pull_array(C, c_cpu.data(), size_c);
+//    static int layer_count_input = 0;
+//    layer_count_input++;
+//    auto size_a = K * M;
+//    auto size_b = K * N;
+//    auto size_c = M * N;
+//
+//    std::vector<float> a_cpu(size_a);
+//    std::vector<float> b_cpu(size_b);
+//    std::vector<float> c_cpu(size_c);
+//
+//    cuda_pull_array(A, a_cpu.data(), size_a);
+//    cuda_pull_array(B, b_cpu.data(), size_b);
+//    cuda_pull_array(C, c_cpu.data(), size_c);
 
 //    parse_entry_cpu(TA, TB, M, N, K, ALPHA, a_cpu.data(), lda, b_cpu.data(),
 //                    ldb, BETA, c_cpu.data(), ldc);
@@ -280,3 +227,63 @@ void parse_input_conv_layer_gpu(int TA, int TB, int M, int N, int K, float ALPHA
 #ifdef __cplusplus
 }
 #endif
+
+/*
+ *
+template<typename type_t>
+bool write_file(type_t *src, std::string &path, std::string &header, int m,
+                int n) {
+    std::ofstream of(path, std::ios::trunc);
+    if (of.good()) {
+        of << header << std::endl;
+        of << std::scientific << std::setprecision(16);
+        for (auto i = 0; i < m; i++) {
+            for (auto j = 0; j < n; j++) {
+                of << src[i * n + j];
+                if (j < (n - 1))
+                    of << ",";
+            }
+            of << std::endl;
+        }
+
+        of.close();
+        return true;
+    }
+    return false;
+}
+
+
+void
+parse_entry_cpu(std::string &path_a, std::string &path_b, int TA, int TB, int M, int N, int K, float ALPHA, float *A,
+                int lda, float *B, int ldb, float BETA, float *C, int ldc) {
+
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (C[i * ldc + j] != 0.0f)
+                std::cout << "Vall C " << C[i * ldc + j] << std::endl;
+        }
+    }
+
+    auto header_a = "TRANSPOSE," + std::to_string(TA) + ",M,"
+                    + std::to_string(M) + ",N," + std::to_string(N) + ",K,"
+                    + std::to_string(K);
+
+    //MATRIX A is M and K
+    if (TA) {
+        write_file(A, path_a, header_a, K, M);
+    } else {
+        write_file(A, path_a, header_a, M, K);
+    }
+
+    auto header_b = "TRANSPOSE," + std::to_string(TB) + ",M,"
+                    + std::to_string(M) + ",N," + std::to_string(N) + ",K,"
+                    + std::to_string(K);
+    //MATRIX B is K and N
+    if (TB) {
+        write_file(B, path_b, header_b, N, K);
+    } else {
+        write_file(B, path_b, header_b, K, N);
+    }
+
+}
+ */
