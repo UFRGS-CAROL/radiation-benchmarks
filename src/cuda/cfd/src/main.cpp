b8056d6c138dbd7fc8528b2c9abcc1fdd4981791
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-09-01 19:43:49 -0300

    Setting up CFD cuda

--- src/cuda/cfd/src/main.cpp
+++ src/cuda/cfd/src/main.cpp
@@ -407,12 +407,10 @@ int main(int argc, char **argv) {
         if (!parameters.generate) {
             std::vector<size_t> error_vector(parameters.stream_number);
 #pragma omp parallel for shared(logger)
-            {
-                for (int stream = 0; stream < parameters.stream_number; stream++) {
-                    error_vector[stream] = compare_gold(gold_array, host_variables[stream],
-                                                        logger, nel, nelr, stream);
-                }
+            for (int stream = 0; stream < parameters.stream_number; stream++) {
+                error_vector[stream] = compare_gold(gold_array, host_variables[stream], logger, nel, nelr, stream);
             }
+
             for (auto err_i : error_vector) {
                 errors += err_i;
             }
