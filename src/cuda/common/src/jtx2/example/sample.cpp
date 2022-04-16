fa59bedb63f0f986a8019079705d94ecfa759d13
prbodmann <prbodmann@gmail.com>
2022-04-16 15:27:10 +0200

    added sleep time as an programm argument

--- src/cuda/common/src/jtx2/example/sample.cpp
+++ src/cuda/common/src/jtx2/example/sample.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 	measure.start_profile();
 
 	std::cout << "SLEEPING FOR "<<argv[0] <<" seconds" << std::endl;
-	sleep(atoi(argv[0]));
+	sleep(atoi(argv[1]));
 	measure.end_profile();
 
 	//auto test = measure.get_data_from_iteration();
